fb281163ee2f3a118979b8648e24b1f6960d596c
Martin Schoeberl <martin@jopdesign.com>
2011-09-17 00:03:43 +0200

    simulation: some more experiments

--- java/src/simulator/Register.java
+++ java/src/simulator/Register.java
@@ -4,11 +4,16 @@ public class Register {
 
 	static class Data {
 		int pc;
-		static class FeDe {
+		static class Fetch {
 			int instr[] = new int[2];
 			boolean valid[] = new boolean[2];
 		}
-		FeDe fede = new FeDe();
+		Fetch fetch = new Fetch();
+		static class Decode {
+			int regA[] = new int[2];
+			int regB[] = new int[2];
+		}
+		Decode decode = new Decode();
 	}
 	
 	Data d = new Data();
@@ -17,8 +22,10 @@ public class Register {
 	void tick() {
 		q.pc = d.pc;
 		for (int i=0; i<2; ++i) {
-			q.fede.instr[i] = d.fede.instr[i];
-			q.fede.valid[i] = q.fede.valid[i];
+			q.fetch.instr[i] = d.fetch.instr[i];
+			q.fetch.valid[i] = d.fetch.valid[i];
+			q.decode.regA[i] = d.decode.regA[i];
+			q.decode.regB[i] = d.decode.regB[i];
 		}
 	}
 
--- java/src/simulator/SimPat.java
+++ java/src/simulator/SimPat.java
@@ -1,6 +1,8 @@
 package simulator;
 
 public class SimPat {
+	
+	final static int REG_SHIFT = 0; // 7;
 
 	Register reg = new Register();
 	
@@ -12,17 +14,29 @@ public class SimPat {
 	};
 	
 	public void tick() {
-		reg.d.fede.instr[0] = mem[reg.q.pc];
-		reg.d.fede.instr[1] = mem[reg.q.pc+1];
-		reg.d.fede.valid[0] = true;
-		if ((reg.d.fede.instr[0] & 0x80000000) != 0) {
+		
+		// fetch and PC stage
+		reg.d.fetch.instr[0] = mem[reg.q.pc];
+		reg.d.fetch.instr[1] = mem[reg.q.pc+1];
+		reg.d.fetch.valid[0] = true;
+		if ((reg.d.fetch.instr[0] & 0x80000000) != 0) {
 			reg.d.pc = reg.q.pc+2;
-			reg.d.fede.valid[0] = true;			
+			reg.d.fetch.valid[1] = true;			
 		} else {
 			reg.d.pc = reg.q.pc+1;
-			reg.d.fede.valid[0] = false;
+			reg.d.fetch.valid[1] = false;
 			
 		}
+		
+		// decode stage
+		for (int i=0; i<2; ++i) {
+			int instr = reg.q.fetch.instr[i];
+			// read register addresses
+			reg.d.decode.regA[i] = (instr>>(REG_SHIFT+5)) & 0x1f;
+			reg.d.decode.regB[i] = (instr>>(REG_SHIFT)) & 0x1f;
+			
+		}
+
 		
 		reg.tick();
 	}
@@ -34,7 +48,8 @@ public class SimPat {
 
 		SimPat sim = new SimPat();
 		for (int i=0; i<10; ++i) {
-			System.out.println(sim.reg.q.pc + " " + sim.reg.q.fede.instr[0]);
+			System.out.println(sim.reg.q.pc + " " + sim.reg.q.fetch.instr[0]
+			                   + " " + sim.reg.q.decode.regB[0]);
 			sim.tick();
 		}
 	}
