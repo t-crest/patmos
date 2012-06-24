7c1819e14acd0877ad96bb359d937ef0b815151f
Martin Schoeberl <martin@jopdesign.com>
2012-06-24 15:08:48 +0200

    asm: sort out asm files

--- asm/test.asm
+++ asm/test.asm
@@ -1,16 +1,7 @@
-label:
-	p1 br lab1
-	addi r1, 10 -> r13 // hhj
-	p1 addi r1, 10 -> r13 // hhj
-	sub r2, r3 -> r2
-	p5 sub r2, r3 -> r31
-	br label
-lab1:
-	br lab1
-	p1 cmp r1 != r2 -> p3
 
+# let's get started with some Patmos assembly code
 
-// That's not Patmos, but Leros assembler....
-// Register definitions
-
-
+	addi r0 = r0, 8;
+	addi r1 = r0, 7;
+	add r2 = r0, r1;
+	halt;
