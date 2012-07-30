5f6a8de6266b8ea2bef6396967892d664179d36f
Martin Schoeberl <martin@jopdesign.com>
2012-07-30 18:09:10 +0200

    asm: some cleanup

--- asm/echo.asm
+++ asm/echo.asm
@@ -1,5 +1,7 @@
 #
-# Expected Result: ...
+# This is a simple echo program on the UART
+#
+# Expected Result: echo entered characters
 #
 
 		addi    r16  = r16 , 64;
@@ -7,8 +9,7 @@
 		lwm     r10  = [r5 + 0];
                 addi    r0  = r0 , 1;
                 and     r11  = r10 , r1;
-                and     r11  = r10 , r1;
-#		benq
+		bne     r1 != r11 , 4;
                 addi    r0  = r0 , 1;
                 lwm     r15  = [r5 + 1];
                 lwm     r15  = [r5 + 1];
@@ -16,14 +17,14 @@
 		lwm     r10  = [r5 + 0];
 		addi    r0  = r0 , 1;
 		and     r11 = r3 , r10;
-		and     r11 = r3 , r10;
-#		benq
+		bne     r1 != r11 , 4;
 		addi    r0  = r0 , 1;
 		swm	[r7 + 1] = r15;
 		addi    r0  = r0 , 1;
 		addi	r3  = r0 , 2;
 		addi	r3  = r0 , 2;
-#		benq
 		addi    r0  = r0 , 1;
+		andi    r3 = r3 , 0;
+		bne	r16 != r0 , 20;
                 halt;
 
--- asm/test.asm
+++ asm/test.asm
@@ -1,28 +1,12 @@
 #
-# Expected Result: ...
+# This is a simple ouput of a single character on the UART
+#
+# TODO: maybe this should just switch a LED to see the result.
 #
 
-		addi    r16  = r16 , 64;
-		addi	r1   = r0 , 2;
-		lwm     r10  = [r5 + 0];
-                addi    r0  = r0 , 1;
-                and     r11  = r10 , r1;
-		bne     r1 != r11 , 4;
-                addi    r0  = r0 , 1;
-                lwm     r15  = [r5 + 1];
-                lwm     r15  = [r5 + 1];
-		addi	r3 = r19 , 1;
-		lwm     r10  = [r5 + 0];
-		addi    r0  = r0 , 1;
-		and     r11 = r3 , r10;
-		bne     r1 != r11 , 4;
-		addi    r0  = r0 , 1;
-		swm	[r7 + 1] = r15;
-		addi    r0  = r0 , 1;
-		addi	r3  = r0 , 2;
-		addi	r3  = r0 , 2;
-		addi    r0  = r0 , 1;
-		andi    r3 = r3 , 0;
-		bne	r16 != r0 , 20;
-                halt;
+# TODO: looks like the UART is in memory address 0....
 
+	addi	r1 = r0, 0;
+	addi	r2 = r0, 42; # '*'
+	swm	[r1 + 1] = r2;
+	halt;
