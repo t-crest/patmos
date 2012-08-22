b8138dd86c37540a020169c6d90cb03326204ac7
Sahar Abbaspour <Sahar Abbaspour>
2012-08-22 19:01:57 +0200

    check for signed

--- asm/compare.s
+++ asm/compare.s
@@ -29,6 +29,39 @@
 	(p5) 	bc	22; #24
 	addi    r10 = r10, 8;	#25
 	nop 	0;	#26
-#	halt; 
+
+	addi    r12 = r0, 0;	
+	addi    r1 = r0, 2;
+	addi    r2 = r0, 3;
+	cmpult	p6 = r1, r2; #
+	(p6) 	bc	30; #
+	addi    r12 = r12, 1;
+	addi    r1 = r1, 1;
+
+	addi    r13 = r0, 0;	
+	addi    r1 = r0, 2;
+	addi    r2 = r0, 4;
+	cmpule	p7 = r1, r2; #
+	(p7) 	bc	37; #
+	addi    r13 = r13, 1;
+	addi    r1 = r1, 1;
+	nop	0;
+#signed check
+	addi	r1 = r0, 1;  
+	sli	r1 = r1, 31;
+	ori	r1 = r1, 15;
+	addi	r2 = r0, 1;  
+	sli	r2 = r2, 31;
+	ori     r2 = r2, 9;
+	nop	0;
+	nop	0;
+	nop	0;
+	cmplt   p2  = r1, r2; 
+	(p2) 	bc	51; 
+	addi	r1 = r2, 1; 
+	nop	0;
+	addi	r23 = r0, 1;  	
+
+	halt; 
 
 
