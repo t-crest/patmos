182f8c0970c34f9525b42111493d24fd2ddd4eb6
Martin Schoeberl <martin@jopdesign.com>
2012-08-23 14:46:53 +0200

    test: more on regression test automation

---
+++ testsuit/patmos-regression-test.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# git clone and run Patmos regression test
+#
+LOG_DIR=`pwd`/logs
+
+# get the JOP source tree
+rm -rf patmos
+git clonc git://github.com/schoeberl/patmos.git
+cd patmos
+
+# Make sure working tree is clean
+# git status | grep "working directory clean" >/dev/null
+# if [ $? -ne 0 ] ; then
+#  echo "Working directory not clean - aborting nightly build" ; exit 1
+# fi
+
+# Setup the environment
+#. testsuite/env.sh
+
+# Run testsuite
+#testsuite/rotate.sh ${LOG_DIR}
+#mkdir ${LOG_DIR}/current
+#testsuite/run.sh ${LOG_DIR}/current
+
+testsuite/run.sh > report.txt
+
+# Clean the working tree
+# git -d clean
+
+# Mail results
+#LOG_FILE=${LOG_DIR}/current/report.txt
+LOG_FILE=report.txt
+RECIPIENTS=`cat testsuite/recipients.txt`
+dos2unix ${LOG_FILE}
+mail -s "[Patmos Nightly] Build report `date`" ${RECIPIENTS} < ${LOG_FILE}
