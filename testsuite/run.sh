b8219055c54260303264ba9c7bbcb8140a471084
Martin Schoeberl <martin@jopdesign.com>
2012-08-23 21:05:08 +0200

    test: report non-working benchmarks

--- testsuite/run.sh
+++ testsuite/run.sh
@@ -3,7 +3,10 @@ tests="basic test ALU ALUi compare"
 not_working="branch dual_forwarding dual_even_odd_address"
 
 make tools
-
+echo === Tests ===
 for f in  ${tests}; do
     testsuite/single.sh ${f}
+done
+for f in  ${not_working}; do
+    echo issue with ${f}
 done
