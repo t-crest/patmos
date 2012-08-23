261cb584d582603e425500163030c98f3d60cd04
Martin Schoeberl <martin@jopdesign.com>
2012-08-23 15:21:37 +0200

    test: build tools first

--- testsuit/run.sh
+++ testsuit/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 tests="basic test ALU ALUi compare"
 not_working="branch dual_forwarding dual_even_odd_address"
+
+make tools
+
 for f in  ${tests}; do
     testsuit/single.sh ${f}
 done
