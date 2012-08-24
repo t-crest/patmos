ccd263099de0c7eda06da7deed62c4b7d3fa30af
Martin Schoeberl <martin@jopdesign.com>
2012-08-24 20:50:19 +0200

    test: add a plain ModelSim compilation first to look for compilation errors

--- testsuite/run.sh
+++ testsuite/run.sh
@@ -3,6 +3,7 @@ tests="basic test ALU ALUi compare"
 not_working="branch dual_forwarding dual_even_odd_address"
 
 make tools
+make rom bsim
 echo === Tests ===
 for f in  ${tests}; do
     testsuite/single.sh ${f}
