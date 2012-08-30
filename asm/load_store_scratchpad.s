626342847d88b56a74c544ddac65250bc3999d98
Sahar Abbaspour <Sahar Abbaspour>
2012-08-30 14:00:03 +0200

    patmos: new load/store word aligned

--- asm/load_store_scratchpad.s
+++ asm/load_store_scratchpad.s
@@ -3,13 +3,13 @@
 # different ld/st from/to scratchpad memory
 
 	addi	r1 = r0, 255;  # first instruction not executed
-	addi	r1 = r0, 255; # r1 = 255
+	addi	r1 = r0, 256; # r1 = 255
 	addi	r2 = r0, 5;
 	addi    r4 = r0, 4;
 	swl	[r1 + 1] = r2; # memory address 259 (255 + (1 sl 2)) = 5 (all memory banks are updated)
 	lwl	r10  = [r1 + 1]; # register(10) = 5
 	nop 	0;
-	add	r2 = r2, r10; # r2 = 15 
+	add	r2 = r2, r10; # r2 = 15
 
 	shl	[r1 + 3] = r2; # memory address 261 = 10 (two memory banks are updated)
 	lhl	r11  = [r1 + 3]; # register(11) = 10
@@ -20,11 +20,11 @@
 
 	lhul    r13 = [r1 + 3]; # r13 = memory address (255 + (3 sl 1)) = 261 (10);
 	lbul	r14 = [r1 + 3]; # r14 = 4;
-
+	addi    r1 = r0 , 256;
 	addi    r5 = r0, 3;
 	sli	r5 = r5, 15;
-	swl     [r1 + 3] = r5; # memory address 267 = 98304
-	lhl	r20 = [r1 + 6];   # r1 + 6 = r1 + 3 to check signed!
+	swl     [r1 + 1] = r5; # memory address 
+	lhl	r20 = [r1 + 2];   # r1 + 6 = r1 + 3 to check signed!
 	addi    r5 = r0, 1;
 	sli	r5 = r5, 7;
 	swl     [r1 + 3] = r5; # memory address 267 =
@@ -44,4 +44,6 @@
 	subi    r30 = r30, 1;
 	cmpneq  p1 = r31, r29;
 (p1)	bc	25; #r20 equals to all upper bits 1 
+	halt; 
+
 	halt; 
