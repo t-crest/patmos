6e66837f86b19fd55e4f1cda6ee2263de426436e
Sahar Abbaspour <Sahar Abbaspour>
2012-08-31 17:41:07 +0200

    patmos: data cache test word aligned

--- asm/load_store_data_cache.s
+++ asm/load_store_data_cache.s
@@ -3,15 +3,15 @@
 # different ld/st from/to scratchpad memory
 
 	addi	r1 = r0, 255;  # first instruction not executed
-	addi	r1 = r0, 255; # r1 = 255
+	addi	r1 = r0, 256; # r1 = 255
 	addi	r2 = r0, 5;
 	addi    r4 = r0, 4;
-	swc	[r1 + 1] = r2; # memory address 259 (255 + (1 sl 2)) = 5 (all memory banks are updated)
+	swc	[r1 + 1] = r2; # memory address 260 (256 + (1 sl 2)) = 5 (all memory banks are updated)
 	lwc	r10  = [r1 + 1]; # register(10) = 5
 	nop 	0;
 	add	r2 = r2, r10; # r2 = 5 + 5 = 10 
 
-	shc	[r1 + 3] = r2; # memory address 261 = 10 (two memory banks are updated)
+	shc	[r1 + 3] = r2; # memory address 262 = 10 (two memory banks are updated)
 	lhc	r11  = [r1 + 3]; # register(11) = 10
 	nop	0;
 
@@ -20,11 +20,11 @@
 
 	lhuc    r13 = [r1 + 3]; # r13 = memory address (255 + (3 sl 1)) = 261 (10);
 	lbuc	r14 = [r1 + 3]; # r14 = 4; #memory address (255 + 3) = 258 (4) 
-
+addi    r1 = r0 , 256;
 	addi    r5 = r0, 3; # r5 = 3
 	sli	r5 = r5, 15; # r5(16, 15) = 11
-	swc     [r1 + 3] = r5; # memory address 267 = 98304
-	lhc	r20 = [r1 + 6];   # r1 + 6 = r1 + 3 to check signed!
+	swc     [r1 + 1] = r5; # memory address 267 = 98304
+	lhc	r20 = [r1 + 2];   # r1 + 6 = r1 + 3 to check signed!
 	addi    r5 = r0, 1;
 	sli	r5 = r5, 7;
 	swc     [r1 + 3] = r5; # memory address 267 = 128
