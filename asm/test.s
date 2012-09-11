be4d83f8dac97e62a3af554133d0c3855b3c51f8
Martin Schoeberl <martin@jopdesign.com>
2012-09-11 23:15:03 +0200

    io: fix changed IO address of the UART

--- asm/test.s
+++ asm/test.s
@@ -7,7 +7,7 @@
 # TODO: looks like the UART is in memory address 0....
 
 	addi	r0 = r0, 0;  # first instruction not executed
-	addi	r1 = r0, 8;
+	addi	r1 = r0, 15;
 	sli	r1 = r1, 28;
 	addi	r2 = r0, 42; # '*'
 	swm	[r1 + 1] = r2;
