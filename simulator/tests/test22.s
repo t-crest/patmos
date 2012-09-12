f527b7b9ba9feb57ce840ab21edee5dff877187d
Daniel Prokesch <daniel.prokesch@gmail.com>
2012-09-12 16:29:12 +0200

    adapted tests to reflect new UART interface
    * new uart data address
    * word access instead of byte access

--- simulator/tests/test22.s
+++ simulator/tests/test22.s
@@ -3,10 +3,10 @@
 #
                 add     r1  = r0, 0xF0000000;
                 addi    r2  = r0, 2;
-                lbl     r3  = [r1 + 0];
+                lwl     r3  = [r1 + 0];
                 andi    r3  = r3, 2;
                 bne     r3 != r2, -4;
                 nop     0;
                 nop     0;
-                lbul    r1  = [r1 + 1];
+                lwl     r1  = [r1 + 1];
                 halt;
--- simulator/tests/test23.s
+++ simulator/tests/test23.s
@@ -3,11 +3,11 @@
 #
                 add     r1  = r0, 0xF0000000;
                 addi    r2  = r0, 1;
-                lbl     r3  = [r1 + 0];
+                lwl     r3  = [r1 + 0];
                 andi    r3  = r3, 1;
                 bne     r3 != r2, -4;
                 nop     0;
                 nop     0;
                 addi    r2 = r0, 0x21;
-                sbl     [r1 + 1] = r2;
+                swl     [r1 + 1] = r2;
                 halt;
