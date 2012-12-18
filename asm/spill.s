dfe577cc9615527784e9ab24927a54b959a8e438
Sahar Abbaspour <Sahar Abbaspour>
2012-12-18 15:54:25 +0100

    patmos: change size of reserve

--- asm/spill.s
+++ asm/spill.s
@@ -4,7 +4,7 @@
 		addi	r5 = r0, 0;                
 		addi    r1 = r0, 0;
 		addi	r10 = r0, 10;
-                sres    16;
+                sres    6;
 l1:             sws     [r1 + 0] = r5;
 		addi	r1 = r1, 1;
 		addi    r5 = r5, 1;
@@ -12,7 +12,7 @@ l1:             sws     [r1 + 0] = r5;
 		(p2)	br      l1;
 		addi    r0 = r0, 0;
 		addi    r0 = r0, 0;
-		sres    10; # this will cause spill
+		sres    8; # this will cause spill
 #		addi 	r2 = r0, 0;
 #		addi    r6 = r0, 1;
 #		sws	[r2 + 0] = r6; # we should store to 
