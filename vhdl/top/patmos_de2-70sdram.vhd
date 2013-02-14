71b98a449c717abf6b0282343ff0f23a019af4f3
Edgar Lakis <edgar.lakis@gmail.com>
2013-02-14 12:32:57 +0100

    sdram: Moved SDRAM parameters and interface into packages for easier configuration

--- vhdl/top/patmos_de2-70sdram.vhd
+++ vhdl/top/patmos_de2-70sdram.vhd
@@ -48,7 +48,7 @@ entity patmos_top is
         dram0_RAS_n : out   std_logic;  -- Row Address Strobe
         dram0_CAS_n : out   std_logic;  -- Column Address Strobe
         dram0_WE_n  : out   std_logic;  -- Write Enable
-        dram0_CS_n  : out   std_logic; -- Chip Select
+        dram0_CS_n  : out   std_logic;  -- Chip Select
         dram0_BA_0  : out   std_logic;  -- Bank Address
         dram0_BA_1  : out   std_logic;  -- Bank Address
         dram0_ADDR  : out   std_logic_vector(12 downto 0); -- SDRAM Address
@@ -60,7 +60,7 @@ entity patmos_top is
         dram1_RAS_n : out   std_logic;  -- Row Address Strobe
         dram1_CAS_n : out   std_logic;  -- Column Address Strobe
         dram1_WE_n  : out   std_logic;  -- Write Enable
-        dram1_CS_n  : out   std_logic; -- Chip Select
+        dram1_CS_n  : out   std_logic;  -- Chip Select
         dram1_BA_0  : out   std_logic;  -- Bank Address
         dram1_BA_1  : out   std_logic;  -- Bank Address
         dram1_ADDR  : out   std_logic_vector(12 downto 0); -- SDRAM Address
@@ -88,61 +88,37 @@ end entity patmos_top;
 library ieee;
 use ieee.numeric_std.all;
 use work.patmos_type_package.all;
+use work.sdram_config.all;
+use work.sdram_controller_interface.all;
 
 architecture RTL of patmos_top is
-    constant BURST_LENGTH : natural := 8;
+    constant BURST_LENGTH : natural           := 8;
+    -- 100MHz, 2 cycles read data latency
+    constant tCLK_PERIOD  : time              := 10 ns;
+    constant CAS_LATENCY  : natural           := 2;
+    constant SDRAM        : sdram_config_type := GetSDRAMParameters(tCLK_PERIOD, CAS_LATENCY);
 
-    constant ADDR_WIDTH  : integer := 24; -- 16M (addr) x 4 (bytes) == 64MB 
-    constant DATA_WIDTH  : integer := 32;
-    -- Address Mapping
-    constant COL_WIDTH   : integer := 9;
-    constant ROW_WIDTH   : integer := 13;
-    constant BA_WIDTH    : integer := 2;
-    constant CS_WIDTH    : integer := 0;
+    -- Address Mapping => (bank & row & column)
+    constant CS_WIDTH    : integer := 0; -- 1 rank
     constant COL_LOW_BIT : integer := 0;
-    constant ROW_LOW_BIT : integer := COL_LOW_BIT + COL_WIDTH; -- 9
-    constant BA_LOW_BIT  : integer := ROW_LOW_BIT + ROW_WIDTH; -- 9+13=22
-    constant CS_LOW_BIT  : integer := BA_LOW_BIT + BA_WIDTH; -- 22+2=24
-    -- SDRAM configuration
-    constant SA_WIDTH    : natural := dram0_ADDR'length;
+    constant ROW_LOW_BIT : integer := COL_LOW_BIT + SDRAM.COL_WIDTH; -- 9
+    constant BA_LOW_BIT  : integer := ROW_LOW_BIT + SDRAM.ROW_WIDTH; -- 9+13=22
+    constant CS_LOW_BIT  : integer := BA_LOW_BIT + SDRAM.BA_WIDTH; -- 22+2=24
 
-    constant tCLK               : time    := 10 ns; --! Clock period
-    constant tINIT_IDLE         : time    := 200 us; --! Inactivity perdiod required during initialization 
-    constant INIT_REFRESH_COUNT : natural := 8; --! Number of Refresh commands required during initialization
-    constant tCAC_CYCLES        : natural := 2; --! CAS latency
-    constant tRRD               : time    := 14 ns; --! Row to Row Delay (ACT[0]-ACT[1])
-    constant tRCD               : time    := 20 ns; --! Row to Column Delay (ACT-READ/WRITE)
-    constant tRAS               : time    := 45 ns; --! Row Access Strobe (ACT-PRE)
-    constant tRC                : time    := 67.5 ns; --! Row Cycle (REF-REF,ACT-ACT)
-    constant tRP                : time    := 20 ns; --! Row Precharge (PRE-ACT)
-    constant tCCD               : time    := tCLK * 1; --! Column Command Delay Time
-    constant tDPL               : time    := 14 ns; --! Input Data to Precharge (DQ_WR-PRE)
-    constant tDAL               : time    := 35 ns; --! Input Data to Activate (DQ_WR-ACT/PRE)
-    constant tRBD               : time    := tCLK * tCAC_CYCLES; --! Burst Stop to High Impedance (Read)
-    constant tWBD               : time    := 0 ns; --! Burst Stop to Input in Invalid (Write)
-    constant tPQL               : time    := tCLK * (tCAC_CYCLES - 1); --! Last Output to Auto-Precharge Start (READ)
-    constant tQMD               : time    := tCLK * 2; --! DQM to Output (Read)
-    constant tDMD               : time    := 0 ns; --! DQM to Input (Write)
-    constant tMRD               : time    := 15 ns; --! Mode Register Delay (program time)
-    constant tMRD_CYCLES        : natural := 2; --! Mode Register Delay (program time) in Cycles
--- This was used to see how many errors would occur if refresh is performed way to rearly 
---    constant tREF               : time    := 1000*64 ms; --! Refresh Cycle (for each row)
-    constant tREF               : time    := 64 ms; --! Refresh Cycle (for each row)
 
     constant DQ_WIDTH         : integer := 8;
     constant MTL_MASK_WIDTH   : integer := 4;
     --  constant MTL_SIZE_WIDTH   : integer := 5;
     constant MTL_SIZE_WIDTH   : integer := 5;
-    constant MTL_ADDR_WIDTH   : integer := ADDR_WIDTH;
+    constant MTL_ADDR_WIDTH   : integer := SDRAM_ADDR_WIDTH;
     -- Request size in bytes
     --  constant GEN_REQUEST_SIZE : integer := 64;
     constant GEN_REQUEST_SIZE : integer := 4 * BURST_LENGTH;
     -- DMA control interface
     constant DMA_ADDR_WIDTH   : integer := 3;
     constant DMA_DATA_WIDTH   : integer := 32;
-    
 
-    signal res_cnt    : std_logic_vector(2 downto 0) := "000";
+    signal res_cnt : std_logic_vector(2 downto 0) := "000";
     attribute altera_attribute : string;
     attribute altera_attribute of res_cnt : signal is "POWER_UP_LEVEL=LOW";
     signal rst, rst_n : std_logic;
@@ -158,28 +134,18 @@ architecture RTL of patmos_top is
     signal dram_CAS_N : std_logic;
     signal dram_RAS_N : std_logic;
     signal dram_CKE   : std_logic;
-    signal dram_CS_N  : std_logic_vector(2**CS_WIDTH-1 downto 0);
+    signal dram_CS_N  : std_logic_vector(2 ** CS_WIDTH - 1 downto 0);
     signal dram_WE_N  : std_logic;
 
     signal dma_addr_special_i : std_logic;
-    signal dma_addr_i         : std_logic_vector(DMA_ADDR_WIDTH-1 downto 0);
+    signal dma_addr_i         : std_logic_vector(DMA_ADDR_WIDTH - 1 downto 0);
     signal dma_rd_i           : std_logic;
     signal dma_rd_data_i      : std_logic_vector(31 downto 0);
     signal dma_wr_i           : std_logic;
     signal dma_wr_data_i      : std_logic_vector(31 downto 0);
 
-    signal ocp_MCmd           : std_logic_vector(2 downto 0);
-    signal ocp_MCmd_doRefresh : std_logic;
-    signal ocp_MAddr          : std_logic_vector(ADDR_WIDTH - 1 downto 0);
-    signal ocp_SCmdAccept     : std_logic;
-    signal ocp_MData          : std_logic_vector(DATA_WIDTH - 1 downto 0);
-    signal ocp_MDataByteEn    : std_logic_vector(DATA_WIDTH / 8 - 1 downto 0);
-    signal ocp_MDataValid     : std_logic;
-    signal ocp_MDataLast      : std_logic;
-    signal ocp_SDataAccept    : std_logic;
-    signal ocp_SData          : std_logic_vector(DATA_WIDTH - 1 downto 0);
-    signal ocp_SResp          : std_logic;
-    signal ocp_SRespLast      : std_logic;
+    signal ocpMaster : SDRAM_controller_master_type;
+    signal ocpSlave  : SDRAM_controller_slave_type;
 
     signal mtl_wr_valid_i       : std_logic;
     signal mtl_cmd_valid_i      : std_logic;
@@ -192,12 +158,12 @@ architecture RTL of patmos_top is
     signal mtl_wr_accept_i      : std_logic;
     signal mtl_wr_data_i        : std_logic_vector(4 * DQ_WIDTH - 1 downto 0);
     signal mtl_wr_mask_i        : std_logic_vector(MTL_MASK_WIDTH - 1 downto 0);
-    signal mtl_rd_last_i      : std_logic;
-    signal mtl_rd_valid_i     : std_logic;
-    signal mtl_rd_accept_i    : std_logic;
-    signal mtl_rd_data_i      : std_logic_vector(4 * DQ_WIDTH - 1 downto 0);
-    
---    attribute ALTERA_ATTRIBUTE : string;
+    signal mtl_rd_last_i        : std_logic;
+    signal mtl_rd_valid_i       : std_logic;
+    signal mtl_rd_accept_i      : std_logic;
+    signal mtl_rd_data_i        : std_logic_vector(4 * DQ_WIDTH - 1 downto 0);
+
+    --    attribute ALTERA_ATTRIBUTE : string;
     attribute ALTERA_ATTRIBUTE of dram0_BA_0 : signal is "FAST_OUTPUT_REGISTER=ON";
     attribute ALTERA_ATTRIBUTE of dram0_BA_1 : signal is "FAST_OUTPUT_REGISTER=ON";
     attribute ALTERA_ATTRIBUTE of dram0_CAS_n : signal is "FAST_OUTPUT_REGISTER=ON";
@@ -221,19 +187,17 @@ architecture RTL of patmos_top is
     attribute ALTERA_ATTRIBUTE of dram1_LDQM : signal is "FAST_OUTPUT_REGISTER=ON";
 
     attribute ALTERA_ATTRIBUTE of dram_DQ : signal is "FAST_INPUT_REGISTER=ON;FAST_OUTPUT_REGISTER=ON";
-    --attribute ALTERA_ATTRIBUTE of sdram_DQoe_r: port is "FAST_OUTPUT_REGISTER=ON";
-  
-    
-begin
-    assert 2**DMA_ADDR_WIDTH = BURST_LENGTH report "BURST_LENGTH should be == 2**DMA_ADDR_WIDTH" severity failure;
-    
+--attribute ALTERA_ATTRIBUTE of sdram_DQoe_r: port is "FAST_OUTPUT_REGISTER=ON";
 
+
+begin
+    assert 2 ** DMA_ADDR_WIDTH = BURST_LENGTH report "BURST_LENGTH should be == 2**DMA_ADDR_WIDTH" severity failure;
 
     process(sys_clk, pll_locked)
     begin
         if pll_locked = '0' then
-            res_cnt  <= "000";
-            rst  <= '1';
+            res_cnt <= "000";
+            rst     <= '1';
         elsif rising_edge(sys_clk) then
             if (res_cnt /= "111") then
                 res_cnt <= std_logic_vector(unsigned(res_cnt) + 1);
@@ -241,7 +205,7 @@ begin
             rst <= not res_cnt(0) or not res_cnt(1) or not res_cnt(2);
         end if;
     end process;
-    rst_n  <= not rst;
+    rst_n <= not rst;
 
     pll : entity work.de2_70_sdram_pll
         port map(
@@ -252,19 +216,20 @@ begin
             locked => pll_locked);
 
     patmos_sdram_inst : entity work.patmos_sdram
-        port map(clk                => dram_clk,
---                 rst                => rst,
-                 led                => led,
-                 txd                => txd,
-                 rxd                => rxd,
-                 dma_addr_special_i => dma_addr_special_i,
-                 dma_addr_i(dma_addr_i'range)  => dma_addr_i,
-                 dma_rd_i           => dma_rd_i,
-                 dma_rd_data_i      => dma_rd_data_i,
-                 dma_wr_i           => dma_wr_i,
-                 dma_wr_data_i      => dma_wr_data_i);
+        port map(clk                          => dram_clk,
+                 --                 rst                => rst,
 
-   sdram_io_controller: work.dma_controller_dtl_cmp_pkg.dma_controller_dtl
+                 led                          => led,
+                 txd                          => txd,
+                 rxd                          => rxd,
+                 dma_addr_special_i           => dma_addr_special_i,
+                 dma_addr_i(dma_addr_i'range) => dma_addr_i,
+                 dma_rd_i                     => dma_rd_i,
+                 dma_rd_data_i                => dma_rd_data_i,
+                 dma_wr_i                     => dma_wr_i,
+                 dma_wr_data_i                => dma_wr_data_i);
+
+    sdram_io_controller : work.dma_controller_dtl_cmp_pkg.dma_controller_dtl
         generic map(
             DQ_WIDTH         => DQ_WIDTH,
             MTL_MASK_WIDTH   => MTL_MASK_WIDTH,
@@ -297,87 +262,52 @@ begin
             dma_rd_data_i        => dma_rd_data_i,
             dma_wr_i             => dma_wr_i,
             dma_wr_data_i        => dma_wr_data_i);
-   
-   sdr_sdram_inst : entity work.sdr_sdram
+
+    sdr_sdram_inst : entity work.sdr_sdram
         generic map(
-	    USE_AUTOMATIC_REFRESH => true,
-            ADDR_WIDTH         => ADDR_WIDTH,
-            DATA_WIDTH         => DATA_WIDTH,
-            BURST_LENGTH       => BURST_LENGTH,
-            SA_WIDTH           => SA_WIDTH,
-            CS_WIDTH           => CS_WIDTH,
-            CS_LOW_BIT         => CS_LOW_BIT,
-            BA_WIDTH           => BA_WIDTH,
-            BA_LOW_BIT         => BA_LOW_BIT,
-            ROW_WIDTH          => ROW_WIDTH,
-            ROW_LOW_BIT        => ROW_LOW_BIT,
-            COL_WIDTH          => COL_WIDTH,
-            COL_LOW_BIT        => COL_LOW_BIT,
-            tCLK               => tCLK,
-            tINIT_IDLE         => tINIT_IDLE,
-            INIT_REFRESH_COUNT => INIT_REFRESH_COUNT,
-            tCAC_CYCLES        => tCAC_CYCLES,
-            tRRD               => tRRD,
-            tRCD               => tRCD,
-            tRAS               => tRAS,
-            tRC                => tRC,
-            tRP                => tRP,
-            tCCD               => tCCD,
-            tDPL               => tDPL,
-            tDAL               => tDAL,
-            tRBD               => tRBD,
-            tWBD               => tWBD,
-            tPQL               => tPQL,
-            tQMD               => tQMD,
-            tDMD               => tDMD,
-            tMRD_CYCLES        => tMRD_CYCLES,
-            tREF               => tREF)
+            USE_AUTOMATIC_REFRESH => SDRAM_USE_AUTOMATIC_REFRESH,
+            BURST_LENGTH          => BURST_LENGTH,
+            SDRAM                 => SDRAM,
+            CS_WIDTH              => CS_WIDTH,
+            CS_LOW_BIT            => CS_LOW_BIT,
+            BA_LOW_BIT            => BA_LOW_BIT,
+            ROW_LOW_BIT           => ROW_LOW_BIT,
+            COL_LOW_BIT           => COL_LOW_BIT)
         port map(
-            rst                => rst,
-            clk                => dram_clk,
-            pll_locked         => pll_locked,
-            ocp_MCmd           => ocp_MCmd,
-            ocp_SFlag_CmdRefresh => '0',
-            ocp_MFlag_RefreshAccept => open,
-            ocp_MAddr          => ocp_MAddr,
-            ocp_SCmdAccept     => ocp_SCmdAccept,
-            ocp_MData          => ocp_MData,
-            ocp_MDataByteEn    => ocp_MDataByteEn,
-            ocp_MDataValid     => ocp_MDataValid,
-            ocp_MDataLast      => ocp_MDataLast,
-            ocp_SDataAccept    => ocp_SDataAccept,
-            ocp_SData          => ocp_SData,
-            ocp_SResp          => ocp_SResp,
-            ocp_SRespLast      => ocp_SRespLast,
-            sdram_CKE          => dram_CKE,
-            sdram_RAS_n        => dram_RAS_n,
-            sdram_CAS_n        => dram_CAS_n,
-            sdram_WE_n         => dram_WE_n,
-            sdram_CS_n         => dram_CS_n,
-            sdram_BA           => dram_BA,
-            sdram_SA           => dram_ADDR,
-            sdram_DQ           => dram_DQ,
-            sdram_DQM          => dram_DQM);
+            rst         => rst,
+            clk         => dram_clk,
+            pll_locked  => pll_locked,
+            ocpSlave    => ocpSlave,
+            ocpMaster   => ocpMaster,
+            sdram_CKE   => dram_CKE,
+            sdram_RAS_n => dram_RAS_n,
+            sdram_CAS_n => dram_CAS_n,
+            sdram_WE_n  => dram_WE_n,
+            sdram_CS_n  => dram_CS_n,
+            sdram_BA    => dram_BA,
+            sdram_SA    => dram_ADDR,
+            sdram_DQ    => dram_DQ,
+            sdram_DQM   => dram_DQM);
 
     -- CMD
-    ocp_MCmd_doRefresh  <= '0';
-    ocp_MCmd         <= '0' & (mtl_cmd_valid_i and (not mtl_cmd_read_i)) & (mtl_cmd_valid_i and mtl_cmd_read_i);
-    ocp_MAddr        <= mtl_cmd_addr_i;
-    mtl_cmd_accept_i <= ocp_SCmdAccept;
+    ocpMaster.MFlag_CmdRefresh <= '0';  -- Use automatic refresh
+    ocpMaster.MCmd             <= '0' & (mtl_cmd_valid_i and (not mtl_cmd_read_i)) & (mtl_cmd_valid_i and mtl_cmd_read_i);
+    ocpMaster.MAddr            <= mtl_cmd_addr_i;
+    mtl_cmd_accept_i           <= ocpSlave.SCmdAccept;
     assert (to_integer(unsigned(mtl_cmd_block_size_i)) + 1) = BURST_LENGTH or mtl_cmd_valid_i /= '1' report "Unsupported block size" severity failure;
     -- Write 
-    ocp_MData       <= mtl_wr_data_i;
-    ocp_MDataValid  <= mtl_wr_valid_i;
-    ocp_MDataLast   <= mtl_wr_last_i;
-    ocp_MDataByteEn <= mtl_wr_mask_i;
-    mtl_wr_accept_i <= ocp_SDataAccept;
+    ocpMaster.MData       <= mtl_wr_data_i;
+    -- ocpMaster.MDataValid  <= mtl_wr_valid_i;
+    -- ocpMaster.MDataLast   <= mtl_wr_last_i;
+    ocpMaster.MDataByteEn <= mtl_wr_mask_i;
+    mtl_wr_accept_i       <= ocpSlave.SDataAccept;
     -- Read 
-    mtl_rd_data_i   <= ocp_SData;
-    mtl_rd_valid_i  <= ocp_SResp;
-    mtl_rd_last_i   <= ocp_SRespLast;
+    mtl_rd_data_i         <= ocpSlave.SData;
+    mtl_rd_valid_i        <= ocpSlave.SResp;
+    mtl_rd_last_i         <= ocpSlave.SRespLast;
     assert mtl_rd_accept_i = '1' or mtl_rd_valid_i /= '1' report "Protocol error: wrapper not ready to accept data" severity failure;
-    -- ocp_MRespAccept not used
-    
+    -- ocpMaster.MRespAccept not used
+
     dram0_BA_0  <= dram_BA(0);
     dram0_BA_1  <= dram_BA(1);
     dram1_BA_0  <= dram_BA(0);
