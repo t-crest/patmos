d75e75db127a92e6cbb8133c29193c09f3445ecf
Martin Schoeberl <martin@jopdesign.com>
2013-03-15 15:29:29 +0100

    test: a start for testcase for Sahar's observed issue

---
+++ asm/pred_issue.s
@@ -0,0 +1,11 @@
+
+# Try to extract the issue Sahar observed,
+# but this works - al variations of r1/r2 lt, eq, gt tried
+	addi	r1 = r0, 2;
+	addi	r2 = r0, 1;
+	cmple   p4 = r1, r2;
+	addi    r15 = r0, 1;
+	addi    r16 = r0, 0;
+	xor     r16 = r15, r16; # r16 = 1
+(!p4)   nor     r16 = r16, r15;
+	halt;
