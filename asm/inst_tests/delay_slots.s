cdcda0ca91bc41f0441181741942ccabb8235499
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2013-03-19 07:09:47 +0100

    test: New test of delay slots added.

---
+++ asm/inst_tests/delay_slots.s
@@ -0,0 +1,23 @@
+# This test case tests the delay slots of branches and loads
+	nop 	0;
+	addi 	r1 = r0, 5;
+	addi	r2 = r0, r0;
+	br 		x1;
+	br 		x2;
+	br 		x3;
+	nop 	0;
+	nop 	0;
+x3:	add		r2 = r2, r1;
+x2:	add 	r2 = r2, r1;
+x1:	add 	r2 = r2, r1;
+	nop 	0;
+	nop		0;
+	br 		x4;
+	add 	r2 = r0, r0;
+	add 	r2 = r2, r1;
+	add 	r2 = r2, r1;
+	nop 	0;
+x4: lws		r4 = r1, 0;
+	add 	r5 = r4, r1;
+	add 	r5 = r4, r1;
+	halt;
