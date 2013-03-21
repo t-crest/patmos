45a6bcf7dbd7ad82155d79c670c75a2381a53df9
Martin Schoeberl <martin@jopdesign.com>
2013-03-21 23:56:00 +0100

    test: there are no rotate instructions anymore

--- testsuite/run.sh
+++ testsuite/run.sh
@@ -26,8 +26,8 @@ tests+=${test_disc}
 tests_c="hello_test"
 not_working="none"
 not_working_chsl="none"
-expect_fail=8
-expect_fail_chsl=5
+expect_fail=7
+expect_fail_chsl=4
 
 # How to implement timeout? IMPLEMENTED!
 # But does not work under OSX
