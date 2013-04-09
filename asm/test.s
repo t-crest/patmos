b42d861742d3158f5e7b17e830811553409517c0
Martin Schoeberl <martin@jopdesign.com>
2013-04-09 13:57:47 +0200

    test: fix UART address in simple test case

--- asm/test.s
+++ asm/test.s
@@ -7,8 +7,7 @@
 # TODO: looks like the UART is in memory address 0....
 
 	addi	r0 = r0, 0;  # first instruction not executed
-	addi	r1 = r0, 15;
-	sli	r1 = r1, 28;
+	add	r1 = r0, 0xf0000100;
 	addi	r2 = r0, 42; # '*'
 x1:	swl	[r1 + 1] = r2;
 	br	x1;
