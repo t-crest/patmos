3572a3da449273ee0498c1ab7efd0ccb937e0e20
Martin Schoeberl <martin@jopdesign.com>
2013-05-14 12:32:04 +0200

    test: update number of expected fails

--- testsuite/run.sh
+++ testsuite/run.sh
@@ -26,8 +26,8 @@ tests+=${test_disc}
 tests_c="hello_test"
 not_working="none"
 not_working_chsl="none"
-expect_fail=13
-expect_fail_chsl=14
+expect_fail=11
+expect_fail_chsl=10
 
 # How to implement timeout? IMPLEMENTED!
 # But does not work under OSX
