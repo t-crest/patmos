0d3cefd70e6dd4617823d576eed897574bc9e0cb
Sahar Abbaspour <Sahar Abbaspour>
2013-05-23 14:16:29 +0200

    chisel: UART: rx synced baud and clk as input

--- chisel/src/io/uart.scala
+++ chisel/src/io/uart.scala
@@ -1,7 +1,41 @@
 /*
-	TX module
+   Copyright 2013 Technical University of Denmark, DTU Compute. 
+   All rights reserved.
+   
+   This file is part of the time-predictable VLIW processor Patmos.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+      1. Redistributions of source code must retain the above copyright notice,
+         this list of conditions and the following disclaimer.
+
+      2. Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS
+   OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+   NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+   The views and conclusions contained in the software and documentation are
+   those of the authors and should not be interpreted as representing official
+   policies, either expressed or implied, of the copyright holder.
  */
 
+/*
+ * UART module in chisel.
+ * 
+ * Author: Sahar Abbaspour (sabb@dtu.com)
+ * 
+ */
 
 
 package hello
@@ -11,7 +45,7 @@ import Node._
 
 
 class UART() extends Component {
-  val io = new Bundle {
+	val io = new Bundle {
     val address = UFix(INPUT, 1)
 	val data_in = UFix(INPUT, 8)
 	val wr = UFix(INPUT, 1) 
@@ -19,9 +53,12 @@ class UART() extends Component {
 	val tx = UFix(OUTPUT, 1)
 	val rx = UFix(INPUT, 1)
 	val rd_data = UFix(OUTPUT, 8)
-//	val data_out_r = UFix(OUTPUT, 8)
+    val clk_freq = UFix(INPUT)
+    val baud_rate = UFix(INPUT)
   }
 
+  	val tx_divide = io.clk_freq / io.baud_rate
+  
   	val accept_reg = Reg(resetVal = UFix(0, 1))
   	val tx_reg = Reg(resetVal = UFix(1, 1))
   	val data_counter = Reg(resetVal = UFix(0, 4))
@@ -40,6 +77,7 @@ class UART() extends Component {
 	val s_baud_counter = Reg(resetVal = UFix(0, 10))
 	val s_baud_tick = Reg(resetVal = UFix(0, 1))
 	val data_buffer = Reg(resetVal = UFix(0, 8))	
+	val rxd_reg     = Reg(resetVal = UFix(1, 1))
 	val r_data = Reg(resetVal = UFix(0, 1))
 	val r_data_counter = Reg(resetVal = UFix(0, 3)) 	
   	val r_reset_state :: r_idle  :: r_receive_data :: r_stop_bit :: Nil  = Enum(4){ UFix() }
@@ -48,9 +86,11 @@ class UART() extends Component {
 	
 	io.rd_data := Mux(io.address === UFix(0), Cat(UFix(0, width = 6), r_data, accept_reg), data_buffer)
 	
+	
+	rxd_reg := io.rx;	
 //	io.rd_data := data_buffer
 	// Baud generator
-	when (baud_counter === (UFix(50000000) / UFix(115200)))
+	when (baud_counter === tx_divide)
 	{
 	  baud_counter := UFix(0)
 	  baud_tick := UFix(1)
@@ -78,7 +118,7 @@ class UART() extends Component {
 
     
     // TX state machine
-when (state === reset_state) {
+	when (state === reset_state) {
 		accept_reg	:= UFix(0)
 		tx_reg      := UFix(1)
 		state       := idle  
@@ -118,7 +158,7 @@ when (state === reset_state) {
 	}
 	.otherwise
 	{
-	  when (s_baud_counter === (UFix(50000000) / UFix(115200)))
+	  when (s_baud_counter === tx_divide)
 	  {
 	    s_baud_counter := UFix(0)
 	  }
@@ -138,7 +178,7 @@ when (state === reset_state) {
 	  	when (r_state === r_idle) {
 	  	   r_data := UFix(0)
 	  		when (s_baud_tick === UFix(1)){
-		  		when (io.rx === UFix(0))
+		  		when (rxd_reg === UFix(0))
 		  		{
 		  		  r_state := r_receive_data
 		  		}
@@ -149,7 +189,7 @@ when (state === reset_state) {
 
 		  		when (s_baud_tick === UFix(1)){
 		  		  
-		  			data_buffer := Cat(io.rx, data_buffer(7, 1))
+		  			data_buffer := Cat(rxd_reg, data_buffer(7, 1))
 		  			r_data_counter := Mux(r_data_counter === UFix(7), UFix(0), r_data_counter + UFix(1)) //
 		  	  	    r_state := Mux(r_data_counter === UFix(7), r_stop_bit, r_receive_data)
 		  		}
@@ -158,7 +198,7 @@ when (state === reset_state) {
 		  	{
 		  	 
 				when (s_baud_tick === UFix(1)){
-			  	 	when (io.rx === UFix(1))		
+			  	 	when (rxd_reg === UFix(1))		
 					{
 			  	 	  r_state := r_reset_state
 			  	 	  r_data := UFix(1)
