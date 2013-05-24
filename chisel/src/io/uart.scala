db493ad41bb601ab7e60e8ff4fe057f545029c71
Sahar Abbaspour <Sahar Abbaspour>
2013-05-24 17:18:16 +0200

    chisel: sync rx

--- chisel/src/io/uart.scala
+++ chisel/src/io/uart.scala
@@ -44,7 +44,7 @@ import Chisel._
 import Node._
 
 
-class UART() extends Component {
+class UART(clk_freq: UFix, baud_rate: UFix) extends Component {
 	val io = new Bundle {
     val address = UFix(INPUT, 1)
 	val data_in = UFix(INPUT, 8)
@@ -53,11 +53,9 @@ class UART() extends Component {
 	val tx = UFix(OUTPUT, 1)
 	val rx = UFix(INPUT, 1)
 	val rd_data = UFix(OUTPUT, 8)
-    val clk_freq = UFix(INPUT)
-    val baud_rate = UFix(INPUT)
   }
 
-  	val tx_divide = io.clk_freq / io.baud_rate
+  	val tx_divide = clk_freq / baud_rate
   
   	val accept_reg = Reg(resetVal = UFix(0, 1))
   	val tx_reg = Reg(resetVal = UFix(1, 1))
@@ -77,17 +75,24 @@ class UART() extends Component {
 	val s_baud_counter = Reg(resetVal = UFix(0, 10))
 	val s_baud_tick = Reg(resetVal = UFix(0, 1))
 	val data_buffer = Reg(resetVal = UFix(0, 8))	
-	val rxd_reg     = Reg(resetVal = UFix(1, 1))
+	val rxd_reg0     = Reg(resetVal = UFix(1, 1))
+	val rxd_reg1     = Reg(resetVal = UFix(1, 1))
+	val rxd_reg2     = Reg(resetVal = UFix(1, 1))
+//	val test     = Reg(resetVal = UFix(1, 1))
 	val r_data = Reg(resetVal = UFix(0, 1))
 	val r_data_counter = Reg(resetVal = UFix(0, 3)) 	
-  	val r_reset_state :: r_idle  :: r_receive_data :: r_stop_bit :: Nil  = Enum(4){ UFix() }
+  	val r_reset_state :: r_idle  :: r_receive_half :: r_receive_data :: r_stop_bit :: Nil  = Enum(5){ UFix() }
 	val r_state = Reg(resetVal = r_reset_state)
+	val rx_clk_ena = Reg(resetVal = UFix(0, 1))
+	val count_wait = Reg(resetVal = UFix(0, 2))
 	//
 	
 	io.rd_data := Mux(io.address === UFix(0), Cat(UFix(0, width = 6), r_data, accept_reg), data_buffer)
 	
 	
-	rxd_reg := io.rx;	
+	rxd_reg0 := io.rx;
+	rxd_reg1 := rxd_reg0;
+	rxd_reg2 := rxd_reg1
 //	io.rd_data := data_buffer
 	// Baud generator
 	when (baud_counter === tx_divide)
@@ -151,7 +156,7 @@ class UART() extends Component {
 	  	}
   	
 	// high speed baud counter
-	when (s_baud_counter === Bits("b0011011001"))//(UFix(50000000) / UFix(115200)) / UFix(2))
+	when (s_baud_counter === (UFix(50000000) / UFix(115200)) / UFix(2))
 	{	 
 	  s_baud_tick := UFix(1)
 	  s_baud_counter := s_baud_counter + UFix(1)  
@@ -164,10 +169,13 @@ class UART() extends Component {
 	  }
 	  .otherwise 
 	  {
-	    s_baud_counter := s_baud_counter + UFix(1)
+	    when (rx_clk_ena === UFix(1)){
+	    	s_baud_counter := s_baud_counter + UFix(1)
+	    }
 	  }
 	  s_baud_tick := UFix(0)
 	}
+	//
 	
 		// RX state machine
 		
@@ -177,31 +185,44 @@ class UART() extends Component {
 		}
 	  	when (r_state === r_idle) {
 	  	   r_data := UFix(0)
-	  		when (s_baud_tick === UFix(1)){
-		  		when (rxd_reg === UFix(0))
+//	  		when (s_baud_tick === UFix(1)){
+		  		when (rxd_reg2 === UFix(0))
 		  		{
-		  		  r_state := r_receive_data
+		  		  r_state := r_receive_half
+		  		  rx_clk_ena := UFix(1);
 		  		}
-	  		}
+	  	   		.otherwise {rx_clk_ena := UFix(0)}
+//	  		}
 	  	}
+	  	
+	  	when (r_state === r_receive_half){
+	  	  when (s_baud_tick === UFix(1)){
+	  	    count_wait := count_wait + UFix(1) // drop the first rx_clk ...
+	  	    when (count_wait === UFix(1)){
+	  		  r_state := r_receive_data
+	  	    }
 
-		  	when (r_state === r_receive_data) {
+	  	  }
+	  	}
+		
+	  	when (r_state === r_receive_data) {
 
 		  		when (s_baud_tick === UFix(1)){
 		  		  
-		  			data_buffer := Cat(rxd_reg, data_buffer(7, 1))
+		  			data_buffer := Cat(rxd_reg2, data_buffer(7, 1))
 		  			r_data_counter := Mux(r_data_counter === UFix(7), UFix(0), r_data_counter + UFix(1)) //
 		  	  	    r_state := Mux(r_data_counter === UFix(7), r_stop_bit, r_receive_data)
 		  		}
 		  	}
-		  	when (r_state === r_stop_bit) 
+		when (r_state === r_stop_bit) 
 		  	{
 		  	 
 				when (s_baud_tick === UFix(1)){
-			  	 	when (rxd_reg === UFix(1))		
+			  	 	when (rxd_reg2 === UFix(1))		
 					{
 			  	 	  r_state := r_reset_state
 			  	 	  r_data := UFix(1)
+			  	 	  rx_clk_ena := UFix(0)
 					}
 			  	 	.otherwise
 			  	 	{
@@ -213,6 +234,3 @@ class UART() extends Component {
 	io.tx := tx_reg
  //   io.data_out_r := data_buffer
 }
-  
-
-
