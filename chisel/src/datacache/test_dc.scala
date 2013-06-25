d970fd65369524ed789a8e074b4f3f161b405edd
Sahar Abbaspour <Sahar Abbaspour>
2013-06-25 18:53:47 +0200

    chisel_dc: 4 way data cache

---
+++ chisel/src/datacache/test_dc.scala
@@ -0,0 +1,68 @@
+/*
+   Copyright 2013 Technical University of Denmark, DTU Compute. 
+   All rights reserved.
+   
+   This file is part of the time-predictable VLIW processor Patmos.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+      1. Redistributions of source code must retain the above copyright notice,
+         this list of conditions and the following disclaimer.
+
+      2. Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS
+   OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+   NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+   The views and conclusions contained in the software and documentation are
+   those of the authors and should not be interpreted as representing official
+   policies, either expressed or implied, of the copyright holder.
+ */
+
+/*
+ * Data cache test
+ * 
+ * Author: Sahar Abbaspour (sabb@dtu.dk)
+ * 
+ */
+
+package dc
+
+import Chisel._
+import Node._
+
+
+class Test() extends Component {
+  val io = new Bundle {
+	val led = UFix(OUTPUT, 1) 
+  }
+  
+	val dc_ 		= new DC_4_way(4, 1024, 32)
+	
+	//dc_.io.mem_data_in := Bits(0)
+	io.led 			:= UFix(1)
+	
+}
+  
+
+
+
+// Generate the Verlog code by invoking chiselMain() in our main()
+object HelloMain {
+  def main(args: Array[String]): Unit = { 
+    chiselMain( args, () => new Test())
+  }
+}
+
+
