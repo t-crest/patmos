3a0ab291968c71b01a6482ea667c59fb51e10cfb
Sahar Abbaspour <Sahar Abbaspour>
2013-07-03 21:33:39 +0200

    chiselio: test uart

--- chisel/src/io/test_read.scala
+++ chisel/src/io/test_read.scala
@@ -59,27 +59,21 @@ class Test() extends Component {
   	val wr = Reg(resetVal = UFix(0, 1))
   	val rd = Reg(resetVal = UFix(0, 1))
  	val wr_data = Reg(resetVal = UFix(0, 8))
-// 	val rd_data = Reg(resetVal = UFix(0, 8))
+ 	val data_in	= Reg(resetVal = UFix(0, 8))
+ //	val rd_data = Reg(resetVal = UFix(0, 8))
   	val address = Reg(resetVal = UFix(0, 1))
   	val r_read :: r_wait_st :: read_st :: data_read_st :: wait_st :: write_st:: Nil  = Enum(6){ UFix() } 
 	val state = Reg(resetVal = r_read)
 
-  	val uart = new UART(UFix(50000000), UFix(115200))
- // 	val rx = new RX()
+  	val uart = new UART(50000000, 115200)
   	
   	
- 
-// 	val wr_data = Mux(blk === UFix(0), UFix(40), UFix(41))
  	
 	uart.io.tx <> io.tx
-	uart.io.rx := io.rx //blk //
+	uart.io.rx :=  io.rx //blk //
 //	rx.io.rx <> io.rx
 
 
-//	rx.io.rx  <> io.rx //:=blk//
-//	when (uart.io.rd_data === UFix(49)){
-//	  blk := UFix(1)
-//	}
 	
  	r1 := r1 + UFix(1)
 	when (r1 === CNT_MAX) {
@@ -88,16 +82,18 @@ class Test() extends Component {
   		
 	}
 	
+	
 	//************//
 	when (state === r_read) {
-	  wr 		:= UFix(0)
+//	  wr 		:= UFix(0)
+//	  rd		:= UFix(0)
 	  address 	:= UFix(0)
 	  state		:= r_wait_st
 	}
 	
-	when (state === r_wait_st) {
+	when (state === r_wait_st) { // read the status of read
 		when (uart.io.rd_data(1) === UFix(1)){
-		    address 	:= UFix(1) // read data from uart
+		    address 	:= UFix(1) // read the data
 		  	state := data_read_st
 		  	blk := UFix(1)
 		}
@@ -106,24 +102,26 @@ class Test() extends Component {
 		}
 	}
 	when (state === data_read_st){
-	  wr_data :=  uart.io.rd_data //uart.io.data_out_r//wr_data //rx.io.data_out//
+	  wr_data :=  uart.io.rd_data // read the data from uart
+	  rd	:= UFix(1)
 	  state   := read_st
 	}
 	//************//
 	when (state === read_st) {
 	  wr 		:= UFix(0)
 //	  wr_data 	:= UFix(0)
-	  rd 		:= UFix(1)
+	  rd 		:= UFix(0)
 	  address 	:= UFix(0)
 	  state		:= wait_st
 	}
 	
-	when (state === wait_st){
+	when (state === wait_st){ // read the status of write
 	  when (uart.io.rd_data(0) === UFix(1)){
 	  	state := write_st
-	  	rd 	  := UFix(0)
+//	  	rd 	  := UFix(0)
 	  	wr 		:= UFix(1)
 	    address 	:= UFix(1)
+	    data_in		:= wr_data
 	  }
 	  .otherwise {
 	  	state := r_read
@@ -139,9 +137,9 @@ class Test() extends Component {
 	
 	uart.io.address := address
   	io.led := blk;
-	uart.io.data_in := wr_data  
+	uart.io.data_in := data_in //wr_data  
   	//rd_data := uart.io.rd_data
-	uart.io.wr := wr//UFix(1); 
+	uart.io.wr := wr//UFix(1); //
 	uart.io.rd := rd
 	
 }
