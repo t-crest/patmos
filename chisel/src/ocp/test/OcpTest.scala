da2ba45782300b7373722909bdbf56ffca7a03a2
Martin Schoeberl <martin@jopdesign.com>
2013-09-05 21:27:03 +0200

    ocp: start with some testing for the OCP merge unit

--- chisel/src/ocp/test/OcpTest.scala
+++ chisel/src/ocp/test/OcpTest.scala
@@ -31,80 +31,32 @@
  */
 
 /*
- * Just a minimal 'hello world' Chisel example.
+ * Start for OCP tests.
  * 
  * Author: Martin Schoeberl (martin@jopdesign.com)
  * 
  */
 
-package example
+package ocp.test
 
 import Chisel._
 import Node._
 
+import ocp._
+
 import scala.collection.mutable.HashMap
 
-class FsmContainer() extends Component {
+class AComponent() extends Component {
   val io = new Bundle {
-    val led = Bits(OUTPUT, 8)
+    val fromMaster = new OcpBurstMasterPort(32, 32, 4)
+    val toSlave = new OcpBurstSlavePort(32, 32, 4)
   }
   
-  var tck = new Tick();
-  
-  val led = Reg(resetVal = Bits(1, 8))
-  val led_next = Cat(led(6, 0), led(7))
-  
-  when (tck.io.tick === Bits(1)) {
-    led := led_next
-  }
-  io.led := ~led
+  io.fromMaster <> io.toSlave
 }
 
-/**
- * Generate a 2 Hz tick to drive the FSM input test bench.
- */
-class Tick() extends Component {
-  val io = new Bundle {
-    val tick = Bits(OUTPUT, 1)
-  }
-  // BeMicro has a 16 MHz clock
-  val CNT_MAX = UFix(16000000/2-1)
-  
-  val r1 = Reg(resetVal = UFix(0, 25))
-  
-  val limit = r1 === CNT_MAX
-  val tick = limit
-  
-  r1 := r1 + UFix(1)
-  when (limit) {
-    r1 := UFix(0)
-  }
-  
-  io.tick := tick
 
-  // **** uninteresting stuff below ****
-  
-// the following does not work - example in package error
-//  val ticka = when(limit) { Bits(0) } .otherwise { Bits(1) }
-//  val tickb = when(limit) { Bits(1) } .otherwise { Bits(0) }
-  
-// this is a very verbose version
-//  val ticka = Bits(width=1)
-//  ticka := Bits(0)
-//  when (limit) { ticka := Bits(0, 1) } .otherwise{ ticka := Bits(1, 1) }
-//  val tickb = Bits(width=1)
-//  tickb := Bits(0)
-//  when (limit) { tickb := Bits(1, 1) } .otherwise{ tickb := Bits(0, 1) }
-    
-  // That's the MUX version. Do we know which is which? True first? 0 first? 
-  // val ticka = Mux(limit, Bits(1), Bits(0))
-  
-  // no if in Chisel
-  // val ticka = if (limit) then Bits(1) else Bits(0)
-
-}
-
-class FsmTest(fsm: FsmContainer) extends Tester(fsm, Array(fsm.io)) {
+class OcpTester(fsm: AComponent) extends Tester(fsm, Array(fsm.io)) {
   defTests {
     val ret = true
     val vars = new HashMap[Node, Node]()
@@ -116,22 +68,16 @@ class FsmTest(fsm: FsmContainer) extends Tester(fsm, Array(fsm.io)) {
       println("iter: "+i)
       println("ovars: "+ovars)
       // where does the 'c' come from? Why does this work?
-      println("led/litVal "+ovars(c.io.led).litValue())
-//      println("led/litVal "+ovars(io.led).litValue())
-      println("led/litVal "+ovars(fsm.io.led).litValue())
-      // the following does not compile
-//      println("led/litVal "+ovars(xyz.io.led).litValue())
+//      println("led/litVal "+ovars(c.io.led).litValue())
     }
     ret
   }
 }
 
-object FsmMain {
+object TestMain {
   def main(args: Array[String]): Unit = {
-    // chiselMain(args, () => new FsmContainer());
-    // Looks like the MainTest is also fine for Verilog code generation
-    chiselMainTest(args, () => new FsmContainer()) {
-      f => new FsmTest(f)
+    chiselMainTest(args, () => new AComponent()) {
+      f => new OcpTester(f)
     }
 
   }
