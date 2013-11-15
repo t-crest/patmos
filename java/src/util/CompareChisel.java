59650c47e865f5351b90aab41edb93c3aae1dd2e
Wolfgang Puffitsch <hausen@gmx.at>
2013-11-15 10:54:10 +0100

    tools: Print line where difference occurs when comparing simulator and emulator traces.

--- java/src/util/CompareChisel.java
+++ java/src/util/CompareChisel.java
@@ -58,9 +58,12 @@ public class CompareChisel {
 		Scanner hs = new Scanner(new FileInputStream(args[0]));
 		Scanner chisel = new Scanner(new FileInputStream(args[1]));
 
+		int line = 1;
+
 		// Read till the Chisel C based simulation provides useful output
 		while (chisel.hasNextLine()) {
 			String s = chisel.nextLine();
+			line++;
 			if (s.startsWith("Patmos start")) {
 				break;
 			}
@@ -128,7 +131,8 @@ public class CompareChisel {
 				} else {
 					++cntNoChange;
 					if (chisel.hasNextLine()) {
-						chisel.nextLine();						
+						chisel.nextLine();
+						line++;
 					} else {
 						break outerloop;
 					}
@@ -174,14 +178,15 @@ public class CompareChisel {
 			}
 			for (int i = 0; i < 32; ++i) {
 				if (csReg[i] != hsReg[i]) {
-					System.out.println("Difference at PC: " + pc);
+					System.out.println("Difference at PC: " + pc + " line: " + line);
 					System.out.println("Register " + i + " Chisel: " + csReg[i]
 							+ " patsim: " + hsReg[i]);
 					System.exit(1);
 				}
 			}
 			if (chisel.hasNextLine()) {
-				chisel.nextLine();						
+				chisel.nextLine();
+				line++;
 			} else {
 				break;
 			}
