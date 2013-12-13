6c3b4b1b7e0274368a71a7773736f3db88350ab2
Stefan Hepp <stefan@stefant.org>
2013-12-13 18:06:43 +0100

    simulator: Check if the stack frame size is a multiple of the stack block size

--- simulator/src/stack-cache.cc
+++ simulator/src/stack-cache.cc
@@ -256,6 +256,10 @@ word_t block_stack_cache_t::prepare_reserve(uword_t size,
     simulation_exception_t::stack_exceeded("Reserving more blocks than"
       "the number of blocks in the stack cache");
   }
+  if (size_blocks * Num_block_bytes != size) {
+    simulation_exception_t::stack_exceeded("Reserving a frame size that is not "
+      "a multiple of the stack block size.");
+  }
 
   if (stack_top < size_blocks * Num_block_bytes) {
     simulation_exception_t::stack_exceeded("Stack top pointer decreased beyond "
@@ -362,6 +366,10 @@ word_t block_stack_cache_t::prepare_free(uword_t size,
     simulation_exception_t::stack_exceeded("Freeing more blocks than"
       " the number of blocks in the stack cache");
   }
+  if (size_blocks * Num_block_bytes != size) {
+    simulation_exception_t::stack_exceeded("Freeing a frame size that is not "
+      "a multiple of the stack block size.");
+  }
 
   // also free space in memory?
   if (freed_spilled_blocks)
@@ -411,6 +419,11 @@ word_t block_stack_cache_t::prepare_ensure(uword_t size,
     simulation_exception_t::stack_exceeded("Ensuring more blocks than"
         " the number of blocks in the stack cache");
   }
+  if (size_blocks * Num_block_bytes != size) {
+    simulation_exception_t::stack_exceeded("Ensuring a frame size that is not "
+      "a multiple of the stack block size.");
+  }
+
 
   uword_t transfer_blocks = 0;
   
@@ -480,6 +493,12 @@ word_t block_stack_cache_t::prepare_spill(uword_t size,
 
   uword_t transfer_blocks = size_blocks;
 
+  if (size_blocks * Num_block_bytes != size) {
+    simulation_exception_t::stack_exceeded("Spilling a frame size that is not "
+      "a multiple of the stack block size.");
+  }
+
+  
   // update the stack top pointer of the processor 
   stack_spill -= transfer_blocks * Num_block_bytes;
 
