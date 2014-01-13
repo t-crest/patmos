989dfd07ca8c25ffd5f0fdcd1c198281670d8347
Wolfgang Puffitsch <hausen@gmx.at>
2014-01-13 18:31:15 +0100

    bootloader: Start a boot loader for CMPs.

---
+++ c/cmpboot.h
@@ -0,0 +1,70 @@
+/*
+   Copyright 2014 Technical University of Denmark, DTU Compute. 
+   All rights reserved.
+   
+   This file is part of the time-predictable VLIW processor Patmos.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+      1. Redistributions of source code must retain the above copyright notice,
+         this list of conditions and the following disclaimer.
+
+      2. Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS
+   OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+   NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+   The views and conclusions contained in the software and documentation are
+   those of the authors and should not be interpreted as representing official
+   policies, either expressed or implied, of the copyright holder.
+ */
+
+/*
+ * Definitions for CMP boot loaders.
+ * 
+ * Author: Wolfgang Puffitsch (wpuffitsch@gmail.com)
+ *
+ */
+
+#ifndef _CMPBOOT_H_
+#define _CMPBOOT_H_
+
+#include <machine/patmos.h>
+
+#define MAX_CORES 64
+#define core_id *((volatile _IODEV int *) 0xF0000000)
+
+#define STATUS_NULL     0
+#define STATUS_DOWNLOAD 1
+#define STATUS_START    2
+
+struct master_info_t {  
+  volatile entrypoint_t entrypoint;
+  volatile int status;
+};
+
+struct slave_info_t {
+  volatile int status;
+};
+
+struct boot_info_t {
+  struct master_info_t master;
+  struct slave_info_t slave[MAX_CORES];
+};
+
+/* Place boot info at the beginning of the memory. Nothing else may be
+   placed there. */
+#define boot_info ((_UNCACHED struct boot_info_t *)0x00000010)
+
+#endif /* _CMPBOOT_H_ */
---
+++ c/cmpmaster.c
@@ -0,0 +1,111 @@
+/*
+   Copyright 2014 Technical University of Denmark, DTU Compute. 
+   All rights reserved.
+   
+   This file is part of the time-predictable VLIW processor Patmos.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+      1. Redistributions of source code must retain the above copyright notice,
+         this list of conditions and the following disclaimer.
+
+      2. Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS
+   OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+   NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+   The views and conclusions contained in the software and documentation are
+   those of the authors and should not be interpreted as representing official
+   policies, either expressed or implied, of the copyright holder.
+ */
+
+/*
+ * Master for CMP boot loader.
+ * 
+ * Author: Wolfgang Puffitsch (wpuffitsch@gmail.com)
+ *
+ */
+
+#include "boot.h"
+#include "cmpboot.h"
+
+int main(void)
+{
+  // setup stack frame and stack cache.
+  asm volatile ("mov $r29 = %0;" // initialize shadow stack pointer"
+                "mts $ss  = %1;" // initialize the stack cache's spill pointer"
+                "mts $st  = %1;" // initialize the stack cache's top pointer"
+                "li $r30 = %2;" // initialize return base"
+                : : "r" (_shadow_stack_base-16),
+                  "r" (_stack_cache_base-16),
+                  "i" (&main));
+
+  unsigned i;
+
+  WRITE("BOOT\n", 5);
+
+  // overwrite potential leftovers from previous runs
+  boot_info->master.status = STATUS_NULL;
+  boot_info->master.entrypoint = NULL;
+  for (i = 0; i < MAX_CORES; i++) {
+    boot_info->slave[i].status = STATUS_NULL;
+  }
+
+  // give the slaves some time to boot
+  for (i = 0; i < 0x10; i++) {
+    boot_info->master.status = STATUS_DOWNLOAD;
+  }
+
+  WRITE("DOWN\n", 5);
+
+  // download application
+  boot_info->master.entrypoint = download();
+
+  // notify slaves that they can call _start()
+  boot_info->master.status = STATUS_START;
+
+  WRITE("START\n", 5);
+    
+  // call the application's _start()
+  int retval = -1;
+  if (boot_info->master.entrypoint != 0) {
+    retval = (*boot_info->master.entrypoint)();
+
+    // Compensate off-by-one of return offset with NOP
+    // (internal base address is 0 after booting).
+    // Return may be "unclean" and leave registers clobbered.
+    asm volatile ("nop" : :
+                  : "$r2", "$r3", "$r4", "$r5",
+                    "$r6", "$r7", "$r8", "$r9",
+                    "$r10", "$r11", "$r12", "$r13",
+                    "$r14", "$r15", "$r16", "$r17",
+                    "$r18", "$r19", "$r20", "$r21",
+                    "$r22", "$r23", "$r24", "$r25",
+                    "$r26", "$r27", "$r28", "$r29");
+  }
+
+  // TODO: wait for slaves to finish
+
+  // Print exit magic and return code
+  static char msg[10];
+  msg[0] = '\0';
+  msg[1] = 'x';
+  msg[2] = retval & 0xff;
+  WRITE(msg, 3);
+
+  // loop back, TODO: replace with a real reset
+  main();
+
+  return 0;
+}
