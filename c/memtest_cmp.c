57cb44523b275a53faa2aa3d159db80d09eba75b
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2014-03-05 21:19:42 +0100

    c: memtest_cmp.c changed to use stdio.h

--- c/memtest_cmp.c
+++ c/memtest_cmp.c
@@ -2,57 +2,68 @@
 	Author: Rasmus Bo Soerensen (rasmus@rbscloud.dk)
 	Copyright: DTU, BSD License
 */
+#include <stdio.h>
 #include <machine/spm.h>
-#include "patio.h"
+#include <machine/patmos.h>
 
-#define MEM_OFFSET 0x300000
 #define MEM_TEST 2000000
 
+extern char _end;
+#define MEM ((volatile _UNCACHED int *) &_end)
+
 int main() {
-	int i, j, k;
+	int res;
 	int error = 0;
 	int test = 0;
 
-	if (CORE_ID <=3){
-		for (i=MEM_OFFSET; i<=MEM_OFFSET+MEM_TEST; i++){ // Read from main memory
-			j = *(MEM+i);
-			if (j == 0){	// If data is not what we expect write error
-				error = error;
-			} else {
+	if (get_cpuid() == 0){
+		for (int i=0; i<=MEM_TEST; i++){ // Read from main memory
+			res = *(MEM+i);
+			if (res != 0){	// If data is not what we expect write error
 				error++;
 			}
 		}
 		if (error != 0){
-			WRITE("MEMORY uninitialized\n",21);
+			puts("MEMORY uninitialized\n");
 		}
 		error = 0;
 
-		for (k = 0; k < 10; k++) { // Test 10 times
-			for (i=MEM_OFFSET; i<=MEM_OFFSET+MEM_TEST; i++) // Write to main memory
+		for (int k = 0; k < 10; k++) { // Test 10 times
+			for (int i=0; i<=MEM_TEST; i++) // Write to main memory
 				*(MEM+i) = i;
 
-			for (i=MEM_OFFSET; i<=MEM_OFFSET+MEM_TEST; i++){ // Read from main memory
-				j = *(MEM+i);
-				if (j != i){	// If data is not what we expect write error
-					WRITE("e",1);
+			for (int i=0; i<=MEM_TEST; i++){ // Read from main memory
+				res = *(MEM+i);
+				if (res != i){	// If data is not what we expect write error
+					puts("e");
 					error++;
 				}
 			}
 			if (error != 0){
 				test++;
-				WRITE("\n",1);
+				puts("\n");
 			}
 			error = 0;
 		}
 		if (test != 0){
-			WRITE("Errors\n",7);
+			puts("Errors\n");
 		} else {
-			WRITE("Success\n",8);
+			puts("Success\n");
 		}
 
 	} else {
-		for(;;);
+		for (int k = 0; k < 100; ++k)
+		{
+			for (int i=0; i<=MEM_TEST; i++){ // Read from main memory
+				res = *(MEM+i);
+				if (res == 0){	// If data is not what we expect write error
+					error = error;
+				} else {
+					error++;
+				}
+			}
+		}
 	}
 
-
+	return 0;
 }
