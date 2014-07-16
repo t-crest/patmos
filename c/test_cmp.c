0c32c68fd67f6427c1a69ab3b870a8f01e44f9b4
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2014-07-16 10:51:21 +0200

    C: test_cmp.c added, finding the size of the communication scratch pad and main memory.

---
+++ c/test_cmp.c
@@ -0,0 +1,143 @@
+/*
+	Author: Rasmus Bo Soerensen (rasmus@rbscloud.dk)
+	Copyright: DTU, BSD License
+*/
+const int NOC_MASTER = 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <machine/patmos.h>
+#include <machine/exceptions.h>
+#include "libnoc/noc.h"
+
+#define MINADDR (512*1024)
+#define TEST_START ((volatile _UNCACHED unsigned int *) MINADDR + 0)
+
+#define ABORT_IF_FAIL(X,Y) if (X<0){puts(Y); abort();}
+
+void prefix(int size, char* buf){
+	int pref = 0;
+	while (size > 1024){
+		size = size >> 10;
+		pref++;
+	}
+	char* format;
+	switch(pref){
+		case 0:
+			format = "%d B";
+			break;
+		case 1:
+			format = "%d KB";
+			break;
+		case 2:
+			format = "%d MB";
+			break;
+		case 3:
+			format = "%d GB";
+			break;
+	}
+	snprintf(buf,11,format,size);
+	return;
+}
+
+int mem_area_test_uncached(volatile _UNCACHED unsigned int * mem_addr,int mem_size) {
+	int i,tmp;
+	for(i = 0; i < mem_size; i++){
+		*(mem_addr+i) = 0;
+		if(*(mem_addr+i) != 0){return -1;}
+		*(mem_addr+i) = i;
+		if(*(mem_addr+i) != i){return -1;}
+	}
+	return 0;
+}
+
+int mem_area_test_spm(volatile int _SPM * mem_addr,int mem_size) {
+	int i,tmp;
+	for(i = 0; i < mem_size; i++){
+		*(mem_addr+i) = 0;
+		if(*(mem_addr+i) != 0){return -1;}
+		*(mem_addr+i) = i;
+		if(*(mem_addr+i) != i){return -1;}
+	}
+	return 0;
+}
+
+int test_mem_size_uncached(volatile _UNCACHED unsigned int * mem_addr){
+	*(mem_addr) = 0xFFEEDDCC;
+	int i = 2;
+	int j = 0;
+	for(j = 0; j < 32; j++) {
+		*(mem_addr+i) = 0;
+		if (*(mem_addr) == 0) {
+			return i*4;
+		}
+		i = i << 1;
+		if (*(mem_addr) != 0xFFEEDDCC){
+			return -1;
+		}
+	}
+	return -1;
+}
+
+int test_mem_size_spm(volatile int _SPM * mem_addr){
+	*(mem_addr) = 0xFFEEDDCC;
+	int i = 2;
+	int j = 0;
+	for(j = 0; j < 32; j++) {
+		*(mem_addr+i) = 0;
+		if (*(mem_addr) == 0) {
+			return i*4;
+		}
+		i = i << 1;
+		if (*(mem_addr) != 0xFFEEDDCC){
+			return -1;
+		}
+	}
+	return -1;
+}
+
+int main_mem_test() {
+	fputs("Testing MAINMEM...",stdout);
+	ABORT_IF_FAIL(mem_area_test_uncached(TEST_START,0x1000),"FAIL");
+	puts("OK");
+	fputs("Testing MAINMEM size...",stdout);
+	int size = 0;
+	size = test_mem_size_uncached(TEST_START);
+	ABORT_IF_FAIL(size,"Size could not be retrieved");
+	char buf[11];
+	prefix(size,buf);
+	puts(buf);
+	return 0;
+}
+
+int com_spm_test() {
+	fputs("Testing COM SPM...",stdout);
+	ABORT_IF_FAIL(mem_area_test_spm(NOC_SPM_BASE,0x8000),"FAIL 0x8000");
+	ABORT_IF_FAIL(mem_area_test_spm(NOC_SPM_BASE,0x8001),"FAIL 0x8001");
+	ABORT_IF_FAIL(mem_area_test_spm(NOC_SPM_BASE,0x80001),"FAIL 0x80001");
+	puts("OK");
+	fputs("Testing COM SPM size...",stdout);	
+	fflush(stdout);
+	int size = 0;
+	size = test_mem_size_spm(NOC_SPM_BASE);
+	ABORT_IF_FAIL(size,"Size could not be retrieved");
+	char buf[11];
+	prefix(size,buf);
+	puts(buf);
+	return 0;
+}
+
+int main() {
+  if (get_cpuid() == 0) {
+		puts("Core 0 running.");
+		com_spm_test();
+		main_mem_test();
+    return 0;
+
+  } else {
+    // other cores do idle loop
+    for (;;) { }
+  }
+  return -1;
+}
+
