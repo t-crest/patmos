0087bbf11ccd3161497f42ab326fcf506564ad08
Carsten Nielsen <clauniel@clauniel.com>
2015-06-01 19:10:33 +0200

    Add a direct-mapped write back data cache with write-allocation.
    In order for the cache to work, the writemaster in DataCache.scala
    must be set to OcpCmd.IDLE when using the data cache by changing
    
        wc.io.writeMaster.M.Cmd := Mux(!selSC, io.master.M.Cmd, OcpCmd.IDLE)
    to
    
        wc.io.writeMaster.M.Cmd := Mux(!selSC && !selDC, io.master.M.Cmd, OcpCmd.IDLE)
    
    In DataCache.scala.

---
+++ hardware/src/datacache/DirectMappedCacheWriteBack.scala
@@ -0,0 +1,269 @@
+/*
+   Copyright 2013 Technical University of Denmark, DTU Compute.
+   All rights reserved.
+
+   This file is part of the time-predictable VLIW processor Patmos.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+      1. Redistributions of source code must retain the above copyright notice,
+         this list of conditions and the following disclaimer.
+
+      2. Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS
+   OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+   NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+   The views and conclusions contained in the software and documentation are
+   those of the authors and should not be interpreted as representing official
+   policies, either expressed or implied, of the copyright holder.
+ */
+
+/*
+ * A direct-mapped cache
+ *
+ * Authors: Martin Schoeberl (martin@jopdesign.com)
+ *          Wolfgang Puffitsch (wpuffitsch@gmail.com)
+ */
+
+package datacache
+
+import Chisel._
+import Node._
+
+import patmos.Constants._
+import patmos.DataCachePerf
+import patmos.MemBlock
+import patmos.MemBlockIO
+
+import ocp._
+
+class DirectMappedCacheWriteBack(size: Int, lineSize: Int) extends Module {
+  val io = new Bundle {
+    val master = new OcpCoreSlavePort(EXTMEM_ADDR_WIDTH, DATA_WIDTH)
+    val slave = new OcpBurstMasterPort(EXTMEM_ADDR_WIDTH, DATA_WIDTH, lineSize/4)
+    val invalidate = Bool(INPUT)
+    val perf = new DataCachePerf()
+  }
+
+
+  val addrBits = log2Up(size / BYTES_PER_WORD)
+  val lineBits = log2Up(lineSize)
+  val dataWidth = io.master.M.Data.getWidth()
+  val burstLength = io.slave.burstLength
+  val burstAddrBits = log2Up(burstLength)
+  val byteAddrBits = log2Up(dataWidth/8)
+  val addrWidth = io.master.M.Addr.getWidth()
+
+  val doingRead = Reg(Bool()) // are we reading or writing
+  val coreWrDataReg = Reg(Bits(width = DATA_WIDTH)) // register for WR cmd data
+
+  val tagWidth = EXTMEM_ADDR_WIDTH - addrBits - 2
+  val tagCount = size / lineSize
+
+  // Register signals from master
+  val masterReg = Reg(io.master.M)
+  masterReg := io.master.M
+
+  // State machine for misses
+  val idle :: write :: writeWaitForDVA :: hold :: fill :: respond :: Nil = Enum(UInt(), 6)
+  val stateReg = Reg(init = idle)
+
+  val burstCntReg = Reg(init = UInt(0, lineBits-2))
+  val missIndexReg = Reg(UInt(lineBits-2))
+
+  // Generate memories
+  val tagMem = MemBlock(tagCount, tagWidth)
+  val tagVMem = Vec.fill(tagCount) { Reg(init = Bool(false)) }
+  val dirtyMem = Vec.fill(tagCount) { Reg(init = Bool(false)) }
+  val mem = new Array[MemBlockIO](BYTES_PER_WORD)
+  for (i <- 0 until BYTES_PER_WORD) {
+    mem(i) = MemBlock(size / BYTES_PER_WORD, BYTE_WIDTH).io
+  }
+
+  val tag = tagMem.io(io.master.M.Addr(addrBits + 1, lineBits))
+  val tagV = Reg(next = tagVMem(io.master.M.Addr(addrBits + 1, lineBits)))
+  val dirty = Reg(next = dirtyMem(io.master.M.Addr(addrBits + 1, lineBits)))
+  val tagValid = tagV && tag === Cat(masterReg.Addr(EXTMEM_ADDR_WIDTH-1, addrBits+2))
+
+  val fillReg = Reg(Bool())
+  val fillAddrReg = Reg(Bits(width = addrBits+2 - lineBits))
+
+  val wrAddrReg = Reg(Bits(width = addrBits))
+  val wrDataReg = Reg(Bits(width = DATA_WIDTH))
+
+  wrAddrReg := io.master.M.Addr(addrBits + 1, 2)
+  wrDataReg := io.master.M.Data
+
+  // Write to cache; store only updates what's already there
+  val stmsk = Mux(masterReg.Cmd === OcpCmd.WR, masterReg.ByteEn,  Bits("b0000"))
+  for (i <- 0 until BYTES_PER_WORD) {
+    mem(i) <= (fillReg || (tagValid && stmsk(i)), wrAddrReg,
+               wrDataReg(BYTE_WIDTH*(i+1)-1, BYTE_WIDTH*i))
+  }
+  // Update dirty bit when writing
+  when(tagValid && (stmsk != Bits("b0000"))) {
+    dirtyMem(masterReg.Addr(addrBits + 1, lineBits)) := Bool(true)
+  }
+
+
+  // Count register used to index reads for write-back
+  val rdAddrCntReg = Reg(init = UInt(0, lineBits-2))
+  // Read from cache
+  val selWrBack = Bool()
+  val rdAddr = Mux(selWrBack, Cat(fillAddrReg, rdAddrCntReg), io.master.M.Addr(addrBits + 1, 2)) // helper signal
+  val rdData = mem.map(_(rdAddr)).reduceLeft((x,y) => y ## x)
+  val rdDataReg = Reg(next = rdData)
+
+  // Return data on a hit
+  io.master.S.Data := rdData
+  io.master.S.Resp := Mux(tagValid && (masterReg.Cmd === OcpCmd.RD || masterReg.Cmd === OcpCmd.WR),
+                          OcpResp.DVA, OcpResp.NULL)
+
+  // Register to delay response
+  val slaveReg = Reg(io.master.S)
+
+  // Register for write-back address
+  val memWrAddrReg = Reg(Bits(width = addrWidth))
+
+  // Default values
+  io.slave.M.Cmd := OcpCmd.IDLE
+  io.slave.M.Addr := Cat(masterReg.Addr(EXTMEM_ADDR_WIDTH-1, lineBits),
+                         Fill(Bits(0), lineBits))
+  io.slave.M.Data := Bits(0)
+  io.slave.M.DataValid := Bits(0)
+  io.slave.M.DataByteEn := Bits(0)
+
+  fillReg := Bool(false)
+  doingRead := doingRead 
+  selWrBack := Bool(false)
+
+
+  // Start handling a miss
+  when(!tagValid && (masterReg.Cmd === OcpCmd.RD || masterReg.Cmd === OcpCmd.WR)) {
+    fillAddrReg := masterReg.Addr(addrBits + 1, lineBits)
+    tagVMem(masterReg.Addr(addrBits + 1, lineBits)) := Bool(true)
+    missIndexReg := masterReg.Addr(lineBits-1, 2).toUInt
+    memWrAddrReg := Cat(tag, masterReg.Addr(addrBits + 1, lineBits), Fill(Bits(0), lineBits))
+
+    // start writing back if block is dirty
+    when(dirty) {
+      io.slave.M.Cmd := OcpCmd.WR
+      stateReg := write
+      selWrBack := Bool(true)
+      rdAddrCntReg := rdAddrCntReg + UInt(1)
+    }
+    // or skip writeback if block is not dirty
+    .otherwise {
+      io.slave.M.Cmd := OcpCmd.RD
+      when(io.slave.S.CmdAccept === Bits(1)) {
+        stateReg := fill
+      }
+      .otherwise {
+        stateReg := hold
+        masterReg.Addr := masterReg.Addr
+      }
+    }
+
+
+    when(masterReg.Cmd === OcpCmd.WR) {
+      doingRead := Bool(false)
+      coreWrDataReg := masterReg.Data
+      dirtyMem(masterReg.Addr(addrBits + 1, lineBits)) := Bool(true)
+    }
+    when(masterReg.Cmd === OcpCmd.RD) {
+      doingRead := Bool(true)
+      dirtyMem(masterReg.Addr(addrBits + 1, lineBits)) := Bool(false)
+    }
+  }
+  tagMem.io <= (!tagValid && (masterReg.Cmd === OcpCmd.RD || masterReg.Cmd === OcpCmd.WR),
+                masterReg.Addr(addrBits + 1, lineBits),
+                masterReg.Addr(EXTMEM_ADDR_WIDTH-1, addrBits+2))
+
+  // writeback state
+  when(stateReg === write) {
+    selWrBack := Bool(true)
+    io.slave.M.Addr := Cat(memWrAddrReg(addrWidth-1, burstAddrBits+byteAddrBits),
+                           Fill(Bits(0), burstAddrBits+byteAddrBits))
+    when(burstCntReg === Bits(0)) {
+      io.slave.M.Cmd := OcpCmd.WR
+    }
+    io.slave.M.DataValid := Bits(1)
+    io.slave.M.Data := rdData
+    io.slave.M.DataByteEn := Bits("b1111")
+    when(io.slave.S.DataAccept === Bits(1)) {
+      burstCntReg := burstCntReg + UInt(1)
+      rdAddrCntReg := rdAddrCntReg + UInt(1)
+    }
+    when(burstCntReg === UInt(burstLength - 1)) {
+      when(io.slave.S.Resp === OcpResp.DVA) {
+        stateReg := hold
+      }
+      .otherwise {
+        stateReg := writeWaitForDVA
+      }
+      rdAddrCntReg := rdAddrCntReg // rdAddrCntReg is one step ahead of burstCnt, so don't update
+    }
+  }
+
+  // Slave responds with DVA to finish write transaction
+  when(stateReg === writeWaitForDVA) {
+    stateReg := writeWaitForDVA
+    when(io.slave.S.Resp === OcpResp.DVA) {
+      stateReg := hold
+    }
+  }
+
+  // Hold read command
+  when(stateReg === hold) {
+    io.slave.M.Cmd := OcpCmd.RD
+    when(io.slave.S.CmdAccept === Bits(1)) {
+      stateReg := fill
+    }
+    .otherwise {
+      stateReg := hold
+      masterReg.Addr := masterReg.Addr
+    }
+  }
+  // Wait for response
+  when(stateReg === fill) {
+    wrAddrReg := Cat(fillAddrReg, burstCntReg)
+    
+    when(io.slave.S.Resp === OcpResp.DVA) { 
+    fillReg := Bool(true)
+    wrDataReg := io.slave.S.Data
+      when(burstCntReg === missIndexReg) {
+        slaveReg := io.slave.S
+        // insert write data from core on write-allocation
+        when(!doingRead) {
+          wrDataReg := coreWrDataReg
+        }
+      }
+      when(burstCntReg === UInt(lineSize/4-1)) {
+        stateReg := respond
+      }
+      burstCntReg := burstCntReg + UInt(1)
+    }
+  }
+  // Pass data to master
+  when(stateReg === respond) {
+    io.master.S := slaveReg
+    stateReg := idle
+  }
+
+  // reset valid bits
+  when (io.invalidate) {
+    tagVMem.map(_ := Bool(false))
+  }
+}
