68737f6be7ec75e9b995a7762c63e4f3bd380c95
dani <dsanz006@gmail.com>
2016-08-31 12:41:53 +0200

    audio_dac_buffer_test extended

--- c/audio_dac_buffer_test.c
+++ c/audio_dac_buffer_test.c
@@ -17,8 +17,18 @@ int main() {
   for(int i=0; i<4; i++) {
     setOutputBuffer(audioArrayL[i], audioArrayR[i]);
   }
-  printf("done\n");
+  printf("done first\n");
 
+  printf("IWannaWaitVeryVeryLong\n");
+
+  // audio data
+  for(int j=0; j<3; j++) {
+    for(int i=0; i<4; i++) {
+      setOutputBuffer(audioArrayL[i]+3, audioArrayR[i]+3);
+    }
+  }
+  printf("done second\n");
+  
   return 0;
 
 }
