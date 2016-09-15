79d77e0508542686f463e1d5e70aeb8012de5ad6
dani <dsanz006@gmail.com>
2016-09-15 18:10:06 +0200

    audio_basic_filters extended, clipping problem

--- c/audio_basic_filters.c
+++ c/audio_basic_filters.c
@@ -7,17 +7,17 @@
 
 #define BUFFER_SIZE 32
 
-#define AUDIO_RECORDING_SIZE 44100*4
+#define AUDIO_RECORDING_SIZE 44100*2
 
-#define Fs 44100 // Hz
-#define Fc 5000   // Hz
+#define Fs 52083 // Hz
 
-#define K tan(M_PI * Fc / Fs)
 
-short x[BUFFER_SIZE][2] = {0}; // input audio
+short x[AUDIO_RECORDING_SIZE][2]; //input
 short x_h[2] = {0}; // stored delayed sample
-short y[BUFFER_SIZE][2] = {0}; // output audio
+short y[AUDIO_RECORDING_SIZE][2]; //output
 
+int Fc;
+float K, b0, b1, a1, c;
 
 
 int filter_1st(short (*x)[2], short *x_h, short (*y)[2], int SIZE, float b0, float a1, float c) {
@@ -43,85 +43,100 @@ int main() {
 
   setup();
 
-  //1st order LPF:
-  float b0 = 1 / ( K + 1 );
-  float b1 = -1 / ( K + 1 );
-  float a1 = ( K - 1 ) / ( K + 1 );
-  float c  = b1 - ( b0 * a1 );
-
-  printf("b0: %f, b1: %f, a1: %f, c: %f\n", b0, b1, a1, c);
-
-  setInputBufferSize(BUFFER_SIZE);
-  setOutputBufferSize(BUFFER_SIZE);
-
   // enable input and output
   *audioDacEnReg = 1;
   *audioAdcEnReg = 1;
 
-  //when keyReg 13, decrement, when keyReg 14, increment
+  setInputBufferSize(BUFFER_SIZE);
+  setOutputBufferSize(BUFFER_SIZE);
 
+  //input recording
   printf("Press KEY0 to start recording\n");
   while(*keyReg != 14);
   printf("Recording...\n");
-  short audioRecording[AUDIO_RECORDING_SIZE][2];
-  short audioFiltered[AUDIO_RECORDING_SIZE][2];
   for(int i=0; i<AUDIO_RECORDING_SIZE; i++) {
-    getInputBuffer(&audioRecording[i][0], &audioRecording[i][1]);
+    getInputBuffer(&x[i][0], &x[i][1]);
   }
-  printf("Calculating Filter...\n");
-  filter_1st(audioRecording, x_h, audioFiltered, AUDIO_RECORDING_SIZE, b0, a1, c);
-  printf("Done!\n");
+  //filtered signals calculation
+  printf("Recording Finished. \n");
+  printf("Press KEY0 to calculate LPF and KEY1 for HPF\n");
+  while(*keyReg == 15);
+  if(*keyReg == 13) {
+    //1st order HPF:
+    printf("Calculating HPF...\n");
+    Fc = 5000; // Hz
+    K = tan(M_PI * Fc / Fs);
+    b0 = 1 / ( K + 1 );
+    b1 = -1 / ( K + 1 );
+    a1 = ( K - 1 ) / ( K + 1 );
+    c  = b1 - ( b0 * a1 );
+    filter_1st(x, x_h, y, AUDIO_RECORDING_SIZE, b0, a1, c);
+  }
+  else {
+    if(*keyReg == 14) {
+      //1st order LPF:
+      printf("Calculating LPF...\n");
+      Fc = 600; // Hz
+      K = tan(M_PI * Fc / Fs);
+      b0 = K / ( K + 1 );
+      b1 = K / ( K + 1 );
+      a1 = ( K - 1 ) / ( K + 1 );
+      c  = b1 - ( b0 * a1 );
+      filter_1st(x, x_h, y, AUDIO_RECORDING_SIZE, b0, a1, c);
+    }
+  }
+  printf("Filter calculation finished\n");
+
+  /*
+  printf("Recording done. Press KEY0 again to look for input max\n");
+  while(*keyReg != 14);
+  short maxLeft = 0;
+  for(int i=0; i<AUDIO_RECORDING_SIZE; i++) {
+    if(abs(audioRecording[i][0]) >= maxLeft) {
+      maxLeft = abs(audioRecording[i][0]);
+      printf("MAX found: 0x%x at time %f\n", maxLeft, ((float)i/Fs));
+    }
+  }
+  */
+
 
 
   int endBool = 0;
-  int playBoolClean = 0;
-  int playBoolFiltered = 0;
-  printf("Press kEY0 for dry playback and KEY1 for filtered playback\n");
+  int playBoolClean, playBoolFilt = 0;
+  printf("KEY1: FILTERED, kEY0: DRY\n");
   while(!endBool) {
     switch(*keyReg) {
     case 14:
-      //play audio
+      //play clean
       if(!playBoolClean) {
         printf("Clean audio playback\n");
         for(int i=0; i<AUDIO_RECORDING_SIZE; i++) {
-          setOutputBuffer(audioRecording[i][0], audioRecording[i][1]);
+          setOutputBuffer(x[i][0], x[i][1]);
         }
         playBoolClean = 1;
       }
       break;
     case 13:
-      //play audio
-      if(!playBoolFiltered) {
+      //play HPF
+      if(!playBoolFilt) {
         printf("Filtered audio playback\n");
         for(int i=0; i<AUDIO_RECORDING_SIZE; i++) {
-          setOutputBuffer(audioFiltered[i][0], audioFiltered[i][1]);
+          setOutputBuffer(y[i][0], y[i][1]);
         }
-        playBoolFiltered = 1;
+        playBoolFilt = 1;
       }
       break;
     case 3:
       endBool = 1;
       break;
     default:
-      playBoolClean = 0;
-      playBoolFiltered = 0;
+      if ( playBoolClean || playBoolFilt ) {
+        printf("KEY1: FILTERED, kEY0: DRY\n");
+        playBoolClean = 0;
+        playBoolFilt = 0;
+      }
     }
   }
 
-  /*
-
-  while(*keyReg != 3) {
-    for(int i=0; i<BUFFER_SIZE; i++) {
-      getInputBuffer(&x[i][0], &x[i][1]);
-    }
-    filter_1st(x, x_h, y, BUFFER_SIZE, b0, a1, c);
-    for(int i=0; i<BUFFER_SIZE; i++) {
-      setOutputBuffer(y[i][0], y[i][1]);
-    }
-  }
-
-
-
-  */
   return 0;
 }
