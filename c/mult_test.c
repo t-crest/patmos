88311068efa85304764946a2b32197c21fe2ed1e
Daniel Sanz Ausin <dsanz006@gmail.com>
2016-09-29 12:12:29 +0200

    work on mult test, still not printing correct values

--- c/mult_test.c
+++ c/mult_test.c
@@ -9,37 +9,97 @@
 // assembly language
 // also, measuring the CPU cycles before and after
 
-float floatMult(volatile float x, volatile float y) {
-  unsigned long long cycleTime1 = get_cpu_cycles();
-  volatile float result = x*y;
-  unsigned long long cycleTime2 = get_cpu_cycles();
+volatile float floatMult(volatile float x, volatile float y) {
+  return (x * y);
 
-  if(((int)result) < -200) {
-    printf("%llu, %llu\n", cycleTime1, cycleTime2);
-    printf("%llu\n", (cycleTime2-cycleTime1));
+  /*
+  //volatile unsigned long long timesArray[10];
+  volatile unsigned long long cycleTime1;
+  volatile unsigned long long cycleTime2;
+  volatile float result;
+  for(int i=0; i<10; i++) {
+    cycleTime1 = get_cpu_cycles();
+    result = x*y;
+    cycleTime2 = get_cpu_cycles();
+    timesArray[i] = cycleTime2 - cycleTime1;
+    x++;
   }
-
+  if(((int) result) < -200) {
+    for(int i=0; i<10; i++) {
+      printf("%llu\n", timesArray[i]);
+    }
+  }
   return result;
+  */
 }
 
-int fixedMult(volatile int x, volatile int y) {
-  unsigned long long cycleTime1 = get_cpu_cycles();
-  volatile int result = x*y;
-  unsigned long long cycleTime2 = get_cpu_cycles();
+volatile int fixedMult(volatile int x, volatile int y) {
+  return (x * y);
 
+  /*
+  volatile unsigned long long timesArray[10];
+  volatile unsigned long long cycleTime1;
+  volatile unsigned long long cycleTime2;
+  volatile int result = -250;
+  for(int i=0; i<10; i++) {
+    cycleTime1 = get_cpu_cycles();
+    result = x*y;
+    cycleTime2 = get_cpu_cycles();
+    timesArray[i] = cycleTime2 - cycleTime1;
+    x++;
+  }
   if(result < -200) {
-    printf("%llu, %llu\n", cycleTime1, cycleTime2);
-    printf("%llu\n", (cycleTime2-cycleTime1));
+    for(int i=0; i<10; i++) {
+      printf("%llu\n", timesArray[i]);
+    }
   }
 
   return result;
+  */
 }
 
 int main() {
-  volatile float x = 11.333;
-  volatile float y = -26.056;
-  volatile float res = floatMult(x, y);
-  //volatile int res = fixedMult((volatile int)x, (volatile int)y);
+  volatile unsigned long long timesArray[10];
+  volatile unsigned long long cycleTime1;
+  volatile unsigned long long cycleTime2;
+
+  /*
+  // FOR FLOATING POINT MULT:
+
+  volatile float x_f, y_f, res_f;
+  x_f = 11.333;
+  y_f = -26.056;
+  for(int i=0; i<10; i++) {
+    cycleTime1 = get_cpu_cycles();
+    res_f = floatMult(x_f, y_f);
+    cycleTime2 = get_cpu_cycles();
+    timesArray[i] = cycleTime2 - cycleTime1;
+  }
+  printf("result: %f\n", res_f);
+  if(res_f < -200) {
+    for(int i=0; i<10; i++) {
+      printf("%llu\n", timesArray[i]);
+    }
+  }
+  */
+  // FOR FIXED POINT MULT:
+
+  volatile int x_i, y_i, res_i;
+  x_i = 11;
+  y_i = -26;
+  for(int i=0; i<10; i++) {
+    cycleTime1 = get_cpu_cycles();
+    res_i = fixedMult(x_i, y_i);
+    cycleTime2 = get_cpu_cycles();
+    timesArray[i] = cycleTime2 - cycleTime1;
+  }
+  printf("result: %d\n", res_i);
+  if(res_i < -200) {
+    for(int i=0; i<10; i++) {
+      printf("%llu\n", timesArray[i]);
+    }
+  }
+
 
   return 0;
 }
