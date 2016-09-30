c34a4b9026de7179bd442bcacdfc2c98bb1c9061
Daniel Sanz Ausin <dsanz006@gmail.com>
2016-09-30 16:25:52 +0200

    audio_basic_filters small fixes

--- c/audio_basic_filters_2.c
+++ c/audio_basic_filters_2.c
@@ -16,25 +16,29 @@
 #include "audio.c"
 
 
-short x[AUDIO_RECORDING_SIZE][2] = {0}; //input
-short y[AUDIO_RECORDING_SIZE][2] = {0}; //output
+short x_in[AUDIO_RECORDING_SIZE][2] = {0}; //input
+short y_out[AUDIO_RECORDING_SIZE][2] = {0}; //output
 
 // LOCATION IN SCRATCHPAD MEMORY
-#define ACCUM_ADDR  0x00000000
-#define B_ADDR      ( ACCUM_ADDR  + 2 * sizeof(int) )
-#define A_ADDR      ( B_ADDR      + FILTER_ORDER_1PLUS * sizeof(short) )
-#define X_FILT_ADDR ( A_ADDR      + FILTER_ORDER_1PLUS * sizeof(short) )
-#define Y_FILT_ADDR ( X_FILT_ADDR + 2 * FILTER_ORDER_1PLUS * sizeof(short) )
-#define PNT_ADDR    ( Y_FILT_ADDR + 2 * FILTER_ORDER_1PLUS * sizeof(short) )
-#define SFTLFT_ADDR ( PNT_ADDR    + sizeof(int) )
+#define ACCUM_XH_ADDR  0x00000000
+#define ACCUM_Y_ADDR ( ACCUM_XH_ADDR + 2 * sizeof(int) )
+#define B_ADDR       ( ACCUM_Y_ADDR  + 2 * sizeof(int) )
+#define A_ADDR       ( B_ADDR        + FILTER_ORDER_1PLUS * sizeof(short) )
+#define X_H_ADDR     ( A_ADDR        + FILTER_ORDER_1PLUS * sizeof(short) )
+#define X_ADDR       ( X_H_ADDR      + 2 * FILTER_ORDER_1PLUS * sizeof(short) )
+#define Y_ADDR       ( X_ADDR        + 2 * sizeof(short) )
+#define PNT_ADDR     ( Y_ADDR        + 2 * sizeof(short) )
+#define SFTLFT_ADDR  ( PNT_ADDR      + sizeof(int) )
 
 //to have fixed-point multiplication:
 float K;
-volatile _SPM int *accum           = (volatile _SPM int *)        ACCUM_ADDR;
+volatile _SPM int *accum_xh        = (volatile _SPM int *)        ACCUM_XH_ADDR; //accumulator for x_h
+volatile _SPM int *accum_y         = (volatile _SPM int *)        ACCUM_Y_ADDR; //accumulator for y
 volatile _SPM short *B             = (volatile _SPM short *)      B_ADDR; // [b2, b1, b0]
-volatile _SPM short *A             = (volatile _SPM short *)      A_ADDR; // [a2, a1,  1]
-volatile _SPM short (*x_filter)[2] = (volatile _SPM short (*)[2]) X_FILT_ADDR; // x_filter[FILTER_ORDER_1PLUS][2] = {0};
-volatile _SPM short (*y_filter)[2] = (volatile _SPM short (*)[2]) Y_FILT_ADDR; // y_filter[FILTER_ORDER_1PLUS][2] = {0};
+volatile _SPM short *A             = (volatile _SPM short *)      A_ADDR; // [a2, a1,  0]
+volatile _SPM short (*x_h)[2]      = (volatile _SPM short (*)[2]) X_H_ADDR; // x_h[FILTER_ORDER_1PLUS][2] = {0};
+volatile _SPM short *x             = (volatile _SPM short *)      X_ADDR; // x[2] input audio
+volatile _SPM short *y             = (volatile _SPM short *)      Y_ADDR; // y[2] output audio
 volatile _SPM int *pnt             = (volatile _SPM int *)        PNT_ADDR; // pointer indicates last position of x_filter buffer
 volatile _SPM int *shiftLeft       = (volatile _SPM int *)        SFTLFT_ADDR; //shift left amount;
 
@@ -111,10 +115,10 @@ int calc_filter_coeff(volatile _SPM short *B, volatile _SPM short *A, float K, i
     A[i] = (short) ( (int) (ONE_16b * Afl[i]) >> *shiftLeft );
   }
   if(FILTER_ORDER_1PLUS == 2) {
-    printf("done! K: %f, b0: %d, b1: %d, a0, %d, a1: %d\n", K, B[1], B[0], A[1], A[0]);
+    printf("done! K: %f, b0: %d, b1: %d, a1: %d\n", K, B[1], B[0], A[0]);
   }
   if(FILTER_ORDER_1PLUS == 3) {
-    printf("done! K: %f, common_factor: %f, b0: %d, b1: %d, b2 : %d, a0: %d, a1: %d, a2: %d\n", K, common_factor, B[2], B[1], B[0], A[2], A[1], A[0]);
+    printf("done! K: %f, common_factor: %f, b0: %d, b1: %d, b2 : %d, a1: %d, a2: %d\n", K, common_factor, B[2], B[1], B[0], A[1], A[0]);
   }
 
   return 0;
@@ -138,7 +142,7 @@ int main() {
   *shiftLeft = 0;
 
 
-  printf("addresses at scratchpad mem: accum: 0x%x, B: 0x%x, A: 0x%x, x_filter: 0x%x, y_filter: 0x%x, pnt: 0x%x, shiftLeft: 0x%x\n", (int)accum, (int)B, (int)A, (int)x_filter, (int)y_filter, (int)pnt, (int)shiftLeft);
+  printf("addresses at scratchpad mem: accum_xh: 0x%x, accum_y: 0x%x, B: 0x%x, A: 0x%x, x_h: 0x%x, x: 0x%x, y: 0x%x, pnt: 0x%x, shiftLeft: 0x%x\n", (int)accum_xh, (int)accum_y, (int)B, (int)A, (int)x_h, (int)x, (int)y, (int)pnt, (int)shiftLeft);
 
   printf("Press KEY0 for real-time playing and KEY1 for recording\n");
   while(*keyReg == 15);
@@ -152,27 +156,23 @@ int main() {
     if(*keyReg == 13) {
       calc_filter_coeff(B, A, K, 5000, 0.707, 1); // 1 for HFP
     }
-
-    //first, fill filter buffer
-    for(*pnt=0; *pnt<(FILTER_ORDER_1PLUS-1); *pnt++) {
-      getInputBufferSPM(&x_filter[*pnt][0], &x_filter[*pnt][1]);
-    }
-    //when filter buffer is full, compute each output sample
+    //IIR filter loop:
+    *pnt = 0;
     while(*keyReg != 3) {
+      //first, read last sample
+      getInputBufferSPM(&x[0], &x[1]);
+      //then, calculate filter
+      filterIIR_2(pnt, x, x_h, y, accum_xh, accum_y, B, A, *shiftLeft);
+      //set output
+      setOutputBuffer(y[0], y[1]);
       //increment pointer
       *pnt = (*pnt+1) % FILTER_ORDER_1PLUS;
-      //first, read last sample
-      getInputBufferSPM(&x_filter[*pnt][0], &x_filter[*pnt][1]);
-      //then, calculate filter
-      filterIIR(pnt, x_filter, y_filter, accum, B, A, *shiftLeft);
-      //set output
-      setOutputBuffer(y_filter[*pnt][0], y_filter[*pnt][1]);
     }
   }
   if(*keyReg == 13) {
     printf("Recording...\n");
     for(int i=0; i<AUDIO_RECORDING_SIZE; i++) {
-      getInputBuffer(&x[i][0], &x[i][1]);
+      getInputBuffer(&x_in[i][0], &x_in[i][1]);
     }
     //filtered signals calculation
     printf("Recording Finished. \n");
@@ -186,26 +186,20 @@ int main() {
       calc_filter_coeff(B, A, K, 600, 0.707, 0); // 0 for LFP
     }
 
-    int x_pnt = 0;
-
-    //first, fill filter buffer
-    for(*pnt=0; *pnt<(FILTER_ORDER_1PLUS-1); *pnt++) {
-      x_filter[*pnt][0] = x[*pnt][0];
-      x_filter[*pnt][1] = x[*pnt][1];
-    }
-    x_pnt = *pnt;
+    // IIR filter loop
+    *pnt = 0;
     //when filter buffer is full, compute each output sample
-    for(x_pnt=(FILTER_ORDER_1PLUS-1); x_pnt<AUDIO_RECORDING_SIZE; x_pnt++) {
+    for(int x_pnt=0; x_pnt<AUDIO_RECORDING_SIZE; x_pnt++) {
+      //first, read last sample
+      x[0] = x_in[x_pnt][0];
+      x[1] = x_in[x_pnt][1];
+      //then, calculate filter
+      filterIIR_2(pnt, x, x_h, y, accum_xh, accum_y, B, A, *shiftLeft);
+      //set output
+      y_out[x_pnt][0] = y[0];
+      y_out[x_pnt][1] = y[1];
       //increment pointer
       *pnt = (*pnt+1) % FILTER_ORDER_1PLUS;
-      //first, read last sample
-      x_filter[*pnt][0] = x[x_pnt][0];
-      x_filter[*pnt][1] = x[x_pnt][1];
-      //then, calculate filter
-      filterIIR(pnt, x_filter, y_filter, accum, B, A, *shiftLeft);
-      //set output
-      y[x_pnt][0] = y_filter[*pnt][0];
-      y[x_pnt][1] = y_filter[*pnt][1];
     }
     printf("Filter calculation finished\n");
 
@@ -221,7 +215,7 @@ int main() {
         if(!playBoolClean) {
           printf("Clean audio playback\n");
           for(int i=0; i<AUDIO_RECORDING_SIZE; i++) {
-            setOutputBuffer(x[i][0], x[i][1]);
+            setOutputBuffer(x_in[i][0], x_in[i][1]);
           }
           playBoolClean = 1;
         }
@@ -231,7 +225,7 @@ int main() {
         if(!playBoolFilt) {
           printf("Filtered audio playback\n");
           for(int i=0; i<AUDIO_RECORDING_SIZE; i++) {
-            setOutputBuffer(y[i][0], y[i][1]);
+            setOutputBuffer(y_out[i][0], y_out[i][1]);
           }
           playBoolFilt = 1;
         }
