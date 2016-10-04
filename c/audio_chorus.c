df9d8bf6b95a000aa27fd67e7338c89b9d2965c0
Daniel Sanz Ausin <dsanz006@gmail.com>
2016-10-04 18:12:21 +0200

    chorus: modulation changed from noise to sin, works except for clicking sound

--- c/audio_chorus.c
+++ c/audio_chorus.c
@@ -10,7 +10,7 @@
 #define Fs 52083 // Hz
 
 #define FILTER_ORDER_1PLUS 3 //FOR LPF filter of noise
-#define COMB_FILTER_ORDER_1PLUS 3 // FOR CHORUS comb filter
+#define COMB_FILTER_ORDER_1PLUS 2 // FOR CHORUS comb filter
 
 
 /* Chorus:
@@ -39,6 +39,12 @@
 
 #define PNT_ADDR    ( DEL_ADDR    + COMB_FILTER_ORDER_1PLUS * sizeof(int) )
 
+//CHORUS: SINUSOIDAL MODULATION
+#define C1_PNT_ADDR ( PNT_ADDR    + sizeof(int) )
+#define C2_PNT_ADDR ( C1_PNT_ADDR + sizeof(int) )
+
+/*
+//NOISE GENERATION ADDRESSES
 #define CH_ACC_ADDR ( PNT_ADDR    + sizeof(int) )
 #define B_ADDR      ( CH_ACC_ADDR + 2 * sizeof(int) )
 
@@ -59,7 +65,7 @@
 #endif
 
 #define SFTLFT_ADDR ( CH_PNT_ADDR + sizeof(int) )
-
+*/
 
 
 
@@ -68,7 +74,13 @@ volatile _SPM short *y               = (volatile _SPM short *)      Y_ADDR; // y
 volatile _SPM short *g               = (volatile _SPM short *)      G_ADDR; // g[FILTER_ORDER_1PLUS]: array of gains [... g2, g1, g0]
 volatile _SPM int *del               = (volatile _SPM int *)        DEL_ADDR; // del[FILTER_ORDER_1PLUS]: array of delays [...d2, d1, 0]
 volatile _SPM int *pnt               = (volatile _SPM int *)        PNT_ADDR; //pointer indicates last position of fir_buffer
-//stuff for LPF noise signal
+
+//CHORUS: SINUSOIDAL MODULATION
+volatile _SPM int *c1_pnt            = (volatile _SPM int *)        C1_PNT_ADDR;
+volatile _SPM int *c2_pnt            = (volatile _SPM int *)        C2_PNT_ADDR;
+
+/*
+//LOW-PASS NOISE GENERATION SIGNALS
 float K;
 volatile _SPM int *ch_accum                            = (volatile _SPM int *)                                CH_ACC_ADDR;
 volatile _SPM short *B                                 = (volatile _SPM short *)                              B_ADDR; // [b2, b1, b0]
@@ -77,10 +89,20 @@ volatile _SPM short (*ch_x)[COMB_FILTER_ORDER_1PLUS-1] = (volatile _SPM short (*
 volatile _SPM short (*ch_y)[COMB_FILTER_ORDER_1PLUS-1] = (volatile _SPM short (*)[COMB_FILTER_ORDER_1PLUS-1]) CH_Y_ADDR;
 volatile _SPM int *ch_pnt                              = (volatile _SPM int *)                                CH_PNT_ADDR;
 volatile _SPM int *shiftLeft                           = (volatile _SPM int *)                                SFTLFT_ADDR; //shift left amount;
+*/
 
 //volatile _SPM short (*fir_buffer)[2] = (volatile _SPM short (*)[2]) FIR_BUFFER_ADDR; // fir_buffer[FIR_BUFFER_LENGTH][2]
 volatile short fir_buffer[FIR_BUFFER_LENGTH][2];
 
+// SIZE is period of sinusoidal in samples
+int storeSin(int *sinArray, int SIZE, int OFFSET, int AMP) {
+    for(int i=0; i<SIZE; i++) {
+        sinArray[i] = OFFSET + AMP*sin(2.0*M_PI* i / SIZE);
+    }
+    printf("sin array storage done\n");
+
+    return 0;
+}
 
 int main() {
 
@@ -93,12 +115,12 @@ int main() {
     setInputBufferSize(BUFFER_SIZE);
     setOutputBufferSize(BUFFER_SIZE);
 
-    *shiftLeft = 0;
+    //*shiftLeft = 0;
 
-    printf("Addresses: accum: %d, y: %d, g: %d, del: %d, pnt: %d, ch_accum: %d, B: %d, A: %d, ch_x: %d, ch_y: %d, ch_pnt: %d, shiftLeft: %d\n", (int)accum, (int)y, (int)g, (int)del, (int)pnt, (int)ch_accum, (int)B, (int)A, (int)ch_x, (int)ch_y, (int)ch_pnt, (int)shiftLeft);
+    //printf("Addresses: accum: %d, y: %d, g: %d, del: %d, pnt: %d, ch_accum: %d, B: %d, A: %d, ch_x: %d, ch_y: %d, ch_pnt: %d, shiftLeft: %d\n", (int)accum, (int)y, (int)g, (int)del, (int)pnt, (int)ch_accum, (int)B, (int)A, (int)ch_x, (int)ch_y, (int)ch_pnt, (int)shiftLeft);
 
     //FILTER COEFFICIENTS FOR NOISE SIGNAL
-    calc_filter_coeff(B, A, K, 600, 0.707, shiftLeft, 0); //0 for LPF
+    //calc_filter_coeff(B, A, K, 600, 0.707, shiftLeft, 0); //0 for LPF
 
     //gains
     g[2] = ONE_16b * 0.8; //g0
@@ -109,23 +131,58 @@ int main() {
     del[2] = 0; //always d0 = 0
 
     //CPU cycles stuff
-    //int CPUcycles[1000] = {0};
+    //int CPUcycles[10000] = {0};
     //int cpu_pnt = 0;
 
+    //sin array storage:
+    int SIN1_PERIOD = 52083; // 1 second
+    int SIN2_PERIOD = 40000; // ~0.8 seconds
+    int sinC1[SIN1_PERIOD];
+    int sinC2[SIN2_PERIOD];
+    storeSin(sinC1, SIN1_PERIOD, ( FIR_BUFFER_LENGTH*0.6 ), ( FIR_BUFFER_LENGTH * 0.02) );
+    storeSin(sinC2, SIN2_PERIOD, ( FIR_BUFFER_LENGTH*0.4 ), ( FIR_BUFFER_LENGTH * 0.012) );
+    printf("sins storage done!\n");
+    printf("0: %d, 2nd last: %d, last: %d\n", sinC1[0], sinC1[SIN1_PERIOD-2], sinC1[SIN1_PERIOD-1]);
 
     *pnt = FIR_BUFFER_LENGTH - 1; //start on top
-    *ch_pnt = 0;
+    //*ch_pnt = 0;
+    *c1_pnt = 0;
+    *c2_pnt = 0;
     while(*keyReg != 3) {
+        /*
         //UPDATE DELAYS: NOISE + LPF
         for(int i=0; i<(COMB_FILTER_ORDER_1PLUS-1); i++) {
-            ch_x[*ch_pnt][i] = rand() % FIR_BUFFER_LENGTH;
-            //printf("ch_x[%d][%d]=%d\n", *ch_pnt, i, ch_x[*ch_pnt][i]);
+            ch_x[*ch_pnt][i] = del[i] + ( (rand() % 3) - 1 );
+            //limits:
+            if(ch_x[*ch_pnt][i] > (FIR_BUFFER_LENGTH-1)) {
+                ch_x[*ch_pnt][i] = FIR_BUFFER_LENGTH - 1;
+            }
+            else {
+                if(ch_x[*ch_pnt][i] < 0) {
+                    ch_x[*ch_pnt][i] = 0;
+                }
+            }
             filterIIR(ch_pnt, ch_x, ch_y, ch_accum, B, A, *shiftLeft);
-            //printf("ch_y[%d][%d]=%d\n", *ch_pnt, i, ch_y[*ch_pnt][i]);
             del[i] = ch_y[*ch_pnt][i];
         }
         *ch_pnt = (*ch_pnt+1) % FILTER_ORDER_1PLUS;
-        //printf("del[1]: %d, del[0]: %d\n", del[1], del[0]);
+        */
+        // SINUSOIDAL MODULATION OF DELAY LENGTH
+        del[0] = sinC1[*c1_pnt];
+        del[1] = sinC2[*c2_pnt];
+        //printf("del[0]=%d, del[1]=%d\n", del[0], del[1]);
+        if (*c1_pnt < SIN1_PERIOD) {
+            *c1_pnt = *c1_pnt + 1;
+        }
+        else {
+            *c1_pnt = 0;
+        }
+        if (*c2_pnt < SIN2_PERIOD) {
+            *c2_pnt = *c2_pnt + 1;
+        }
+        else {
+            *c2_pnt = 0;
+        }
 
         //first, read sample
         getInputBuffer((short *)&fir_buffer[*pnt][0], (short *)&fir_buffer[*pnt][1]);
@@ -145,7 +202,7 @@ int main() {
         //store CPU Cycles
         CPUcycles[cpu_pnt] = get_cpu_cycles();
         cpu_pnt++;
-        if(cpu_pnt == 1000) {
+        if(cpu_pnt == 10000) {
             break;
         }
         */
@@ -153,10 +210,11 @@ int main() {
     }
 
     /*
-    for(int i=1; i<1000; i++) {
+    for(int i=1; i<10000; i++) {
         printf("%d\n", (CPUcycles[i]-CPUcycles[i-1]));
     }
     */
+
 
     return 0;
 }
