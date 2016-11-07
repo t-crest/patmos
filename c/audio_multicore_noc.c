ad1ea41321aa02f7b7170316aaa6ca2b8ae4a0d4
Daniel Sanz Ausin <dsanz006gmail.com>
2016-11-07 17:00:23 +0100

    audio_multicore_noc polished

--- c/audio_multicore_noc.c
+++ c/audio_multicore_noc.c
@@ -12,22 +12,18 @@ const int NOC_MASTER = 0;
 #define MP_CHAN_SHORTS_AMOUNT 2
 
 #define MP_CHAN_1_ID 1
-#define MP_CHAN_1_NUM_BUF 2
+#define MP_CHAN_1_NUM_BUF 1
 #define MP_CHAN_1_MSG_SIZE MP_CHAN_SHORTS_AMOUNT * 2 // 2 shorts = 4 bytes (actually always multiples of 4)
 
 #define MP_CHAN_2_ID 2
-#define MP_CHAN_2_NUM_BUF 2
+#define MP_CHAN_2_NUM_BUF 1
 #define MP_CHAN_2_MSG_SIZE MP_CHAN_SHORTS_AMOUNT * 2 // 2 shorts = 4 bytes
 
 
 void thread1_delay(void* args) {
     volatile _UNCACHED int **inArgs = (volatile _UNCACHED int **) args;
-    volatile _UNCACHED int *ledOnTimeP  = inArgs[0];
-    volatile _UNCACHED int *ledOffTimeP = inArgs[1];
-    volatile _UNCACHED int *exitP       = inArgs[2];
-
-    volatile _UNCACHED int *stateVar1P = inArgs[3];
-    volatile _UNCACHED int *stateVar2P = inArgs[4];
+    volatile _UNCACHED int *exitP      = inArgs[0];
+    volatile _UNCACHED int *stateVar1P = inArgs[1];
 
     //MP:
     // Create the queuing ports
@@ -38,7 +34,6 @@ void thread1_delay(void* args) {
 
     // Initialize the communication channels
     int nocret = mp_init_ports();
-
 
     //receive from master and respond
     for(short i=0; i<10; i++) {
@@ -63,17 +58,6 @@ void thread1_delay(void* args) {
 
     }
 
-    /*
-    while(*exitP == 0) {
-        for(int i=0; i<(*ledOnTimeP * 100000); i++) {
-            *ledReg = 1;
-        }
-        for(int i=0; i<(*ledOffTimeP * 100000); i++) {
-            *ledReg = 0;
-        }
-    }
-    */
-
     // exit with return value
     int ret = 0;
     corethread_exit(&ret);
@@ -91,23 +75,14 @@ int main() {
     //create corethread type var
     corethread_t threadOne = (corethread_t) 1;
     //arguments to thread 1 function
-    int ledOnTime = 15;
-    int ledOffTime = 5;
     int exit = 0;
-    volatile _UNCACHED int *ledOnTimeP = (volatile _UNCACHED int *) &ledOnTime;
-    volatile _UNCACHED int *ledOffTimeP = (volatile _UNCACHED int *) &ledOffTime;
-    volatile _UNCACHED int *exitP      = (volatile _UNCACHED int *) &exit;
-    volatile _UNCACHED int (*thread1_args[5]);
-    thread1_args[0] = ledOnTimeP;
-    thread1_args[1] = ledOffTimeP;
-    thread1_args[2] = exitP;
-
     int stateVar1 = -1;
-    int stateVar2 = -1;
+    volatile _UNCACHED int *exitP      = (volatile _UNCACHED int *) &exit;
     volatile _UNCACHED int *stateVar1P = (volatile _UNCACHED int *) &stateVar1;
-    volatile _UNCACHED int *stateVar2P = (volatile _UNCACHED int *) &stateVar2;
-    thread1_args[3] = stateVar1P;
-    thread1_args[4] = stateVar2P;
+    volatile _UNCACHED int (*thread1_args[2]);
+    thread1_args[0] = exitP;
+    thread1_args[1] = stateVar1P;
+
     printf("starting thread...\n");
     //set thread function and start thread
     corethread_create(&threadOne, &thread1_delay, (void*) thread1_args);
@@ -138,8 +113,6 @@ int main() {
         //send message from master to slave
         for(int j=0; j<MP_CHAN_SHORTS_AMOUNT; j++) {
             send_data[i][j] = (short)((i*5)+j);
-            //printf("write buffer pointer is: 0x%x\n", (unsigned int)&(*((volatile _SPM short *)chan1->write_buf)));
-            //printf("pos to write is: 0x%x\n", (unsigned int)&(*((volatile _SPM short *)chan1->write_buf+j)));
             *((volatile _SPM short *)chan1->write_buf+j) = send_data[i][j];
         }
 
@@ -158,8 +131,10 @@ int main() {
 
         // Acknowledge the received data
         mp_ack(chan2,0);
+
     }
 
+    //printing sent and received values:
     for(short i=0; i<10; i++) {
         printf("sent data at %d was", i);
         for(int j=0; j<MP_CHAN_SHORTS_AMOUNT; j++) {
@@ -172,42 +147,6 @@ int main() {
         printf("\n");
     }
 
-    /*
-    //AudioFX struct: contains no effect
-    struct AudioFX audio1FX;
-    struct AudioFX *audio1FXPnt = &audio1FX;
-    int AUDIO_ALLOC_AMOUNT = alloc_dry_vars(audio1FXPnt, 0);
-
-    setup(0); //guitar?
-
-    // enable input and output
-    *audioDacEnReg = 1;
-    *audioAdcEnReg = 1;
-
-    setInputBufferSize(BUFFER_SIZE);
-    setOutputBufferSize(BUFFER_SIZE);
-
-    //CPU cycles stuff
-    //int CPUcycles[1000] = {0};
-    //int cpu_pnt = 0;
-
-    while(*keyReg != 3) {
-        //put input in x
-        audioIn(audio1FXPnt);
-        audio_dry(audio1FXPnt);
-        //put y in output
-        audioOut(audio1FXPnt);
-        // / *
-        //store CPU Cycles
-        CPUcycles[cpu_pnt] = get_cpu_cycles();
-        cpu_pnt++;
-        if(cpu_pnt == 1000) {
-            break;
-        }
-        // * /
-    }
-    */
-
 
     //exit stuff
     printf("exit here!\n");
@@ -217,10 +156,6 @@ int main() {
     int *retval;
     corethread_join(threadOne, (void **)&retval);
     printf("thread 1 finished!\n");
-    /*
-    for(int i=1; i<1000; i++) {
-        printf("%d\n", (CPUcycles[i]-CPUcycles[i-1]));
-    }
-    */
+
     return 0;
 }
