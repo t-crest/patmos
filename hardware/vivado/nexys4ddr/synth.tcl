0fae7474d7da6c30f431d481f0a6b9f713850ed5
Luca Pezzarossa <lpez@dtu.dk>
2017-02-20 11:58:49 +0100

    Fixing tcl scripts for vivado synth and config from patmos makefile.

--- hardware/vivado/nexys4ddr/synth.tcl
+++ hardware/vivado/nexys4ddr/synth.tcl
@@ -1,6 +1,6 @@
 # Xilinx Vivado TCL script form synthesis to bitstream generation.
-#The runs run with 8 jobs, reduce it for old machines.
-#Author: Luca Pezzarossa (lpez@dtu.dk)
+# The runs run with 8 jobs, reduce it for old machines.
+# Author: Luca Pezzarossa (lpez@dtu.dk)
 open_project ./vivado/nexys4ddr/nexys4ddr.xpr
 reset_project
 launch_runs impl_1 -to_step write_bitstream -jobs 8
