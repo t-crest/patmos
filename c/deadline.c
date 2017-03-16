7088ea5f0d73bdd0d76fc5dbb745ae1e7efa2a5e
Martin Schoeberl <martin@jopdesign.com>
2017-03-16 17:15:51 +0100

    deadline: better example for deadline usage

--- c/deadline.c
+++ c/deadline.c
@@ -15,7 +15,7 @@ int main() {
   // Measure execution time with the clock cycle timer
   volatile _IODEV int *timer_ptr = (volatile _IODEV int *) 0xf0020004;
   // use function
-  int start, end;
+  int start, end, work;
   int val;
 
   start = *timer_ptr;
@@ -49,9 +49,22 @@ int main() {
     ;
   }
   start = *timer_ptr;
-  val = *dead_ptr; // this read is delayed by 500 clock cycles
+  val = *dead_ptr; // this read is not delayed anymore
   end = *timer_ptr;
   printf("Delay measurement after expired %d %d\n", end-start, val);
+
+  // Show a real use of deadline instruction to stall for the remaining time after some operation
+
+  start = *timer_ptr;
+  *dead_ptr = 500;
+  work = start + 250;
+  while (*timer_ptr - work < 0) {
+    ;
+  }
+  work = *timer_ptr;
+  val = *dead_ptr; // this read is delayed for the rest of the time
+  end = *timer_ptr;
+  printf("Delay whole code: %d, stall time %d, val %d\n", end-start, end - work, val);
 
   // Show absolute value in a simple example
 
