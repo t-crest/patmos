3e33d080231f8c6bcb55bd2019bab2de828c955c
Daniel Sanz Ausin <dsanz006gmail.com>
2017-03-17 15:25:29 +0100

    audio_forward.c simple example added for singlecore platform

---
+++ c/libaudio/audio_singlecore.h
@@ -0,0 +1,63 @@
+#ifndef _AUDIO_SINGLECORE_H_
+#define _AUDIO_SINGLECORE_H_
+
+#include <machine/spm.h>
+#include <machine/rtc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define LED_ADDR       (volatile _SPM int *) 0xF0090000
+#define KEY_ADDR       (volatile _SPM int *) 0xF00A0000
+#define DACL_ADDR      (volatile _SPM int *) 0xF00B0000
+#define DACR_ADDR      (volatile _SPM int *) 0xF00B0010
+#define DACEN_ADDR     (volatile _SPM int *) 0xF00B0020
+#define DACBUFS_ADDR   (volatile _SPM int *) 0xF00B0040
+#define DACBUFWP_ADDR  (volatile _SPM int *) 0xF00B0050
+#define DACBUFFU_ADDR  (volatile _SPM int *) 0xF00B0060
+#define ADCL_ADDR      (volatile _SPM int *) 0xF00B0080
+#define ADCR_ADDR      (volatile _SPM int *) 0xF00B0090
+#define ADCEN_ADDR     (volatile _SPM int *) 0xF00B00A0
+#define ADCBUFS_ADDR   (volatile _SPM int *) 0xF00B00B0
+#define ADCBUFRP_ADDR  (volatile _SPM int *) 0xF00B00C0
+#define ADCBUFEM_ADDR  (volatile _SPM int *) 0xF00B00D0
+#define I2CDATA_ADDR   (volatile _SPM int *) 0xF00B00E0
+#define I2CADDR_ADDR   (volatile _SPM int *) 0xF00B00F0
+#define I2CACK_ADDR    (volatile _SPM int *) 0xF00B0100
+#define I2CREQ_ADDR    (volatile _SPM int *) 0xF00B0110
+
+//Leds
+volatile _SPM int *ledReg = LED_ADDR;
+//Keys
+volatile _SPM int *keyReg = KEY_ADDR;
+//DAC
+volatile _SPM int *audioDacLReg	   = DACL_ADDR;
+volatile _SPM int *audioDacRReg    = DACR_ADDR;
+volatile _SPM int *audioDacEnReg   = DACEN_ADDR;
+//DAC buffer
+volatile _SPM int *audioDacBufferSizeReg        = DACBUFS_ADDR;
+volatile _SPM int *audioDacBufferWritePulseReg  = DACBUFWP_ADDR;
+volatile _SPM int *audioDacBufferFullReg        = DACBUFFU_ADDR;
+//ADC
+volatile _SPM int *audioAdcLReg	   = ADCL_ADDR;
+volatile _SPM int *audioAdcRReg	   = ADCR_ADDR;
+volatile _SPM int *audioAdcEnReg   = ADCEN_ADDR;
+//ADC buffer
+volatile _SPM int *audioAdcBufferSizeReg      = ADCBUFS_ADDR;
+volatile _SPM int *audioAdcBufferReadPulseReg = ADCBUFRP_ADDR;
+volatile _SPM int *audioAdcBufferEmptyReg     = ADCBUFEM_ADDR;
+//I2C
+volatile _SPM int *i2cDataReg = I2CDATA_ADDR;
+volatile _SPM int *i2cAdrReg  = I2CADDR_ADDR;
+volatile _SPM int *i2cAckReg  = I2CACK_ADDR;
+volatile _SPM int *i2cReqReg  = I2CREQ_ADDR;
+
+void 	setup(int guitar);
+
+int setInputBufferSize(int bufferSize);
+int setOutputBufferSize(int bufferSize);
+
+int getInputBufferSPM(volatile _SPM short *l, volatile _SPM short *r);
+int setOutputBufferSPM(volatile _SPM short *l, volatile _SPM short *r);
+
+#endif /* _AUDIO_SINGLECORE_H_ */
