2b54c70ddf8eb6cfd9966ac3ef2fc07878ae6dad
Martin Schoeberl <martin@jopdesign.com>
2017-04-18 17:13:56 +0200

    emulator: fix register display in VCD file

--- hardware/emulator.cpp
+++ hardware/emulator.cpp
@@ -772,6 +772,10 @@ int main (int argc, char* argv[]) {
   for(int i = 0; i < 5; i++) {
     dat_t<1> reset = LIT<1>(1);
     c->clock_lo(reset);
+    // Print tracing information
+    if (vcd) {
+      c->dump(f, t);
+    }
     c->clock_hi(reset);
   }
 
@@ -784,6 +788,10 @@ int main (int argc, char* argv[]) {
     dat_t<1> reset = LIT<1>(0);
 
     c->clock_lo(reset);
+    // Print tracing information
+    if (vcd) {
+      c->dump(f, t);
+    }
     c->clock_hi(reset);
 
     // Emulate external devices
@@ -801,10 +809,6 @@ int main (int argc, char* argv[]) {
     emu_ethmac(c, ethmac_tap);
     #endif /* IO_ETHMAC */
 
-    // Print tracing information
-    if (vcd) {
-      c->dump(f, t);
-    }
     if (!quiet && c->Patmos_core__enableReg.to_bool()) {
       print_state(c);
     }
