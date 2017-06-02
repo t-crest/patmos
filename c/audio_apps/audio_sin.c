7bd159d1794ab1bd42fbc292435d0458ebef7d56
Luca Pezzarossa <lpez@dtu.dk>
2017-06-02 10:47:55 +0200

    Fixing the audio sin app.

--- c/audio_apps/audio_sin.c
+++ c/audio_apps/audio_sin.c
@@ -7,19 +7,19 @@ short sineArray[220];
 
 int main() {
 
-    setup(0); //line in
+    ADAU1761_init(1); //line in
 
-    setInputBufferSize(BUFFER_SIZE);
-    setOutputBufferSize(BUFFER_SIZE);
+    //setInputBufferSize(BUFFER_SIZE);
+    //setOutputBufferSize(BUFFER_SIZE);
 
     // enable input
-    *audioAdcEnReg = 1;
+    //*audioAdcEnReg = 1;
     //let input buffer fill in before starting to output
-    for(unsigned int i=0; i<(BUFFER_SIZE * 1536); i++) { //wait for BUFFER_SIZE samples
-        *audioDacEnReg = 0;
-    }
+    //for(unsigned int i=0; i<(BUFFER_SIZE * 1536); i++) { //wait for BUFFER_SIZE samples
+    //    *audioDacEnReg = 0;
+    //}
     //finally, enable output
-    *audioDacEnReg = 1;
+    //*audioDacEnReg = 1;
 
 
     volatile _SPM short *left;
@@ -37,7 +37,7 @@ int main() {
         sineArray[i] = 16384*sin(2.0*M_PI* i /220);
     }
 
-    while(*keyReg != 3) {
+    for (int j = 0; j < 1*480000/220; j++){
 
         for (int i = 0; i < 220; i++) {
             *left = sineArray[i];
