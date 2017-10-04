25969e9dcd93c1b71645bc8f894856bccc3c2adf
Martin Schoeberl <martin@jopdesign.com>
2017-10-04 16:54:06 +0200

    bench: start to measure memory access time

--- c/apps/bench/bench.c
+++ c/apps/bench/bench.c
@@ -3,9 +3,36 @@
 #include<stdio.h>
 #include <machine/patmos.h>
 
+volatile static int field;
 
 int main() {
 
-  printf("Hello World\n");
-  printf("%08x\n", PATMOS_IO_DEADLINE);
+  // Pointer to the deadline device
+  volatile _IODEV int *dead_ptr = (volatile _IODEV int *) PATMOS_IO_DEADLINE;
+  // Measure execution time with the clock cycle timer
+  volatile _IODEV int *timer_ptr = (volatile _IODEV int *) (PATMOS_IO_TIMER+4);
+  // use function
+  int start, end;
+  int val;
+
+  start = *timer_ptr;
+  val = *timer_ptr-start;
+  printf("Empty measurement %d\n", val);
+
+  start = *timer_ptr;
+  field = 0;
+  val = *timer_ptr-start;
+  // result is 23, 1 for the measurement, 1 for a constant load
+  // between the two measurements. So a write is 21 clock cycles.
+  printf("Single write %d\n", val);
+
+  for (int i=0; i<100; ++i) {
+    start = *timer_ptr;
+    field = 0;
+    val = *timer_ptr-start;
+    // now 22 clock cycles
+    printf("More writes %d\n", val);
+  }
+
+
 }
