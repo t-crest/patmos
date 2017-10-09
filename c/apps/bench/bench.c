10c0a053aa18038a375bcf0c80270a7d4e5f2f10
Martin Schoeberl <martin@jopdesign.com>
2017-10-09 16:04:50 +0200

    bench: use uncached loads

--- c/apps/bench/bench.c
+++ c/apps/bench/bench.c
@@ -5,9 +5,9 @@
 #include <machine/patmos.h>
 #include <machine/spm.h>
 
-#define CNT 30
+#define CNT 512
 
-volatile static int field;
+volatile _UNCACHED static int field;
 unsigned _SPM *data_spm = SPM_BASE;
 
 void do_delay_times() {
@@ -28,6 +28,9 @@ int main() {
   // use function
   int start, end;
   int val;
+  int acc, add;
+
+  acc = 0;
 
   start = *timer_ptr;
   val = *timer_ptr-start;
@@ -36,30 +39,60 @@ int main() {
   start = *timer_ptr;
   field = 0;
   val = *timer_ptr-start;
+  --val;
   // result is 23, 1 for the measurement, 1 for a constant load
   // between the two measurements. So a write is 21 clock cycles.
   printf("Single write %d\n", val);
 
+  start = *timer_ptr;
+  add = field;
+  val = *timer_ptr-start;
+  acc += add;
+  --val;
+  // result is 23, 1 for the measurement
+  // between the two measurements. So a read is 22 clock cycles.
+  printf("Single read %d\n", val);
+
+  printf("Access in a loop:\n");
   for (int i=0; i<CNT; ++i) {
-    field = 1;
+    // field = 1;
+    add = field;
+    acc += add;
     start = *timer_ptr;
-    field = 0;
+    // field = 0;
+    add = field;
     val = *timer_ptr-start;
     // now 22 clock cycles
-    printf("More writes %d\n", val);
+    acc += add;
+    --val;
+    printf("%d ", val);
   }
+  printf("\n");
+
+  int min = 100;
+  int max = 0;
 
   do_delay_times();
+  printf("Access with a random delay\n");
   for (int i=0; i<CNT; ++i) {
-    field = 1;
+    // field = 1;
+    add = field;
+    acc += add;
     *dead_ptr = data_spm[i];
     val = *dead_ptr; // delay by a random value
     start = *timer_ptr;
-    field = 0;
+    // field = 0;
+    add = field;
     val = *timer_ptr-start;
-    // now 22 clock cycles
-    printf("More writes %d %d\n", val, data_spm[i]);
+    acc += add;
+    --val;
+    printf("%d ", val);
+    if (min>val) min = val;
+    if (max<val) max = val;
   }
+  printf("\n");
 
+  printf("Min: %d max: %d\n", min, max);
+  printf("Acc result %d\n", acc);
 
 }
