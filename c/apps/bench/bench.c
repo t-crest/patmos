6903b3d2d7677302d86f162e99c27ef84188594e
Martin Schoeberl <martin@jopdesign.com>
2017-10-10 16:44:54 +0200

    bench: add multithreading

--- c/apps/bench/bench.c
+++ c/apps/bench/bench.c
@@ -1,9 +1,41 @@
+/*
+    Benchmarking the 9-core version of T-CREST.
 
+    Author: Martin Schoeberl
+    Copyright: DTU, BSD License
+*/
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <machine/patmos.h>
 #include <machine/spm.h>
+
+#include "../../libcorethread/corethread.h"
+
+// Blink the individual LED of a core
+void blink(int period) {
+
+  volatile _IODEV int *led_ptr = (volatile _IODEV int *) PATMOS_IO_LED;
+  volatile _IODEV int *us_ptr = (volatile _IODEV int *) (PATMOS_IO_TIMER+12);
+
+  int time = period*1000/2;
+  int next;
+
+  for (;;) {
+    next = *us_ptr + time;
+    while (*us_ptr-next < 0) *led_ptr = 1;
+    next = *us_ptr + time;
+    while (*us_ptr-next < 0) *led_ptr = 0;
+  }
+}
+
+// The main function for the other thread on the another core
+void work(void* arg) {
+  int val = *((int*)arg);
+
+  blink(val);
+}
+
 
 #define CNT 512
 
@@ -17,7 +49,7 @@ void do_delay_times() {
   }
 }
 
-int main() {
+void bench_mem() {
 
   // Pointer to the deadline device
   volatile _IODEV int *dead_ptr = (volatile _IODEV int *) PATMOS_IO_DEADLINE;
@@ -94,5 +126,22 @@ int main() {
 
   printf("Min: %d max: %d\n", min, max);
   printf("Acc result %d\n", acc);
+}
 
+int main() {
+
+  bench_mem();
+
+  printf("Hello CMP\n");
+  corethread_t worker_id = 1; // The core number
+  int parameter = 1000;
+  corethread_create( &worker_id, &work, (void *) &parameter);  
+
+  blink(2000);
+
+  // the folowing is not executed in this example
+  int* res;
+  corethread_join( worker_id, (void *) &res );
+
+  return 0;
 }
