9e0a1d5110055c640e8a3b69d90829508c382637
unknown <emoun.open@gmail.com>
2017-12-18 20:03:12 +0100

    Software to acompany the SSPM.

---
+++ c/benchmarks/noc_write_bench.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <machine/patmos.h>
+#include <machine/rtc.h>
+#include "libcorethread/corethread.c"
+#include "libmp/mp.h"
+#include "libmp/mp_internal.h"
+
+#define MP_CHAN_NUM_BUF 2
+#define MP_CHAN_BUF_SIZE 40
+#define CHANNEL_BUFFER_CAPACITY (256)
+
+const int NOC_MASTER = 0;
+
+const int TIMES_TO_WRITE = 1000;
+int start_clock[TIMES_TO_WRITE];
+int end_clock[TIMES_TO_WRITE];
+
+void slave(void* args){
+	mp_create_qport(1, SINK, CHANNEL_BUFFER_CAPACITY*sizeof(int),MP_CHAN_NUM_BUF);
+	mp_init_ports();
+}
+
+int main(){
+
+	int cpuid = get_cpuid();
+	int slaveId = 1;
+	corethread_create(&slaveId, &slave, NULL);
+	qpd_t * chan = mp_create_qport(1, SOURCE, CHANNEL_BUFFER_CAPACITY*sizeof(int),MP_CHAN_NUM_BUF);
+	mp_init_ports();
+	
+	int start, end;
+
+	for(int i = 0; i<TIMES_TO_WRITE; i++){
+		asm volatile ("" : : : "memory");
+		start = get_cpu_cycles();
+		asm volatile ("" : : : "memory");
+		
+		for(int k = 0; k<CHANNEL_BUFFER_CAPACITY; k++){
+			(( volatile int _SPM * ) ( chan->write_buf ))[k] = i;
+		}
+		asm volatile ("" : : : "memory");
+		end = get_cpu_cycles();
+		asm volatile ("" : : : "memory");
+		start_clock[i] = start;
+		end_clock[i] = end;
+	}
+	
+	printf("Write bursts: %d\n", CHANNEL_BUFFER_CAPACITY);
+	for(int i = 0; i<TIMES_TO_WRITE; i++){
+		printf("%d\n", end_clock[i]-start_clock[i]);
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
