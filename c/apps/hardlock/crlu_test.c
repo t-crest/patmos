aee03d52fc149191c03d9d1336a8c7affa278c71
Tórur Biskopstø Strøm <torur.strom@gmail.com>
2018-02-01 17:48:22 +0100

    hardlock: Updated test

--- c/apps/hardlock/crlu_test.c
+++ c/apps/hardlock/crlu_test.c
@@ -1,44 +1,80 @@
-#include <string.h>
+//#include <string.h>
+#include <stdio.h>
 #include <machine/patmos.h>
 #include <machine/spm.h>
 #include "libcorethread/corethread.h"
 #include "crlu.h"
+#include <include/patio.h>
+#define MAX_CNT 20
 
 
-_UNCACHED char data[20] = "AAAAAAAAAAAAAAAAAAAA";
+_UNCACHED char data[MAX_CNT] = "AAAAAAAAAAAAAAAAAAAA";
 _UNCACHED volatile int cnt;
-_UNCACHED int times[20];
+_UNCACHED int acquisitions[MAX_CNT];
+_UNCACHED int releases[MAX_CNT];
+_UNCACHED int acquirels[MAX_CNT];
 
 int _main()
 {
-  const char *msg = "Hello, World!\n";
+  int id = get_cpuid();
+  int cnt = get_cpucnt();
+  const int shift = 10;
+  const int iter = 1 << shift;
+  int acquire = 0;
+  int release = 0;
+  int acquirel = 0;
+  
+  int stop1;
+  int stop2;
+  int stop3;
 
-  volatile _SPM int *led_ptr = (volatile _SPM int *) 0xF0090000;
-  volatile _SPM int *uart_ptr = (volatile _SPM int *) 0xF0080004;
-
-  for(int k = 0; k < 4; k++)
+  for(int i = 0; i < iter; i++)
   {
-    lock(0);
-    for (int j = 0; j < 100000; j++) {
-      asm volatile("");
-    }
-    data[cnt++] = get_cpuid() + 48;
-    if(get_cpuid() == 0) {
-      for (int i = 0; i < strlen(msg); i++) {
-        *uart_ptr = msg[i];
-        for (int j = 0; j < 100000; j++) {
-          asm volatile("");
-        }
+    
+    stop1 = TIMER_CLK_LOW;
+    lock(id);
+    stop2 = TIMER_CLK_LOW;
+    unlock(id);
+    stop3 = TIMER_CLK_LOW;
+    acquire += stop2 - stop1;
+    release += stop3 - stop2;
+  }
+
+  for(int i = 0; i < iter; i++)
+  {
+    
+    stop1 = TIMER_CLK_LOW;
+    lock(id);
+    unlock(id);
+    stop2 = TIMER_CLK_LOW;
+    acquirel += stop2 - stop1;
+  }
+
+  acquisitions[id] = acquire >> shift;
+  releases[id] = release >> shift;
+  acquirels[id] = acquirel >> shift;
+  data[id] = 'F';
+  if(id == 0) {
+    for (int i = 0; i < cnt; i++) {
+      printf("Waiting for core %d\n", i);
+      while(data[i] != 'F') {
+        asm volatile("");
       }
-      for (int i = 0; i < 20; i++) {
-        *uart_ptr = data[i];
-        for (int j = 0; j < 100000; j++) {
-          asm volatile("");
-        }
-      }
-      *uart_ptr = '\n';
     }
-    unlock(0);
+    printf("Iterations: %d\n", iter);
+    printf("Average cycles for\n");
+    printf("Acquisitions:\n");
+    for (int i = 0; i < cnt; i++) {
+      printf("%d\n", acquisitions[i]);
+    }
+    printf("Releases:\n");
+    for (int i = 0; i < cnt; i++) {
+      printf("%d\n", releases[i]);
+    }
+    printf("Acquisition and immediate releases:\n");
+    for (int i = 0; i < cnt; i++) {
+      printf("%d\n", acquirels[i]);
+    }
   }
   return 0;
 }
@@ -52,7 +88,7 @@ void worker_func(void* arg) {
 
 int main() {
 
-  id threads[20];
+  int threads[MAX_CNT];
   int len = sizeof threads / sizeof *threads;
   if(get_cpucnt() < len)
     len = get_cpucnt();
