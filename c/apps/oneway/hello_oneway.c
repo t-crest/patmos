0bb5727bc783580ba02ebe272b32299c840af54b
Martin Schoeberl <martin@jopdesign.com>
2018-03-02 21:38:58 +0100

    oneway: cleanup hello world example

--- c/apps/oneway/hello_oneway.c
+++ c/apps/oneway/hello_oneway.c
@@ -1,50 +1,35 @@
 /*
-    Small test program for the One-Way Shared Memory
+  Small test program for the One-Way Shared Memory.
 
-    Author: Martin Schoeberl
-    Copyright: DTU, BSD License
+  Fills TX memories with core specific patterns and reads out
+  the RX memory of core 0 to see which blocks of data come from where.
+
+  Author: Martin Schoeberl
 */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <machine/patmos.h>
 #include <machine/spm.h>
-
-
 #include "../../libcorethread/corethread.h"
 
 #define CNT 4
-#define ONEWAY_BASE *((volatile _SPM int *) 0xE8000000)
 #define WORDS 256
 
-// Shared data in main memory for the return value
-volatile _UNCACHED static int field;
-volatile _UNCACHED static int end_time;
-
-// The main function for the other threads on the another cores
+// The main function for the other threads on the other cores
 void work(void* arg) {
 
-  // Measure execution time with the clock cycle timer
-  volatile _IODEV int *timer_ptr = (volatile _IODEV int *) (PATMOS_IO_TIMER+4);
-  volatile _SPM int *mem_ptr = (volatile _IODEV int *) (0xE8000000);
-
-  *mem_ptr = 0xabcd;
+  volatile _SPM int *txMem = (volatile _SPM int *) (0xE8000000);
 
   int id = get_cpuid();
   for (int i=0; i<CNT; ++i) {
     for (int j=0; j<WORDS; ++j) {
-      *mem_ptr++ = id*0x10000 + i*0x100 + j;
+      txMem[i*WORDS +j] = id*0x10000 + i*0x100 + j;
     }
   }
 }
 
 int main() {
 
-  // Pointer to the deadline device
-  volatile _IODEV int *dead_ptr = (volatile _IODEV int *) PATMOS_IO_DEADLINE;
-  // Measure execution time with the clock cycle timer
-  volatile _IODEV int *timer_ptr = (volatile _IODEV int *) (PATMOS_IO_TIMER+4);
-  volatile _SPM int *rxMem = (volatile _IODEV int *) (0xE8000000);
+  volatile _SPM int *rxMem = (volatile _SPM int *) (0xE8000000);
 
   for (int i=1; i<get_cpucnt(); ++i) {
     corethread_create(i, &work, NULL); 
