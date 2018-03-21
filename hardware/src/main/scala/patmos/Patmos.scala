4c3a1ce24ca5dfff76059a255ea47b226e8336f2
OktayBaris <oktaybaris@hotmail.com>
2018-03-21 23:58:38 +0100

    ownspm:a producer-consumer application

--- hardware/src/main/scala/patmos/Patmos.scala
+++ hardware/src/main/scala/patmos/Patmos.scala
@@ -223,9 +223,9 @@ class Patmos(configFile: String, binFile: String, datFile: String) extends Modul
     }
     // 3 and 4 are reserved for Oktay and Lefteris  
   } else if (cmpDevice == 3) {
-    val tdm = Module(new cmp.TDM(nrCores))
+    val tdmArbiter = Module(new cmp.TdmArbiter(nrCores))
     for (i <- (0 until cores.length)) {
-     tdm.io.slave(i) <> cores(i).io.comSpm
+     tdmArbiter.io.slave(i) <> cores(i).io.comSpm
     }
   } else if (cmpDevice == 5) {
     val ownspm = Module(new cmp.OwnSPM(nrCores, nrCores, 1024))
