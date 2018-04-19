11812b3a96611096a8e5377e60725b28b721d4e3
Morten Borup Petersen <morten_bp@live.dk>
2018-04-19 17:05:19 +0200

    Add test for sequential Write/Read to local node (currently incorrect)

--- hardware/src/test/scala/twoway/TwoWayMemTester.scala
+++ hardware/src/test/scala/twoway/TwoWayMemTester.scala
@@ -126,7 +126,8 @@ class TestExternalReadback(dut: TwoWayMem) extends Tester(dut) {
   }  
   step(1)
 
-  val receivingNode = 1;
+  val transmittingNode = 7  
+  val receivingNode = 8;
 
   //Write 0x42 to address 0x342, which is in node 3.
   poke(dut.io.nodearray(receivingNode).test.out.rw, 1)  
@@ -146,27 +147,97 @@ class TestExternalReadback(dut: TwoWayMem) extends Tester(dut) {
 
 
   //Ask for memory 0x342 from node 0.
-  poke(dut.io.nodearray(0).test.out.rw, 0)  
-  poke(dut.io.nodearray(0).test.out.address, 0x42 + 0x100 * receivingNode)
-  poke(dut.io.nodearray(0).test.out.valid, true)
+  poke(dut.io.nodearray(transmittingNode).test.out.rw, 0)  
+  poke(dut.io.nodearray(transmittingNode).test.out.address, 0x42 + 0x100 * receivingNode)
+  poke(dut.io.nodearray(transmittingNode).test.out.valid, true)
 
   var counter = 0
-  while(peek(dut.io.nodearray(0).test.in.valid) == 0 && counter < 20){
+  while(peek(dut.io.nodearray(transmittingNode).test.in.valid) == 0 && counter < 20){
     step(1)
     counter += 1
 
 
   }
 
-  peek(dut.io.nodearray(0).test.in.data)
-  peek(dut.io.nodearray(0).test.in.valid)
+  peek(dut.io.nodearray(transmittingNode).test.in.data)
+  peek(dut.io.nodearray(transmittingNode).test.in.valid)
 
   step(3)
-  
+}
+
+class TestLocalSequentialWriteRead(dut: TwoWayMem) extends Tester(dut) {
+   //Write four different values in receiving node (address 42,43,44,45)
+  val receivingNode = 0;
+  for(i <- 0 until 4){
+    poke(dut.io.nodearray(receivingNode).test.out.rw, 1)  
+    poke(dut.io.nodearray(receivingNode).test.out.data, 0x42 + i)
+    poke(dut.io.nodearray(receivingNode).test.out.address, 0x42 + i + 0x100 * receivingNode)
+    poke(dut.io.nodearray(receivingNode).test.out.valid, true)
+    step(1)
+  }
+
+  step(1)
+
+  // Read the written adresses
+  for(i <- 0 until 4){
+    poke(dut.io.nodearray(receivingNode).test.out.rw, 0)  
+    poke(dut.io.nodearray(receivingNode).test.out.address, 0x42 + i + 0x100 * receivingNode)
+    poke(dut.io.nodearray(receivingNode).test.out.valid, true)
+    step(1)
+    peek(dut.io.nodearray(receivingNode).test.out.data)
+  }
+}
+
+class TestSimultaniousReads(dut: TwoWayMem) extends Tester(dut) {
+  for (j <- 0 until 4) {
+    //Write 0 to all nodes
+    poke(dut.io.nodearray(j).test.out.rw, 0)  
+    poke(dut.io.nodearray(j).test.out.data, 0)
+    poke(dut.io.nodearray(j).test.out.address, 0)
+    poke(dut.io.nodearray(j).test.out.valid, false)
+  }  
+  step(1)
+
+  val receivingNode = 3;
+
+  //Write four different values in receiving node (address 42,43,44,45)
+  for(i <- 0 until 4){
+    poke(dut.io.nodearray(receivingNode).test.out.rw, 1)  
+    poke(dut.io.nodearray(receivingNode).test.out.data, 0x42 + i)
+    poke(dut.io.nodearray(receivingNode).test.out.address, 0x42 + i + 0x100 * receivingNode)
+    poke(dut.io.nodearray(receivingNode).test.out.valid, true)
+    step(1)
+  }
+
+  //Set receiving node to not write
+  poke(dut.io.nodearray(receivingNode).test.out.rw, 0)  
+  poke(dut.io.nodearray(receivingNode).test.out.data, 0x00)
+  poke(dut.io.nodearray(receivingNode).test.out.address, 0x00)
+  poke(dut.io.nodearray(receivingNode).test.out.valid, false)
+  step(1)
 
 
+  //Ask all nodes to receive memory from the receiving node
+  for(i <- 0 until 4){
+    poke(dut.io.nodearray(i).test.out.rw, 0)  
+    poke(dut.io.nodearray(i).test.out.address, 0x42 + i + 0x100 * receivingNode)
+    poke(dut.io.nodearray(i).test.out.valid, true)
+  }
+  step(1)
 
+  var counter = 0
+  while(counter < 20){
+    step(1)
+    counter += 1
+    for(i <- 0 until 4){
+      peek(dut.io.nodearray(i).test.in.valid)
+      peek(dut.io.nodearray(i).test.in.data)
+      peek(dut.io.nodearray(i).test.in.valid)
+    }
 
+  }
+
+  step(3)
 }
 
 
@@ -175,7 +246,7 @@ object TwoWayMemTester {
     chiselMainTest(Array("--genHarness", "--test", "--backend", "c",
       "--compile", "--vcd", "--targetDir", "generated"),
       () => Module(new TwoWayMem(2, 1024))) {
-        c => new TestExternalReadback(c)
+        c => new TestLocalSequentialWriteRead(c)
       }
   }
 }
