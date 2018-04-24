9f018724c0f81437eaee6d848fd5ceabd8a4a564
Martin Schoeberl <martin@jopdesign.com>
2018-04-24 22:30:53 +0200

    s4noc: cleanup

--- c/apps/s4noc/hello_s4noc.c
+++ c/apps/s4noc/hello_s4noc.c
@@ -1,5 +1,5 @@
 /*
-  Small test program for the S4NOC.
+  Small test program for the S4NOC. The (slot) constants are for the 2x2 configuration.
 
   Author: Martin Schoeberl
 */
@@ -16,10 +16,9 @@ void work(void* arg) {
 
   volatile _SPM int *s4noc = (volatile _SPM int *) (S4NOC_ADDRESS);
 
-  // Wait for RX FIFO data available
-  for(;;) {
-    if (s4noc[RX_READY]) break;
-  }
+  // Wait for RX data available
+  while (!s4noc[RX_READY]) ;
+
   int val = s4noc[IN_DATA];
   if (val == 0xcafebabe) {
     done = 1;
@@ -34,15 +33,13 @@ int main() {
 
   corethread_create(RCV, &work, NULL);
 
-  // wait for TX FIFO ready
-  for(;;) {
-    int status = s4noc[2]; 
-    if (status & 1) break;
-  }
-  s4noc[0] = 0xcafebabe; // into time slot 0 that goes to core 3
+  // wait for TX free
+  while (!s4noc[TX_FREE]) ;
+
+  s4noc[0] = 0xcafebabe; // send at time slot 0 that goes to core 3
 
   printf("Number of cores: %d\n", get_cpucnt());
-  // now, after the print, we should be done
+  // now, after the print, we should have some result
   if (done) {
     printf("0xcafebabe received\n");
     return 0;
