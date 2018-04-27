fae5242ea09b96ffe3b2c16255b8688090fdd938
Simon <simon.thye@gmail.com>
2018-04-27 13:49:44 +0200

    Modified twoway c tester for compatibility

--- c/apps/twoway/hello_twoway.c
+++ c/apps/twoway/hello_twoway.c
@@ -1,53 +1,45 @@
 /*
-    Small test program for the One-Way Shared Memory
-    Author: Martin Schoeberl
-    Copyright: DTU, BSD License
+  Small test program for the One-Way Shared Memory.
+
+  Fills TX memories with core specific patterns and reads out
+  the RX memory of core 0 to see which blocks of data come from where.
+
+  Author: Martin Schoeberl
 */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <machine/patmos.h>
 #include <machine/spm.h>
-
 #include "../../libcorethread/corethread.h"
 
 #define CNT 4
-#define ONEWAY_BASE *((volatile _SPM int *) 0xE8000000)
-#define WORDS 4
+#define WORDS 256
 
-// Shared data in main memory for the return value
-volatile _UNCACHED static int field;
-volatile _UNCACHED static int end_time;
-
-// The main function for the other thread on the another core
+// The main function for the other threads on the other cores
 void work(void* arg) {
 
-  // Measure execution time with the clock cycle timer
-  volatile _IODEV int *timer_ptr = (volatile _IODEV int *) (PATMOS_IO_TIMER+4);
-  volatile _SPM int *mem_ptr = (volatile _IODEV int *) (0xE8000000);
+  volatile _SPM int *txMem = (volatile _SPM int *) (0xE8000000);
 
+  int id = get_cpuid();
   for (int i=0; i<CNT; ++i) {
     for (int j=0; j<WORDS; ++j) {
-      *mem_ptr++ = ((i+1) << 8) + (j+1);
+      txMem[id*WORDS +j] = id*0x10000 + i*0x100 + j;
     }
   }
 }
 
 int main() {
 
-  // Pointer to the deadline device
-  volatile _IODEV int *dead_ptr = (volatile _IODEV int *) PATMOS_IO_DEADLINE;
-  // Measure execution time with the clock cycle timer
-  volatile _IODEV int *timer_ptr = (volatile _IODEV int *) (PATMOS_IO_TIMER+4);
-  volatile _SPM int *mem_ptr = (volatile _IODEV int *) (0xE8000000);
+  volatile _SPM int *rxMem = (volatile _SPM int *) (0xE8000000);
 
-  printf("Number of cores: %d\n", get_cpucnt());
-  field = 42;
-  corethread_create(1, &work, NULL); 
-  printf("Field %d\n", field);
-  for (int i=0; i<CNT*WORDS; ++i) {
-    printf("%04x\n", *mem_ptr++);
+  for (int i=1; i<get_cpucnt(); ++i) {
+    corethread_create(i, &work, NULL); 
   }
 
+  printf("Number of cores: %d\n", get_cpucnt());
+  for (int i=0; i<CNT; ++i) {
+    for (int j=0; j<4; ++j) {
+      printf("%08x\n", rxMem[i*WORDS + j]);
+    }
+  }
   return 0;
 }
