d27fb00794671882510336f02db197dfa5a2ddb7
Simon <simon.thye@gmail.com>
2018-04-28 09:17:24 +0200

    Twoway tester which tests write and read to its own memory.

--- c/apps/twoway/hello_twoway.c
+++ c/apps/twoway/hello_twoway.c
@@ -20,26 +20,37 @@ void work(void* arg) {
   volatile _SPM int *txMem = (volatile _SPM int *) (0xE8000000);
 
   int id = get_cpuid();
-  for (int i=0; i<CNT; ++i) {
-    for (int j=0; j<WORDS; ++j) {
-      txMem[id*WORDS +j] = id*0x10000 + i*0x100 + j;
-    }
-  }
+  txMem[4] = id*0x10000 + 0x142;
+
 }
 
 int main() {
 
   volatile _SPM int *rxMem = (volatile _SPM int *) (0xE8000000);
 
-  for (int i=1; i<get_cpucnt(); ++i) {
+  /*for (int i=1; i<get_cpucnt(); ++i) {
     corethread_create(i, &work, NULL); 
-  }
+  }*/
+  rxMem[0] = 0x40;
+  rxMem[1] = 0x41;
+  rxMem[2] = 0x42;
+  rxMem[3] = 0x43;
 
+
+  printf("\n");
   printf("Number of cores: %d\n", get_cpucnt());
+
+  printf("%d\n",rxMem[0]);
+  printf("%d\n",rxMem[1]);
+  printf("%d\n",rxMem[2]);
+  printf("%d\n",rxMem[3]);
+
+
+  /*
   for (int i=0; i<CNT; ++i) {
     for (int j=0; j<4; ++j) {
       printf("%08x\n", rxMem[i*WORDS + j]);
     }
-  }
+  }*/
   return 0;
 }
