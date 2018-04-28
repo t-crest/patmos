9377cc485d6f7493ce10e01b781ce2ba32d3144a
Simon <simon.thye@gmail.com>
2018-04-28 10:56:08 +0200

    Modified the c test program to show a working example

--- c/apps/twoway/hello_twoway.c
+++ c/apps/twoway/hello_twoway.c
@@ -20,37 +20,32 @@ void work(void* arg) {
   volatile _SPM int *txMem = (volatile _SPM int *) (0xE8000000);
 
   int id = get_cpuid();
-  txMem[4] = id*0x10000 + 0x142;
-
+  //Slave cores write to their own memory.
+  for(int i = 0; i < CNT; i++){
+    txMem[id*CNT + i] = id*0x10000 + 0x100 + i;
+  }
 }
 
 int main() {
 
   volatile _SPM int *rxMem = (volatile _SPM int *) (0xE8000000);
 
-  /*for (int i=1; i<get_cpucnt(); ++i) {
+  for (int i=1; i<get_cpucnt(); ++i) {
     corethread_create(i, &work, NULL); 
-  }*/
-  rxMem[0] = 0x40;
-  rxMem[1] = 0x41;
-  rxMem[2] = 0x42;
-  rxMem[3] = 0x43;
+  }
+
 
 
   printf("\n");
   printf("Number of cores: %d\n", get_cpucnt());
 
-  printf("%d\n",rxMem[0]);
-  printf("%d\n",rxMem[1]);
-  printf("%d\n",rxMem[2]);
-  printf("%d\n",rxMem[3]);
+  //Print content off all the slaves memory.
 
-
-  /*
   for (int i=0; i<CNT; ++i) {
     for (int j=0; j<4; ++j) {
-      printf("%08x\n", rxMem[i*WORDS + j]);
+      printf("%08x\n", rxMem[i*CNT + j]);
     }
-  }*/
+  }
+
   return 0;
 }
