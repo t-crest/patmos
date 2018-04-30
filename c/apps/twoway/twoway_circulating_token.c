be01cdc7b0ec8b55101e888455fd97ae5b8021ad
Morten Borup Petersen <morten_bp@live.dk>
2018-04-30 22:46:14 +0200

    Add LED to circulating token test

--- c/apps/twoway/twoway_circulating_token.c
+++ c/apps/twoway/twoway_circulating_token.c
@@ -15,13 +15,12 @@
 
 #define TOKEN 0x42
 
-#define N_TOKENS 3 // Number of times a core sees a token
+#define N_TOKENS 5 // Number of times a core sees a token
+#define LED ( *((volatile _IODEV unsigned *)0xF0090000))
 
 // Shared data in main memory for the return value
 volatile _UNCACHED static int field;
 volatile _UNCACHED static int end_time;
-
-
 
 // The main function for the other thread on the another core
 void work(void* arg) {
@@ -29,30 +28,33 @@ void work(void* arg) {
     volatile _IODEV int *timer_ptr = (volatile _IODEV int *) (PATMOS_IO_TIMER+4);
     volatile _SPM int *mem_ptr = (volatile _IODEV int *) (0xE8000000);
 
-    volatile _SPM int* readPtr = mem_ptr + (get_cpuid() << BLOCKWIDTH);
+    // writing occurs on addresses 0x100, 0x200, 0x300, 0x400e
 
     //1 reads from 3, 2 from 1 and 3 from 2
-    int partnerID = get_cpuid() - 1 == 0 ? get_cpucnt() - 1 : get_cpuid() - 1;
+    int partnerID = (get_cpuid() - 1) == 0 ? get_cpucnt() - 1 : get_cpuid() - 1;
+    volatile _SPM int* readPtr = mem_ptr + (get_cpuid() << BLOCKWIDTH);
     volatile _SPM int* p_readPtr = mem_ptr + (partnerID << BLOCKWIDTH);
 
     if(get_cpuid() == 1){
-        // Inject token inside core 1 @ 0b0100
+        // Inject token inside core 1 @ 0x100
         *readPtr = TOKEN;
     }
     
     int tokenCounter = 0;
     while(tokenCounter != N_TOKENS){
-
+        
         while(*p_readPtr != TOKEN){
             // wait until we observe our token
         }
+        */
         *p_readPtr = 0; // reset the token on the partner - we now own the token
 
         // Our turn to light the LEDS
-        for(int i = 0; i < 1000000; i++){
-            // Turn on the LED
+        for (int i = 4; i != 0; --i)
+        {
+            LED = 1;
         }
-        // turn off the LED
+        LED = 0;
 
 
         // Write token to ourself (starting transfer of token to next core)
@@ -67,6 +69,7 @@ void work(void* arg) {
 }
 
 int main() {
+  volatile _SPM int *mem_ptr = (volatile _IODEV int *) (0xE8000000);
 
   printf("Number of cores: %d\n", get_cpucnt());
   int parameter = 0;
@@ -79,12 +82,11 @@ int main() {
       corethread_join(i, &param);
   }
 
-  volatile _SPM int *mem_ptr = (volatile _IODEV int *) (0xE8000000);
+
   for(int i = 1; i < get_cpucnt(); i++){
       // Print values in cores
       printf("Core %d:\n", i);
-      printf("%04x = %04x\n", mem_ptr + (i << BLOCKWIDTH), *(mem_ptr + (i << BLOCKWIDTH)));
-      printf("%04x = %04x\n", mem_ptr + (i << BLOCKWIDTH) + 1, *(mem_ptr + (i << BLOCKWIDTH) + 1));
+      printf("%p = %04x\n", mem_ptr + (i << BLOCKWIDTH), *(mem_ptr + (i << BLOCKWIDTH)));
       printf("\n");
   }
   return 0;
