4f1a942d218ebbd6a64a41ddaa634d90b552095b
Morten Borup Petersen <morten_bp@live.dk>
2018-04-30 20:59:51 +0200

    Remove lock from twoway_circulating_token

--- c/apps/twoway/twoway_circulating_token.c
+++ c/apps/twoway/twoway_circulating_token.c
@@ -11,7 +11,6 @@
 
 #include "../../libcorethread/corethread.h"
 
-#define ONEWAY_BASE *((volatile _SPM int *) 0xE8000000)
 #define BLOCKWIDTH 8
 
 #define TOKEN 0x42
@@ -22,20 +21,19 @@
 volatile _UNCACHED static int field;
 volatile _UNCACHED static int end_time;
 
+
+
 // The main function for the other thread on the another core
 void work(void* arg) {
+    // Measure execution time with the clock cycle timer
+    volatile _IODEV int *timer_ptr = (volatile _IODEV int *) (PATMOS_IO_TIMER+4);
+    volatile _SPM int *mem_ptr = (volatile _IODEV int *) (0xE8000000);
 
-  // Measure execution time with the clock cycle timer
-  volatile _IODEV int *timer_ptr = (volatile _IODEV int *) (PATMOS_IO_TIMER+4);
-  volatile _SPM int *mem_ptr = (volatile _IODEV int *) (0xE8000000);
-    
     volatile _SPM int* readPtr = mem_ptr + (get_cpuid() << BLOCKWIDTH);
-    volatile _SPM int* lock = readPtr + 1;
 
     //1 reads from 3, 2 from 1 and 3 from 2
     int partnerID = get_cpuid() - 1 == 0 ? get_cpucnt() - 1 : get_cpuid() - 1;
     volatile _SPM int* p_readPtr = mem_ptr + (partnerID << BLOCKWIDTH);
-    volatile _SPM int* p_lock = readPtr + 1;
 
     if(get_cpuid() == 1){
         // Inject token inside core 1 @ 0b0100
@@ -48,16 +46,17 @@ void work(void* arg) {
         while(*p_readPtr != TOKEN){
             // wait until we observe our token
         }
-        *p_lock = 1; // Lock  the partner core
-        *p_readPtr = 0; // reset the token
+        *p_readPtr = 0; // reset the token on the partner - we now own the token
 
-        // Bad utilization? wait until lock has been written to partner
-        while(*p_lock != 1){
-
+        // Our turn to light the LEDS
+        for(int i = 0; i < 1000000; i++){
+            // Turn on the LED
         }
-        // partner is locked, write token to ourself and release our lock
+        // turn off the LED
+
+
+        // Write token to ourself (starting transfer of token to next core)
         *readPtr = TOKEN;
-        *lock = 0;
 
         // Increment token counter by 1:
         tokenCounter++;
@@ -84,10 +83,9 @@ int main() {
   for(int i = 1; i < get_cpucnt(); i++){
       // Print values in cores
       printf("Core %d:\n", i);
-      printf("%04x = %04x\n", (int)mem_ptr | (i << BLOCKWIDTH), *(volatile _SPM int*)((int)mem_ptr | (i << BLOCKWIDTH)));
-      printf("%04x = %04x\n", (int)mem_ptr | (i << BLOCKWIDTH) | 1, *(volatile _SPM int*)((int)mem_ptr | (i << BLOCKWIDTH) | 1));
+      printf("%04x = %04x\n", mem_ptr + (i << BLOCKWIDTH), *(mem_ptr + (i << BLOCKWIDTH)));
+      printf("%04x = %04x\n", mem_ptr + (i << BLOCKWIDTH) + 1, *(mem_ptr + (i << BLOCKWIDTH) + 1));
       printf("\n");
   }
-
   return 0;
 }
