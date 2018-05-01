902f99b01d0032cf56d802697495fbf6093ecd88
Morten Borup Petersen <morten_bp@live.dk>
2018-05-01 15:14:01 +0200

    run Clang-format on examples

--- c/apps/twoway/master_reading.c
+++ c/apps/twoway/master_reading.c
@@ -9,58 +9,78 @@
 #define CNT 4
 #define WORDS 128
 
-#define LED ( *((volatile _IODEV unsigned *)0xF0090000))
-
+#define LED (*((volatile _IODEV unsigned *)0xF0090000))
 
 #define DELAY_TIME 3000000
 
 #define DELAY(time) for (volatile int i = time; i != 0; i--)
 
 // The main function for the other threads on the other cores
-void work(void* arg) {
+void slaveWriter(void *arg)
+{
+    volatile _SPM int *mem = (volatile _SPM int *)(0xE8000000);
 
-  volatile _SPM int *mem = (volatile _SPM int *) (0xE8000000);
+    int id = get_cpuid();
 
-  int id = get_cpuid();
-	
     // Initialize own value to cpuid, to be able to discern between
     // the three cores
-    mem[id*WORDS] = id;
+    mem[id * WORDS] = id;
 
-	while(1){
-  	  //Slave cores write to their own memory.
-        mem[id*WORDS] = mem[id*WORDS] + 1;
+    while (1)
+    {
+        //Slave cores write to their own memory.
+        mem[id * WORDS] = mem[id * WORDS] + 1;
         DELAY(DELAY_TIME);
-	}
+    }
 }
 
-int main() {
+#define slaveReaderStartAddress 0x10
 
-  volatile _SPM int *rxMem = (volatile _SPM int *) (0xE8000000);
+void slaveReader(void *arg)
+{
+    volatile _SPM int *mem = (volatile _SPM int *)(0xE8000000);
 
-  for (int i=1; i<get_cpucnt(); ++i) {
-    corethread_create(i, &work, NULL); 
-  }
+    // A slave reads all subsequent cores at their base adress
 
-
-
-  printf("\n");
-  printf("Number of cores: %d\n", get_cpucnt());
-
-  //Print content of all the slaves memory.
-  for (int i=0; i<CNT; ++i) {
-    for (int j=0; j<4; ++j) {
-      printf("%08x\n", rxMem[i*WORDS + j]);
+    for (int i = get_cpuid() + 1; i < get - cpucnt(); ++i)
+    {
+        rxMem[i*WORDS]);
     }
-  }
+    DELAY(DELAY_TIME);
+    printf("\n");
+}
 
-    while(1){
-        for (int i=1; i<CNT; ++i) {
-            printf("Core:%d, %08x\n", i,rxMem[i*WORDS]);
+int main()
+{
+
+    volatile _SPM int *rxMem = (volatile _SPM int *)(0xE8000000);
+
+    for (int i = 1; i < get_cpucnt(); ++i)
+    {
+        corethread_create(i, &slaveWriter, NULL);
+    }
+
+    printf("\n");
+    printf("Number of cores: %d\n", get_cpucnt());
+
+    //Print content of all the slaves memory.
+    for (int i = 0; i < CNT; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            printf("%08x\n", rxMem[i * WORDS + j]);
+        }
+    }
+
+    while (1)
+    {
+        for (int i = 1; i < CNT; ++i)
+        {
+            printf("Core:%d, %08x\n", i, rxMem[i * WORDS]);
         }
         DELAY(DELAY_TIME);
         printf("\n");
     }
 
-  return 0;
+    return 0;
 }
--- c/apps/twoway/twoway_circulating_token.c
+++ c/apps/twoway/twoway_circulating_token.c
@@ -16,37 +16,41 @@
 #define TOKEN 0x42
 
 #define N_TOKENS 5 // Number of times a core sees a token
-#define LED ( *((volatile _IODEV unsigned *)0xF0090000))
+#define LED (*((volatile _IODEV unsigned *)0xF0090000))
 
 // Shared data in main memory for the return value
 volatile _UNCACHED static int field;
 volatile _UNCACHED static int end_time;
 
 // The main function for the other thread on the another core
-void work(void* arg) {
+void work(void *arg)
+{
     // Measure execution time with the clock cycle timer
-    volatile _IODEV int *timer_ptr = (volatile _IODEV int *) (PATMOS_IO_TIMER+4);
-    volatile _SPM int *mem_ptr = (volatile _IODEV int *) (0xE8000000);
+    volatile _IODEV int *timer_ptr = (volatile _IODEV int *)(PATMOS_IO_TIMER + 4);
+    volatile _SPM int *mem_ptr = (volatile _IODEV int *)(0xE8000000);
 
     // writing occurs on addresses 0x100, 0x200, 0x300, 0x400e
 
     //1 reads from 3, 2 from 1 and 3 from 2
     int partnerID = (get_cpuid() - 1) == 0 ? get_cpucnt() - 1 : get_cpuid() - 1;
-    volatile _SPM int* readPtr = mem_ptr + (get_cpuid() << BLOCKWIDTH);
-    volatile _SPM int* p_readPtr = mem_ptr + (partnerID << BLOCKWIDTH);
+    volatile _SPM int *readPtr = mem_ptr + (get_cpuid() << BLOCKWIDTH);
+    volatile _SPM int *p_readPtr = mem_ptr + (partnerID << BLOCKWIDTH);
 
-    if(get_cpuid() == 1){
+    if (get_cpuid() == 1)
+    {
         // Inject token inside core 1 @ 0x100
         *readPtr = TOKEN;
     }
-    
+
     int tokenCounter = 0;
-    while(tokenCounter != N_TOKENS){
-        
-        while(*p_readPtr != TOKEN){
+    while (tokenCounter != N_TOKENS)
+    {
+
+        while (*p_readPtr != TOKEN)
+        {
             // wait until we observe our token
         }
-        
+
         *p_readPtr = 0; // reset the token on the partner - we now own the token
 
         // Our turn to light the LEDS
@@ -56,7 +60,6 @@ void work(void* arg) {
         }
         LED = 0;
 
-
         // Write token to ourself (starting transfer of token to next core)
         *readPtr = TOKEN;
 
@@ -64,30 +67,32 @@ void work(void* arg) {
         tokenCounter++;
     }
 
-    
     return;
 }
 
-int main() {
-  volatile _SPM int *mem_ptr = (volatile _IODEV int *) (0xE8000000);
+int main()
+{
+    volatile _SPM int *mem_ptr = (volatile _IODEV int *)(0xE8000000);
 
-  printf("Number of cores: %d\n", get_cpucnt());
-  int parameter = 0;
-  for(int i = 1; i < get_cpucnt(); i++){
-    corethread_create(i, (void*)&work, (void*)&parameter); 
-  }
+    printf("Number of cores: %d\n", get_cpucnt());
+    int parameter = 0;
+    for (int i = 1; i < get_cpucnt(); i++)
+    {
+        corethread_create(i, (void *)&work, (void *)&parameter);
+    }
 
-  int* param;
-  for(int i = 1; i < get_cpucnt(); i++){
-      corethread_join(i, &param);
-  }
+    int *param;
+    for (int i = 1; i < get_cpucnt(); i++)
+    {
+        corethread_join(i, &param);
+    }
 
-
-  for(int i = 1; i < get_cpucnt(); i++){
-      // Print values in cores
-      printf("Core %d:\n", i);
-      printf("%p = %04x\n", mem_ptr + (i << BLOCKWIDTH), *(mem_ptr + (i << BLOCKWIDTH)));
-      printf("\n");
-  }
-  return 0;
+    for (int i = 1; i < get_cpucnt(); i++)
+    {
+        // Print values in cores
+        printf("Core %d:\n", i);
+        printf("%p = %04x\n", mem_ptr + (i << BLOCKWIDTH), *(mem_ptr + (i << BLOCKWIDTH)));
+        printf("\n");
+    }
+    return 0;
 }
