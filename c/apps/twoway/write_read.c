6d52c5188c84cccc8ff8d1a144f92325e249e9c7
Morten Borup Petersen <morten_bp@live.dk>
2018-05-09 20:09:37 +0200

    Add test for memory access measurements

---
+++ c/apps/twoway/write_read.c
@@ -0,0 +1,88 @@
+/*
+    Small test program for the One-Way Shared Memory
+    Author: Martin Schoeberl
+    Copyright: DTU, BSD License
+*/
+
+#include <machine/patmos.h>
+#include <machine/spm.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../libcorethread/corethread.h"
+
+#define BLOCKWIDTH 7
+
+void measure_from_master() {
+  // Pointer to the deadline device
+  volatile _IODEV int *dead_ptr = (volatile _IODEV int *)PATMOS_IO_DEADLINE;
+  // Measure execution time with the clock cycle timer
+  volatile _IODEV int *timer_ptr = (volatile _IODEV int *)(PATMOS_IO_TIMER + 4);
+
+  volatile _SPM int *mem_ptr = (volatile _IODEV int *)(0xE8000000);
+
+  int start, end, val;
+
+  // 1 for measurement, 1 for constant load cycle between the measurement
+  const int const_measurement = 2;
+
+  printf("\n\n---- TIMING TEST ----\n");
+  printf("local accesses (core 0 -> core 0)\n");
+  // Local write
+  start = *timer_ptr;
+  *(mem_ptr) = 0;
+  val = *timer_ptr - start;
+  printf("Local write: %d\n", val);
+
+  // Local read
+  start = *timer_ptr;
+  val = *mem_ptr;
+  val = *timer_ptr - start;
+  printf("Local read: %d\n", val);
+
+  printf("\n---- EXTERNAL MEASUREMENTS ----\n");
+
+  // External read/writes
+  const int CNT = 1 << 9;
+  printf("Measuring min/max values over %d attempts\n", CNT);
+  int min = 100;
+  int max = 0;
+  printf("\n");
+  for (int ae = 0; ae < 2; ae++) {
+    printf("-- iteration %d: --\n", ae);
+    for (int i = 1; i < get_cpucnt(); i++) {
+      printf("master to core %d:\n", i);
+      volatile _SPM int *ptr = mem_ptr + (i << BLOCKWIDTH);
+      // External write
+      for (int k = 0; k < CNT; k++) {
+        *dead_ptr = 10000;
+        val = *dead_ptr; // Delay by random value
+        start = *timer_ptr;
+        *ptr = val;
+        val = *timer_ptr - start;
+        if (min > val)
+          min = val;
+        if (max < val)
+          max = val;
+      }
+      printf("Write:  Min: %d max: %d\n", min, max);
+
+      // External read
+      for (int k = 0; k < CNT; k++) {
+        *dead_ptr = 10000;
+        val = *dead_ptr;
+        start = *timer_ptr;
+        val = *ptr;
+        val = *timer_ptr - start;
+        if (min > val)
+          min = val;
+        if (max < val)
+          max = val;
+      }
+      printf("Read:   Min: %d max: %d\n", min, max);
+      printf("\n");
+    }
+  }
+}
+
+int main() { measure_from_master(); }
