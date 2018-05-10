9211060c49914355cb160f8015a84ee2360ae520
Morten Borup Petersen <morten_bp@live.dk>
2018-05-10 14:14:42 +0200

    Set correct blockwidth in write_read test

--- c/apps/twoway/write_read.c
+++ c/apps/twoway/write_read.c
@@ -11,39 +11,47 @@
 
 #include "../../libcorethread/corethread.h"
 
-#define BLOCKWIDTH 8
+#define BLOCKWIDTH 7
+
+// Pointer to the deadline device
+volatile _IODEV int *dead_ptr = (volatile _IODEV int *)PATMOS_IO_DEADLINE;
+// Measure execution time with the clock cycle timer
+volatile _IODEV int *timer_ptr = (volatile _IODEV int *)(PATMOS_IO_TIMER + 4);
+
+volatile _SPM int *mem_ptr = (volatile _IODEV int *)(0xE8000000);
+
+void measure_from_master_lean() {
+  int start, end, val;
+  for (int i = 0; i < get_cpucnt(); i++) {
+    volatile _SPM int *ptr =
+        (volatile _IODEV int *)(mem_ptr + (i << BLOCKWIDTH));
+    *(ptr) = 10;
+  }
+
+  for (int i = 0; i < get_cpucnt(); i++) {
+    volatile _SPM int *ptr =
+        (volatile _IODEV int *)(mem_ptr + (i << BLOCKWIDTH));
+    val = *ptr;
+  }
+}
+
+void randDelay() {
+  for (volatile int i = rand() % 50; i != 0; i--) {
+  }
+}
 
 void measure_from_master() {
-  // Pointer to the deadline device
-  volatile _IODEV int *dead_ptr = (volatile _IODEV int *)PATMOS_IO_DEADLINE;
-  // Measure execution time with the clock cycle timer
-  volatile _IODEV int *timer_ptr = (volatile _IODEV int *)(PATMOS_IO_TIMER + 4);
-
-  volatile _SPM int *mem_ptr = (volatile _IODEV int *)(0xE8000000);
-
   int start, end, val;
 
   // 1 for measurement, 1 for constant load cycle between the measurement
   const int const_measurement = 2;
 
   printf("\n\n---- TIMING TEST ----\n");
-  printf("local accesses (core 0 -> core 0)\n");
-  // Local write
-  start = *timer_ptr;
-  *(mem_ptr) = 0;
-  val = *timer_ptr - start;
-  printf("Local write: %d\n", val);
-
-  // Local read
-  start = *timer_ptr;
-  val = *mem_ptr;
-  val = *timer_ptr - start;
-  printf("Local read: %d\n", val);
 
   printf("\n---- EXTERNAL MEASUREMENTS ----\n");
 
   // External read/write
-  const int CNT = 1 << 12;
+  const int CNT = 1 << 10;
   const int TRYS = 1;
   printf("Measuring min/max values over %d attempts\n", CNT);
   int min = 100;
@@ -52,12 +60,12 @@ void measure_from_master() {
   for (int t = 1; t <= TRYS; t++) {
     printf("-- Try #%d --\n", t);
     for (int i = 0; i < get_cpucnt(); i++) {
-      volatile _SPM int *ptr = mem_ptr + (i << BLOCKWIDTH);
-      printf("master to core %d:    write ptr:%p\n", i, ptr);
+      volatile _SPM int *ptr =
+          (volatile _IODEV int *)(mem_ptr + (i << BLOCKWIDTH));
+      printf("master to core %d:\n", i);
       // External write
       for (int k = 0; k < CNT; k++) {
-        *dead_ptr = 10000;
-        val = *dead_ptr; // Delay by random value
+        randDelay();
         start = *timer_ptr;
         *(ptr) = 10;
         val = *timer_ptr - start;
@@ -72,8 +80,7 @@ void measure_from_master() {
 
       // External read
       for (int k = 0; k < CNT; k++) {
-        *dead_ptr = 10000;
-        val = *dead_ptr;
+        randDelay();
         start = *timer_ptr;
         val = *(ptr);
         val = *timer_ptr - start;
