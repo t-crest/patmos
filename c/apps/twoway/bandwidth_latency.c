ff1fcfe0764577a1b530545d1e65f24ef57fc198
Morten Borup Petersen <morten_bp@live.dk>
2018-05-11 20:24:24 +0200

    Expand bandwidth test to test cummulative 2x2 network bandwidth

--- c/apps/twoway/bandwidth_latency.c
+++ c/apps/twoway/bandwidth_latency.c
@@ -95,10 +95,14 @@ void latency_from_master() {
   }
 }
 
+  // -----------------------------------------------------------------
+  // --------- Bandwidth tests ---------------------------------------
+  // -----------------------------------------------------------------
+
 #define write                                                                  \
-  "swl  [%4] = %5;"                                                            \
+  "swl  [%2] = %5;"                                                            \
   "swl	[%3] = %5;"                                                             \
-  "swl	[%2] = %5;"
+  "swl	[%4] = %5;"
 
 #define write2 write write
 #define write4 write2 write2
@@ -109,21 +113,47 @@ void latency_from_master() {
 #define write128 write64 write64
 #define write256 write128 write128
 
-void bandwidth_2x2() {
+void bandwidth_2x2(void *arg) {
   int sum = 0;
   volatile int start, stop, val;
   start = *timer_ptr; // Use timer ptr so we can refer to it in inline assembly
                       // by name
 
-  // printf("-- MAXIMUM BANDWITH --\n");
-  // printf("\n");
-  // printf("Write a word to all cores, following the schedule\n");
-  // Hardcoded for 2x2 NoC
+  // Set targets according to schedule
+  int target1, target2, target3;
+  switch (get_cpuid()) {
+  case 0: {
+    target1 = 3;
+    target2 = 2;
+    target1 = 1;
+    break;
+  }
+  case 1: {
+    target1 = 3;
+    target2 = 0;
+    target1 = 2;
+    break;
+  }
+  case 2: {
+    target1 = 1;
+    target2 = 0;
+    target1 = 3;
+    break;
+  }
+  case 3: {
+    target1 = 0;
+    target2 = 1;
+    target1 = 2;
+    break;
+  }
+  }
 
-  _SPM int *mem_ptr_nv = (_IODEV int *)(0xE8000000);
-  _SPM int *ptr1 = (_IODEV int *)(mem_ptr_nv + (1 << BLOCKWIDTH));
-  _SPM int *ptr2 = (_IODEV int *)(mem_ptr_nv + (2 << BLOCKWIDTH));
-  _SPM int *ptr3 = (_IODEV int *)(mem_ptr_nv + (3 << BLOCKWIDTH));
+  volatile _SPM int *ptr1 =
+      (volatile _IODEV int *)(mem_ptr + (target1 << BLOCKWIDTH));
+  volatile _SPM int *ptr2 =
+      (volatile _IODEV int *)(mem_ptr + (target2 << BLOCKWIDTH));
+  volatile _SPM int *ptr3 =
+      (volatile _IODEV int *)(mem_ptr + (target3 << BLOCKWIDTH));
 
   // 256 times, write a word to each of the other 3 cores in the 2x2 network
   const int n = 256 * 3;
@@ -145,14 +175,40 @@ void bandwidth_2x2() {
       : "r"(ptr1), "r"(ptr2), "r"(ptr3), "r"(v)
       : "$r10" /* temporary timer ptr reg*/);
 
+  // Return result
   val = stop - start;
-  printf("%d %d \n", start, stop);
-  printf("Bandwith: %f word/cycle\n", (n / (double)val));
+  double wordsPerCycle = n / (double)val;
+  *((double *)arg) = wordsPerCycle;
+
+  return;
 }
 
 int main() {
   // latency_from_master();
-  bandwidth_2x2();
 
-  // printf("Cummulative bandwidth of all nodes: %f\n");
+  printf("-- MAXIMUM BANDWITH --\n");
+  printf("[wpc] = Words per cycle\n");
+  printf("\n");
+
+  double retvals[get_cpucnt()];
+  for (int i = 1; i < get_cpucnt(); i++) {
+    corethread_create(i, &bandwidth_2x2, (void *)&retvals[i]);
+  }
+  bandwidth_2x2((void *)&retvals[0]);
+
+  for (int i = 1; i < get_cpucnt(); i++) {
+    void *res;
+    corethread_join(i, &res);
+  }
+
+  double sumBandwidth = 0;
+  for (int i = 0; i < get_cpucnt(); i++) {
+    sumBandwidth += retvals[i];
+  }
+
+  // printf("Single core bandwidths:\n");
+  for (int i = 0; i < get_cpucnt(); i++) {
+    printf("\tCore %d: %f wpc\n", i, retvals[i]);
+  }
+  printf("\nCummulative bandwidth of 2x2 network: %f wpc\n", sumBandwidth);
 }
