fc76679740a0ce77d972020d7e7c752940b98176
Luca Pezzarossa <lpez@dtu.dk>
2018-09-13 16:35:29 +0200

    c: Fix of UART2 function.

--- c/apps/de10-nano/acts_props_uart2.c
+++ c/apps/de10-nano/acts_props_uart2.c
@@ -31,22 +31,28 @@ int propulsion_read(unsigned int propulsion_id){
 }
 
 //Writes a byte to the uart2 (to be sent)
-void uart2_write(unsigned char data){
-  *UART2 = (unsigned int) data;
+//Returns 0 is a character was sent, -1 otherwise.
+int uart2_write(unsigned char data){
+  if ((*UART2 & 0x00000001) != 0) {
+    *UART2 = (unsigned int) data;
+    return 0;
+  } else {
+    *data = 0;
+    return -1;
+  }
 }
 
 //Reads a byte from uart2 (from received data) and places it int the variable
 //specified by the pointer * data.
 //Returns 0 is a character was read, -1 otherwise.
 int uart2_read(unsigned char * data){
-  if (*UART2 == 3) {
+  if ((*UART2 & 0x00000002) != 0) {
     *data = (unsigned char)(*(UART2+1) & 0x000000FF);
     return 0;
   } else {
     *data = 0;
     return -1;
   }
-
 }
 
 //Blinks the LEDs once
