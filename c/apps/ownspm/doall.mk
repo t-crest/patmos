49f8a67aa191b9c1f1b8f941c7fb31b88614ed7c
Tórur Biskopstø Strøm <torur.strom@gmail.com>
2018-09-14 14:03:27 +0200

    ownspm: Cleanup + timing.c fix

--- c/apps/ownspm/doall.mk
+++ c/apps/ownspm/doall.mk
@@ -1,66 +1,20 @@
 # This Makefile is used from the main Patmos folder to run all measurements
-# for one buffer configuration. Buffer configuration is set in ownspm.h
-
-EMUP?=exp
-
-all: manual
-
-manual:
-	-rm log.txt
-	make app APP=ownspm MAIN=pc COPTS="-D DATA_LEN=4096 -D _MAINMEM"
-	$(EMUP)/emu4sspm tmp/ownspm.elf >> log.txt
-	make app APP=ownspm MAIN=pc COPTS="-D DATA_LEN=4096 -D _SSPM"
-	$(EMUP)/emu4sspm tmp/ownspm.elf >> log.txt
-	$(EMUP)/emu8sspm tmp/ownspm.elf >> log.txt
-	make app APP=ownspm MAIN=pc COPTS="-D DATA_LEN=4096 -D _OWN"
-	$(EMUP)/emu4_8own tmp/ownspm.elf >> log.txt
-	make app APP=ownspm MAIN=pc COPTS="-D DATA_LEN=4096 -D _MULTIOWN"
-	$(EMUP)/emu4_8multi tmp/ownspm.elf >> log.txt
-	grep measure log.txt > results.txt
-	cat results.txt
-
-pc:
-	-rm log.txt
-	for i in 4 8 16 32 64 128 256; \
+rm log.txt
+for pipeline in "" "-D _PIPELINE"; \
+do \
+	for cpucnt in 2 3 4 5 6 7 8; \
 	do \
-		make app APP=ownspm MAIN=pc COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _MAINMEM"; \
-		$(EMUP)/emu4sspm tmp/ownspm.elf >> log.txt; \
-		make app APP=ownspm MAIN=pc COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _SSPM"; \
-		$(EMUP)/emu4sspm tmp/ownspm.elf >> log.txt; \
-		$(EMUP)/emu8sspm tmp/ownspm.elf >> log.txt; \
-		make app APP=ownspm MAIN=pc COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _OWN"; \
-		$(EMUP)/emu4_8own tmp/ownspm.elf >> log.txt; \
-		make app APP=ownspm MAIN=pc COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _MULTIOWN"; \
-		$(EMUP)/emu4_8multi tmp/ownspm.elf >> log.txt; \
+		for bufsize in 4 8 16 32 64 128; \
+		do \
+			for dev in "-D _MAINMEM" "-D _SSPM" "-D _OWN -D _OWNMAINMEM" "-D _OWN" "-D _SPMPOOL"; \
+			do \
+				make app APP=ownspm MAIN=pc COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$bufsize -D MAX_CPU_CNT=$cpucnt $dev $pipeline"; \
+				#patemu tmp/ownspm.elf >> log.txt; \
+				make config
+				make APP=ownspm download >> log.txt; \
+			done
+		done
 	done
-	grep measure log.txt > results.txt
-	cat results.txt
-
-df_single:
-	-rm log.txt
-#	make app APP=ownspm MAIN=pc_df COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _MAINMEM_DF -D _SINGLE_RELAY"; \
-#	$(EMUP)/emu4sspm tmp/ownspm.elf >> log.txt
-#	make app APP=ownspm MAIN=pc_df COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _SSPM_DF -D _SINGLE_RELAY"; \
-#	$(EMUP)/emu4sspm tmp/ownspm.elf >> log.txt; 
-#	$(EMUP)/emu8sspm tmp/ownspm.elf >> log.txt; 
-#	make app APP=ownspm MAIN=pc_df COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _OWN_DF -D _SINGLE_RELAY"; \
-#	$(EMUP)/emu4_8own tmp/ownspm.elf >> log.txt
-#	make app APP=ownspm MAIN=pc_df COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _MULTIOWN_DF -D _SINGLE_RELAY"; \
-#	$(EMUP)/emu4_8multi tmp/ownspm.elf >> log.txt
-	grep measure log.txt > results.txt
-	cat results.txt
-
-df_multi:
-# _MULTIOWN_DF with multiple relay nodes requires SPM number of (CNT-1)*2
-	-rm log.txt
-#	make app APP=ownspm MAIN=pc_df COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _MAINMEM_DF -D _MULTI_RELAY"; \
-#	$(EMUP)/emu4sspm tmp/ownspm.elf >> log.txt
-#	make app APP=ownspm MAIN=pc_df COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _SSPM_DF -D _MULTI_RELAY"; \
-#	$(EMUP)/emu4sspm tmp/ownspm.elf >> log.txt; 
-#	$(EMUP)/emu8sspm tmp/ownspm.elf >> log.txt; 
-#	make app APP=ownspm MAIN=pc_df COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _OWN_DF -D _MULTI_RELAY"; \
-#	$(EMUP)/emu4_8own tmp/ownspm.elf >> log.txt
-#	make app APP=ownspm MAIN=pc_df COPTS="-D DATA_LEN=4096 -D BUFFER_SIZE=$$i -D _MULTIOWN_DF -D _MULTI_RELAY"; \
-#	$(EMUP)/emu4_8multi tmp/ownspm.elf >> log.txt
-	grep measure log.txt > results.txt
-	cat results.txt
+done
+grep measure log.txt > results.txt
+cat results.txt
--- c/apps/ownspm/setup.h
+++ c/apps/ownspm/setup.h
@@ -12,8 +12,3 @@
 #ifndef MAX_CPU_CNT
 #define MAX_CPU_CNT 16
 #endif
-
-#define CNT 4 //cores
-#define STATUS_OFFSET (CNT-1) // no of status flags for buffer1
-#define STATUS_LEN (STATUS_OFFSET*2) // no of status flags for buffer1
-#define FLAG_OFFSET STATUS_LEN
