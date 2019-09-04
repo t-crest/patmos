08e47d452a6fe47f7bab3df736fa7929345c50ab
Eleftherios Kyriakakis <egk696@hotmail.com>
2019-09-04 11:33:45 +0200

    axi first steps

--- hardware/src/main/scala/axi/Axi.scala
+++ hardware/src/main/scala/axi/Axi.scala
@@ -1,5 +1,47 @@
 package axi
 
-class axilite {
+import Chisel._
 
+class AxiLiteWriteAddressChannel(addrWidth: Int) extends Bundle {
+  val addr = Bits(width = Math.min(addrWidth, 32)) // M->S
+  val prot = Bits(width = 2) // M->S Ignored in our case)
+  if (addrWidth % 32 != 0) throw new IllegalArgumentException("Address width has to be multiple of 32 bits")
+}
+
+class AxiLiteWriteDataChannel(dataWidth: Int) extends Bundle {
+  val data = Bits(width = dataWidth) // M->S
+  val strb = Bits(width = dataWidth / 8) // M->S
+  if (dataWidth % 32 != 0) throw new IllegalArgumentException("Data width has to be multiple of 32 bits")
+}
+
+class AxiLiteWriteRespChannel() extends Bundle {
+  val resp = Bits(width = 2) // S->M
+}
+
+class AxiLiteReadAddressChannel(addrWidth: Int) extends Bundle {
+  val addr = Bits(width = Math.min(addrWidth, 32)) // M->S
+  val prot = Bits(width = 2) // M->S Ignored in our case)
+  if (addrWidth % 32 != 0) throw new IllegalArgumentException("Address width has to be multiple of 32 bits")
+}
+
+class AxiLiteReadRespChannel(dataWidth: Int) extends Bundle {
+  val data = Bits(dataWidth) // S->M
+  val resp = Bits(width = 2) // S->M
+  if (dataWidth % 32 != 0) throw new IllegalArgumentException("Data width has to be multiple of 32 bits")
+}
+
+class AxiLiteMasterPort(addrWidth: Int, dataWidth: Int) extends Bundle {
+  val aw = Decoupled(new AxiLiteReadAddressChannel(addrWidth))
+  val w = Decoupled(new AxiLiteWriteDataChannel(dataWidth))
+  val b = Decoupled(new AxiLiteWriteRespChannel()).flip()
+  val ar = Decoupled(new AxiLiteReadAddressChannel(addrWidth))
+  val r = Decoupled(new AxiLiteReadRespChannel(dataWidth)).flip()
+}
+
+class AxiLiteSlavePort(addrWidth: Int, dataWidth: Int) extends Bundle {
+  val aw = Decoupled(new AxiLiteReadAddressChannel(addrWidth)).flip()
+  val w = Decoupled(new AxiLiteWriteDataChannel(dataWidth)).flip()
+  val b = Decoupled(new AxiLiteWriteRespChannel())
+  val ar = Decoupled(new AxiLiteReadAddressChannel(addrWidth)).flip()
+  val r = Decoupled(new AxiLiteReadRespChannel(dataWidth))
 }
--- hardware/src/main/scala/axi/OcpToAxiBridge.scala
+++ hardware/src/main/scala/axi/OcpToAxiBridge.scala
@@ -1,5 +1,43 @@
 package axi
 
-class OcpToAxiBridge {
+import Chisel._
+import ocp.{OcpCmd, OcpCoreMasterPort, OcpResp}
+
+class OcpToAxiBridge(addrWidth: Int, dataWidth: Int) {
+
+  /**
+    * Converts an OCP master to an AXI4-Lite master interface
+    * @param mOcpPort
+    * @return an AXI4-Lite Master interface
+    */
+  def MOcpToMAxiWrapper(mOcpPort: OcpCoreMasterPort): AxiLiteMasterPort = {
+    val mAxiPort = new AxiLiteMasterPort(addrWidth, dataWidth)
+
+    val sCmdAccept = RegNext(mAxiPort.ar.ready || (mAxiPort.aw.ready && mAxiPort.w.ready))
+
+    val mOcpReg = Reg(mOcpPort.M) // data channel
+
+    when(sCmdAccept) {
+      mOcpReg := mOcpPort.M
+    }
+
+    // Write channel
+    mAxiPort.aw.valid := mOcpReg.Cmd === OcpCmd.WR
+    mAxiPort.aw.bits.addr := mOcpReg.Addr
+    mAxiPort.w.valid := mOcpReg.Cmd === OcpCmd.WR
+    mAxiPort.w.bits.data := mOcpReg.Data
+    mAxiPort.w.bits.strb := mOcpReg.ByteEn
+
+    // Read channel
+    mAxiPort.ar.valid := mOcpReg.Cmd === OcpCmd.RD
+    mAxiPort.r.ready := true.B // the ocp bus is always ready to accept data
+
+    // Drive OCP slave
+    mOcpPort.S.Data := mAxiPort.r.bits.data
+    mOcpPort.S.Resp := Mux(mAxiPort.b.bits.resp === Bits("00") || mAxiPort.r.bits.resp === Bits("00"), OcpResp.DVA, OcpResp.NULL)
+
+    // Return an AXI wrapped interface
+    mAxiPort
+  }
 
 }
