98dcf75c9f8c798b852fcfdd4d26a2547eedafd3
Thonner <anthon.riber@yahoo.com>
2020-02-14 09:29:24 +0100

    Chisel3: Subword assignment workaround

--- hardware/src/main/scala/cmp/Hardlock.scala
+++ hardware/src/main/scala/cmp/Hardlock.scala
@@ -10,7 +10,7 @@
 package cmp
  
 import Chisel._
-
+import chisel3.core.VecInit
 import patmos.Constants._
 import ocp._
 
@@ -34,10 +34,13 @@ abstract class AbstractHardlock(coreCnt : Int,lckCnt : Int) extends Module {
   
   
   val queueReg = RegInit(Vec.fill(lckCnt){UInt(0, coreCnt)})
+  val queueBools = Wire(Vec(lckCnt, Vec(coreCnt, Bool()))) //Chisel3 - Subword assignment unsupported
   for (i <- 0 until lckCnt) {
+    queueBools(i) := queueReg(i).toBools
     for (j <- 0 until coreCnt) {
       when(io.cores(j).sel === UInt(i) && io.cores(j).en === Bool(true)) {
-        queueReg(i)(j) := io.cores(j).op
+        queueBools(i)(j) := io.cores(j).op
+        queueReg(i) := queueBools(i).asUInt()
       }
     }
   }
@@ -45,12 +48,16 @@ abstract class AbstractHardlock(coreCnt : Int,lckCnt : Int) extends Module {
   val curReg = RegInit(Vec.fill(lckCnt){UInt(0, log2Up(coreCnt))})
   
   val blocks = Wire(Vec(coreCnt, Bits(width = lckCnt)))
+  val blockBools = Wire(Vec(coreCnt, Vec(lckCnt, Bool()))) //subword assignment workaround
   
   for (i <- 0 until coreCnt) {
+    blockBools(i) := UInt(0).toBools
     blocks(i) := UInt(0)
     for (j <- 0 until lckCnt) {
-      blocks(i)(j) := queueReg(j)(i) && (curReg(j) =/= UInt(i)) 
+      blockBools(i)(j) := queueReg(j)(i) && (curReg(j) =/= UInt(i))
+      blocks(i) := blockBools(i).asUInt()
     }
+
     io.cores(i).blck := blocks(i).orR
   }
 }
@@ -58,14 +65,20 @@ abstract class AbstractHardlock(coreCnt : Int,lckCnt : Int) extends Module {
 class Hardlock(coreCnt : Int,lckCnt : Int) extends AbstractHardlock(coreCnt, lckCnt) {  
   // Circular priority encoder
   val hi = Wire(Vec(lckCnt, Bits(width = coreCnt)))
+  val hiBools = Wire(Vec(lckCnt, Vec(coreCnt, Bool())))//subword assignment workaround
   val lo = Wire(Vec(lckCnt, Bits(width = coreCnt)))
+  val loBools = Wire(Vec(lckCnt, Vec(coreCnt, Bool())))//subword assignment workaround
   
   for (i <- 0 until lckCnt) {
     lo(i) := UInt(0)
+    hiBools(i) := UInt(0).toBools
     hi(i) := UInt(0)
+    loBools(i) := UInt(0).toBools
     for (j <- 0 until coreCnt) {
-      lo(i)(j) := queueReg(i)(j) && (curReg(i) > UInt(j))
-      hi(i)(j) := queueReg(i)(j) && (curReg(i) <= UInt(j))
+      loBools(i)(j) := queueReg(i)(j) && (curReg(i) > UInt(j))
+      hiBools(i)(j) := queueReg(i)(j) && (curReg(i) <= UInt(j))
+      lo(i) := loBools(i).asUInt()
+      hi(i) := hiBools(i).asUInt()
     }
     
     when(hi(i).orR) {
@@ -86,6 +99,9 @@ class HardlockOCPWrapper(hardlockgen: () => AbstractHardlock) extends Module {
   // Mapping between internal io and OCP here
   
   val reqReg = Reg(init = Bits(0,hardlock.CoreCount))
+  val reqBools = Wire(Vec(hardlock.CoreCount, Bool()))
+
+  reqBools := reqReg.toBools
 
   for (i <- 0 until hardlock.CoreCount) {
     hardlock.io.cores(i).op := io.cores(i).M.Data(0);
@@ -96,10 +112,12 @@ class HardlockOCPWrapper(hardlockgen: () => AbstractHardlock) extends Module {
     }
 
     when(io.cores(i).M.Cmd =/= OcpCmd.IDLE) {
-      reqReg(i) := Bool(true)
+      reqBools(i) := Bool(true)
+      reqReg := reqBools.asUInt()
     }
     .elsewhen(reqReg(i) === Bool(true) && hardlock.io.cores(i).blck === Bool(false)) {
-      reqReg(i) := Bool(false)
+      reqBools(i) := Bool(false)
+      reqReg := reqBools.asUInt()
     }
     
     io.cores(i).S.Resp := OcpResp.NULL
