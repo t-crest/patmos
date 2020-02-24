30cf9380c38c722ea6d000be488351961f76ca78
Eleftherios Kyriakakis <egk696@hotmail.com>
2020-02-24 17:39:47 +0100

    argo wrapper: hot fix commented wrapper blackboxes

--- hardware/src/main/scala/argo/NoCNodeWrapper.scala
+++ hardware/src/main/scala/argo/NoCNodeWrapper.scala
@@ -101,8 +101,7 @@ class NoCNodeWrapper(argoConf: ArgoConfig, master: Boolean) extends BlackBox {
     val south_out = new OutputPort(argoConf)
     val west_out = new OutputPort(argoConf)
   }
-  throw new Error("BlackBox wrapper for NoCNode needs update for Chisel 3")
-  /* Commented out to compile with Chisel3
+  //TODO: Fix blackbox to compile with Chisel3
   setModuleName("noc_node_wrapper")
   addClock(Driver.implicitClock)
   renameClock("clk", "clk")
@@ -111,5 +110,5 @@ class NoCNodeWrapper(argoConf: ArgoConfig, master: Boolean) extends BlackBox {
     setVerilogParameters("#(.MASTER(" + 1 + "))")
   } else {
     setVerilogParameters("#(.MASTER(" + 0 + "))")
-  }*/
+  }
 }
