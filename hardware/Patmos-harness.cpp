919f01f2d422cefe7d767730bb80ed0efd0aea2f
Thonner <anthon.riber@yahoo.com>
2020-03-27 20:36:36 +0100

    Chisel3: work in progress - extmem emu

--- hardware/Patmos-harness.cpp
+++ hardware/Patmos-harness.cpp
@@ -1,20 +1,48 @@
+/* 
+NOTES ON STUFF MISSING FROM THE OLD EMULATOR
+- generating the emulator_config. These care hardcoded below for now
+- VCD dump
+- init_extmem - should not be needed as the current state of the new emulator 
+  uses hashmap for memory. Only for testing width of data channel to memory.
+  but i haven't found a way to get signal width from verilator.
+
+*/
+
+//HARDCODED FROM emulator_config
+#define CORE_COUNT 1
+#define ICACHE_METHOD
+#define IO_UART
+#define IO_LEDS
+#define IO_KEYS
+#define IO_TIMER
+#define IO_DEADLINE
+#define EXTMEM_SRAMCTRL
+
+#define EXTMEM_ADDR_BITS 20
+//HARDCODED FROM emulator_config
 
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <libelf.h>
+#include <gelf.h>
+
 #include "VPatmos.h"
 #include "verilated.h"
 #if VM_TRACE
 #include "verilated_vcd_c.h"
 #endif
-#include <iostream>
-#include <string>
+
+#define OCMEM_ADDR_BITS 16
+
+typedef uint64_t val_t;
 
 using namespace std;
 
 class Emulator
 {
   unsigned long m_tickcount;
-  VPatmos *c;
+  public: VPatmos *c;
 
   // For Uart:
   bool UART_on;
@@ -31,8 +59,19 @@ class Emulator
   string write_str;
   int write_cntr;
   int write_len;
-  ostream* outputTarget = &std::cout;
+  ostream *outputTarget = &std::cout;
 
+  //elf - mem - ram
+  #ifdef EXTMEM_SSRAM32CTRL
+  uint32_t *ram_buf; 
+  #endif /* EXTMEM_SSRAM32CTRL */
+
+  #ifdef EXTMEM_SRAMCTRL
+  uint16_t *ram_buf; 
+  #endif /* EXTMEM_SRAMCTRL */
+
+  //DEBUG STUFF
+  int prints;
 public:
   Emulator(void)
   {
@@ -55,6 +94,17 @@ public:
     write_len = 0;
     c->io_UartCmp_rx = 1; // keep UART tx high when idle
     outputTarget = &cout; // default uart print to terminal
+
+    #ifdef EXTMEM_SSRAM32CTRL
+    ram_buf = (uint32_t *)calloc(1 << EXTMEM_ADDR_BITS, sizeof(uint32_t));
+    #endif /* EXTMEM_SSRAM32CTRL */
+
+    #ifdef EXTMEM_SRAMCTRL
+    ram_buf = (uint16_t *)calloc(1 << EXTMEM_ADDR_BITS, sizeof(uint16_t));
+    #endif /* EXTMEM_SRAMCTRL */
+
+    //DEBUF STUFF
+    prints = 0;
   }
 
   ~Emulator(void)
@@ -63,11 +113,14 @@ public:
     c = NULL;
   }
 
-  void reset(void)
+  void reset(int cycles)
   {
     c->reset = 1;
     // Make sure any inheritance gets applied
-    this->tick();
+    for (int i = 0; i < cycles; i++)
+    {
+      this->tick();
+    }
     c->reset = 0;
   }
 
@@ -86,10 +139,12 @@ public:
     // Rising edge
     c->clock = 1;
     c->eval();
-    // Falling edge
-    c->clock = 0;
-    c->eval();
 
+    //printf("debug: addr: %d, data %d\n", 150000+m_tickcount, ram_buf[150000+m_tickcount]);
+    if(prints < 1020){
+      printf("debug: pc %d\n", c->Patmos__DOT__cores_0__DOT__fetch__DOT__pcReg);
+      prints++;
+    }
     //UART emulation
     if (UART_on)
     {
@@ -144,7 +199,7 @@ public:
         }
         else if ((bit_counter_in > 0) && (bit_counter_in <= 8))
         { // data bits
-          c->io_UartCmp_rx = (write_str[write_cntr] >> (8 - bit_counter_in)) & 1; 
+          c->io_UartCmp_rx = (write_str[write_cntr] >> (8 - bit_counter_in)) & 1;
           bit_counter_in++;
         }
         else
@@ -197,16 +252,423 @@ public:
     writing = true;
   }
 
-  void UART_to_file(string path){
+  void UART_to_file(string path)
+  {
     static ofstream outFile;
-    if (!outFile.is_open()){
+    if (!outFile.is_open())
+    {
       outFile.open(path);
     }
     outputTarget = &outFile;
   }
 
-  void UART_to_console(void){
+  void UART_to_console(void)
+  {
     outputTarget = &cout;
+  }
+
+  //static val_t readelf(istream &is, Patmos_t *c)
+  val_t readelf(istream &is)
+  {
+    vector<unsigned char> elfbuf;
+    elfbuf.reserve(1 << 20);
+
+    // read the whole stream.
+    while (!is.eof())
+    {
+      char buf[1024];
+
+      // read into buffer
+      is.read(&buf[0], sizeof(buf));
+
+      // check how much was read
+      streamsize count = is.gcount();
+      assert(count <= 1024);
+
+      // write into main memory
+      for (unsigned i = 0; i < count; i++)
+        elfbuf.push_back(buf[i]);
+    }
+
+    // check libelf version
+    elf_version(EV_CURRENT);
+
+    // open elf binary
+    Elf *elf = elf_memory((char *)&elfbuf[0], elfbuf.size());
+    assert(elf);
+
+    // check file kind
+    Elf_Kind ek = elf_kind(elf);
+    if (ek != ELF_K_ELF)
+    {
+      cerr << "readelf: ELF file must be of kind ELF.\n";
+      exit(EXIT_FAILURE);
+    }
+
+    // get elf header
+    GElf_Ehdr hdr;
+    GElf_Ehdr *tmphdr = gelf_getehdr(elf, &hdr);
+    assert(tmphdr);
+
+    if (hdr.e_machine != 0xBEEB)
+    {
+      cerr << "readelf: unsupported architecture: ELF file is not a Patmos ELF file.\n";
+      exit(EXIT_FAILURE);
+    }
+
+    // check class
+    int ec = gelf_getclass(elf);
+    if (ec != ELFCLASS32)
+    {
+      cerr << "readelf: unsupported architecture: ELF file is not a 32bit Patmos ELF file.\n";
+      exit(EXIT_FAILURE);
+    }
+
+    // get program headers
+    size_t n;
+    int ntmp = elf_getphdrnum(elf, &n);
+    assert(ntmp == 0);
+
+    for (size_t i = 0; i < n; i++)
+    {
+      // get program header
+      GElf_Phdr phdr;
+      GElf_Phdr *phdrtmp = gelf_getphdr(elf, i, &phdr);
+      assert(phdrtmp);
+
+      if (phdr.p_type == PT_LOAD)
+      {
+        // some assertions
+        //assert(phdr.p_vaddr == phdr.p_paddr);
+        assert(phdr.p_filesz <= phdr.p_memsz);
+
+        // copy from the buffer into the on-chip memories
+        for (size_t k = 0; k < phdr.p_memsz; k++)
+        {
+          if ((phdr.p_flags & PF_X) != 0 &&
+              ((phdr.p_paddr + k) >> OCMEM_ADDR_BITS) == 0x1 &&
+              ((phdr.p_paddr + k) & 0x3) == 0)
+          {
+            // Address maps to ISPM and is at a word boundary
+
+            val_t word = k >= phdr.p_filesz ? 0 : 
+              (((val_t)elfbuf[phdr.p_offset + k + 0] << 24) |
+               ((val_t)elfbuf[phdr.p_offset + k + 1] << 16) |
+               ((val_t)elfbuf[phdr.p_offset + k + 2] << 8) | 
+               ((val_t)elfbuf[phdr.p_offset + k + 3] << 0));
+            val_t addr = ((phdr.p_paddr + k) - (0x1 << OCMEM_ADDR_BITS)) >> 3;
+            unsigned size = (sizeof(c->Patmos__DOT__cores_0__DOT__fetch__DOT__MemBlock__DOT__mem) / //ANTHON THIS MIGHT BE WRONG - SHOULD BE OKAY
+                             sizeof(c->Patmos__DOT__cores_0__DOT__fetch__DOT__MemBlock__DOT__mem[0]));
+            assert(addr < size && "Instructions mapped to ISPM exceed size");
+  
+            // Write to even or odd block
+            if (((phdr.p_paddr + k) & 0x4) == 0)
+            {
+
+              c->Patmos__DOT__cores_0__DOT__fetch__DOT__MemBlock__DOT__mem[addr] = word;
+            }
+            else
+            {
+              c->Patmos__DOT__cores_0__DOT__fetch__DOT__MemBlock_1__DOT__mem[addr] = word;
+            }
+          }
+
+          if (((phdr.p_paddr + k) & 0x3) == 0)
+          {
+            // Address maps to SRAM and is at a word boundary
+            val_t word = k >= phdr.p_filesz ? 0 : (((val_t)elfbuf[phdr.p_offset + k + 0] << 24) | ((val_t)elfbuf[phdr.p_offset + k + 1] << 16) | ((val_t)elfbuf[phdr.p_offset + k + 2] << 8) | ((val_t)elfbuf[phdr.p_offset + k + 3] << 0));
+
+            val_t addr = ((phdr.p_paddr + k) >> 2);
+            write_extmem(addr, word);
+          }
+        }
+      }
+    }
+
+    // get entry point
+    val_t entry = hdr.e_entry;
+
+    elf_end(elf);
+
+    return entry;
+  }
+
+  #ifdef EXTMEM_SSRAM32CTRL
+  void write_extmem(val_t address, val_t word)
+  {
+    ram_buf[address] = word; // This gives segmentation fault dumb on second run!
+  }
+
+  /*void init_extmem(bool random) {
+    // Get SRAM properties
+    uint32_t addr_bits = c->Patmos__io_sSRam32CtrlPins_ramOut_addr.width();
+    uint32_t cells = 1 << addr_bits;
+
+    // Check data width and allocate buffer
+    assert(c->Patmos__io_sSRam32CtrlPins_ramOut_dout.width() == 32);
+    ram_buf = (uint32_t *)calloc(cells, sizeof(uint32_t));
+    if (ram_buf == NULL) {
+      cerr << program_name << ": error: Cannot allocate memory for SRAM emulation" << endl;
+      exit(EXIT_FAILURE);
+    }
+
+    // Initialize with random data
+    if (random) {
+      for (int i = 0; i < cells; i++) {
+        write_extmem(i, rand());
+      }
+    }
+  } */
+
+static void emu_extmem(Patmos_t *c) {
+  static uint32_t addr_cnt;
+  static uint32_t address;
+  static uint32_t counter;
+
+  // Start of request
+  if (!c->Patmos__io_sSRam32CtrlPins_ramOut_nadsc.to_bool()) {
+    address = c->Patmos__io_sSRam32CtrlPins_ramOut_addr.to_ulong();
+    addr_cnt = address;
+    counter = 0;
+  }
+
+  // Advance address for burst
+  if (!c->Patmos__io_sSRam32CtrlPins_ramOut_nadv.to_bool()) {
+    addr_cnt++;
+  }
+
+  // Read from external memory
+  if (!c->Patmos__io_sSRam32CtrlPins_ramOut_noe.to_bool()) {
+    counter++;
+    if (counter >= SRAM_CYCLES) {
+      c->Patmos__io_sSRam32CtrlPins_ramIn_din = ram_buf[address];
+      if (address <= addr_cnt) {
+        address++;
+      }
+    }
+  }
+
+  // Write to external memory
+  if (c->Patmos__io_sSRam32CtrlPins_ramOut_nbwe.to_ulong() == 0) {
+    uint32_t nbw = c->Patmos__io_sSRam32CtrlPins_ramOut_nbw.to_ulong();
+    uint32_t mask = 0x00000000;
+    for (unsigned i = 0; i < 4; i++) {
+      if ((nbw & (1 << i)) == 0) {
+        mask |= 0xff << (i*8);
+      }
+    }
+
+    ram_buf[address] &= ~mask;
+    ram_buf[address] |= mask & c->Patmos__io_sSRam32CtrlPins_ramOut_dout.to_ulong();
+
+    if (address <= addr_cnt) {
+      address++;
+    }
+  }
+}
+
+#endif /*EXTMEM_SSRAM32CTRL*/
+
+#ifdef EXTMEM_SRAMCTRL
+  void write_extmem(val_t address, val_t word) {
+    ram_buf[(address << 1) | 0] = word & 0xffff;
+    ram_buf[(address << 1) | 1] = word >> 16;
+  }
+
+  /*void init_extmem(Patmos_t *c, bool random) {
+    // Get SRAM properties
+    uint32_t addr_bits = c->Patmos_ramCtrl__addrReg.width();
+    uint32_t cells = 1 << addr_bits;
+
+    // Check data width and allocate buffer
+    assert(c->Patmos__io_SRamCtrl_ramOut_dout.width() == 16);
+    ram_buf = (uint16_t *)calloc(cells, sizeof(uint16_t));
+    if (ram_buf == NULL) {
+      cerr << program_name << ": error: Cannot allocate memory for SRAM emulation" << endl;
+      exit(EXIT_FAILURE);
+    }
+
+    // Initialize with random data
+    if (random) {
+      for (int i = 0; i < cells/2; i++) {
+        write_extmem(i, rand());
+      }
+    }
+  }*/
+
+  void emu_extmem() {
+    uint32_t address = (uint32_t) c->Patmos__DOT__ramCtrl__DOT__addrReg;
+
+    // Read from external memory unconditionally
+    c->io_SRamCtrl_ramIn_din = ram_buf[address];
+
+    // Write to external memory
+    //printf("DEBUG: %d\n", address);
+    if (c->io_SRamCtrl_ramOut_nwe != 1) {
+      uint16_t mask = 0x0000;
+      if (c->io_SRamCtrl_ramOut_nub != 1) {
+        mask |= 0xff00;
+      }
+      if (c->io_SRamCtrl_ramOut_nlb != 1) {
+        mask |= 0x00ff;
+      }
+      ram_buf[address] &= ~mask;
+      ram_buf[address] |= mask & ((unsigned long int) c->io_SRamCtrl_ramOut_dout);
+    }
+  }
+#endif /*EXTMEM_SRAMCTRL*/
+
+  void init_icache(val_t entry)
+  {
+    if (entry != 0)
+    {
+      if (entry >= 0x20000)
+      {
+#ifdef ICACHE_METHOD
+        //init for method cache
+        c->Patmos__DOT__cores_0__DOT__fetch__DOT__pcReg = -1;
+        printf("DDBUG pc: %d\n", c->Patmos__DOT__cores_0__DOT__fetch__DOT__pcReg);
+        c->Patmos__DOT__cores_0__DOT__icache__DOT__repl__DOT__hitReg = 0;
+// add multicore support, at the moment only for the method cache and not the ISPM
+#if CORE_COUNT > 1
+        c->Patmos__DOT__cores_1__DOT__fetch__DOT__pcReg = -1;
+        c->Patmos__DOT__cores_1__DOT__icache__DOT__repl__DOT__hitReg = 0;
+#endif
+#if CORE_COUNT > 2
+        c->Patmos__DOT__cores_2__DOT__fetch__DOT__pcReg = -1;
+        c->Patmos__DOT__cores_2__DOT__icache__DOT__repl__DOT__hitReg = 0;
+#endif
+#if CORE_COUNT > 3
+        c->Patmos__DOT__cores_3__DOT__fetch__DOT__pcReg = -1;
+        c->Patmos__DOT__cores_3__DOT__icache__DOT__repl__DOT__hitReg = 0;
+#endif
+#if CORE_COUNT > 4
+        c->Patmos__DOT__cores_4__DOT__fetch__DOT__pcReg = -1;
+        c->Patmos__DOT__cores_4__DOT__icache__DOT__repl__DOT__hitReg = 0;
+#endif
+#if CORE_COUNT > 5
+        c->Patmos__DOT__cores_5__DOT__fetch__DOT__pcReg = -1;
+        c->Patmos__DOT__cores_5__DOT__icache__DOT__repl__DOT__hitReg = 0;
+#endif
+#if CORE_COUNT > 6
+        c->Patmos__DOT__cores_6__DOT__fetch__DOT__pcReg = -1;
+        c->Patmos__DOT__cores_6__DOT__icache__DOT__repl__DOT__hitReg = 0;
+#endif
+#if CORE_COUNT > 7
+        c->Patmos__DOT__cores_7__DOT__fetch__DOT__pcReg = -1;
+        c->Patmos__DOT__cores_7__DOT__icache__DOT__repl__DOT__hitReg = 0;
+#endif
+#endif /* ICACHE_METHOD */
+#ifdef ICACHE_LINE
+        //init for icache
+        c->Patmos__DOT__cores_0__DOT__fetch__DOT__pcReg = (entry >> 2) - 1;
+#endif /* ICACHE_LINE */
+        c->Patmos__DOT__cores_0__DOT__fetch__DOT__relBaseReg = 0;
+        c->Patmos__DOT__cores_0__DOT__fetch__DOT__relocReg = (entry >> 2) - 1;
+        c->Patmos__DOT__cores_0__DOT__fetch__DOT__selCache = 1;
+        c->Patmos__DOT__cores_0__DOT__icache__DOT__repl__DOT__selCacheReg = 1;
+#if CORE_COUNT > 1
+        c->Patmos__DOT__cores_1__DOT__fetch__DOT__relBaseReg = 0;
+        c->Patmos__DOT__cores_1__DOT__fetch__DOT__relocReg = (entry >> 2) - 1;
+        c->Patmos__DOT__cores_1__DOT__fetch__DOT__selCache = 1;
+        c->Patmos__DOT__cores_1__DOT__icache__DOT__repl__DOT__selCacheReg = 1;
+#endif
+#if CORE_COUNT > 2
+        c->Patmos__DOT__cores_2__DOT__fetch__DOT__relBaseReg = 0;
+        c->Patmos__DOT__cores_2__DOT__fetch__DOT__relocReg = (entry >> 2) - 1;
+        c->Patmos__DOT__cores_2__DOT__fetch__DOT__selCache = 1;
+        c->Patmos__DOT__cores_2__DOT__icache__DOT__repl__DOT__selCacheReg = 1;
+#endif
+#if CORE_COUNT > 3
+        c->Patmos__DOT__cores_3__DOT__fetch__DOT__relBaseReg = 0;
+        c->Patmos__DOT__cores_3__DOT__fetch__DOT__relocReg = (entry >> 2) - 1;
+        c->Patmos__DOT__cores_3__DOT__fetch__DOT__selCache = 1;
+        c->Patmos__DOT__cores_3__DOT__icache__DOT__repl__DOT__selCacheReg = 1;
+#endif
+#if CORE_COUNT > 4
+        c->Patmos__DOT__cores_4__DOT__fetch__DOT__relBaseReg = 0;
+        c->Patmos__DOT__cores_4__DOT__fetch__DOT__relocReg = (entry >> 2) - 1;
+        c->Patmos__DOT__cores_4__DOT__fetch__DOT__selCache = 1;
+        c->Patmos__DOT__cores_4__DOT__icache__DOT__repl__DOT__selCacheReg = 1;
+#endif
+#if CORE_COUNT > 5
+        c->Patmos__DOT__cores_5__DOT__fetch__DOT__relBaseReg = 0;
+        c->Patmos__DOT__cores_5__DOT__fetch__DOT__relocReg = (entry >> 2) - 1;
+        c->Patmos__DOT__cores_5__DOT__fetch__DOT__selCache = 1;
+        c->Patmos__DOT__cores_5__DOT__icache__DOT__repl__DOT__selCacheReg = 1;
+#endif
+#if CORE_COUNT > 6
+        c->Patmos__DOT__cores_6__DOT__fetch__DOT__relBaseReg = 0;
+        c->Patmos__DOT__cores_6__DOT__fetch__DOT__relocReg = (entry >> 2) - 1;
+        c->Patmos__DOT__cores_6__DOT__fetch__DOT__selCache = 1;
+        c->Patmos__DOT__cores_6__DOT__icache__DOT__repl__DOT__selCacheReg = 1;
+#endif
+#if CORE_COUNT > 7
+        c->Patmos__DOT__cores_7__DOT__fetch__DOT__relBaseReg = 0;
+        c->Patmos__DOT__cores_7__DOT__fetch__DOT__relocReg = (entry >> 2) - 1;
+        c->Patmos__DOT__cores_7__DOT__fetch__DOT__selCache = 1;
+        c->Patmos__DOT__cores_7__DOT__icache__DOT__repl__DOT__selCacheReg = 1;
+#endif
+      }
+      else
+      {
+        // pcReg for ispm starts at entry point - ispm base
+        c->Patmos__DOT__cores_0__DOT__fetch__DOT__pcReg = ((entry - 0x10000) >> 2) - 1;
+        c->Patmos__DOT__cores_0__DOT__fetch__DOT__relBaseReg = (entry - 0x10000) >> 2;
+        c->Patmos__DOT__cores_0__DOT__fetch__DOT__relocReg = 0x10000 >> 2;
+        c->Patmos__DOT__cores_0__DOT__fetch__DOT__selSpm = 1;
+        c->Patmos__DOT__cores_0__DOT__icache__DOT__repl__DOT__selSpmReg = 1;
+      }
+      c->Patmos__DOT__cores_0__DOT__icache__DOT__repl__DOT__callRetBaseReg = (entry >> 2);
+#if CORE_COUNT > 1
+      c->Patmos__DOT__cores_1__DOT__icache__DOT__repl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 2
+      c->Patmos__DOT__cores_2__DOT__icache__DOT__repl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 3
+      c->Patmos__DOT__cores_3__DOT__icache__DOT__repl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 4
+      c->Patmos__DOT__cores_4__DOT__icache__DOT__repl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 5
+      c->Patmos__DOT__cores_5__DOT__icache__DOT__repl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 6
+      c->Patmos__DOT__cores_6__DOT__icache__DOT__repl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 7
+      c->Patmos__DOT__cores_7__DOT__icache__DOT__repl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#ifdef ICACHE_METHOD
+      c->Patmos__DOT__cores_0__DOT__icache__DOT__ctrl__DOT__callRetBaseReg = (entry >> 2);
+#if CORE_COUNT > 1
+      c->Patmos__DOT__cores_1__DOT__icache__DOT__ctrl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 2
+      c->Patmos__DOT__cores_2__DOT__icache__DOT__ctrl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 3
+      c->Patmos__DOT__cores_3__DOT__icache__DOT__ctrl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 4
+      c->Patmos__DOT__cores_4__DOT__icache__DOT__ctrl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 5
+      c->Patmos__DOT__cores_5__DOT__icache__DOT__ctrl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 6
+      c->Patmos__DOT__cores_6__DOT__icache__DOT__ctrl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#if CORE_COUNT > 7
+      c->Patmos__DOT__cores_7__DOT__icache__DOT__ctrl__DOT__callRetBaseReg = (entry >> 2);
+#endif
+#endif /* ICACHE_METHOD */
+#ifdef ICACHE_LINE
+      c->Patmos__DOT__cores_0__DOT__fetch__DOT__relBaseReg = (entry >> 2);
+#endif /* ICACHE_LINE */
+    }
   }
 };
 
@@ -222,16 +684,43 @@ int main(int argc, char **argv, char **env)
 {
   Verilated::commandArgs(argc, argv);
   Emulator *emu = new Emulator();
-  emu->reset();
+  emu->reset(1);
   emu->tick();
   emu->UART_init(115200, 80000000);
   //emu->UART_to_file("uart_dump.txt");
-  emu->tick();
-  emu->UART_write("hej");
+  emu->UART_write("TEST");
 
+  val_t entry = 0;
+  if (true)
+  { //CHANGE TO LOOK FOR ARGUMENTS AND USE ELF
+    ifstream *fs = new ifstream("/home/anthon/t-crest/patmos/tmp/blinking.elf");
+    if (!fs->good())
+    {
+      cerr << "Error: Cannot open elf file " << endl;
+      exit(EXIT_FAILURE);
+    }
+    entry = emu->readelf(*fs);
+  }
+
+  printf("Debug: %d\n", (int) entry);
+
+  emu->reset(5);
+
+  emu->init_icache(entry);
+
+
+  int cnt = 0;
   while (!emu->done() && emu->get_tick_count() != 10000000)
   {
+    if(cnt < 100){
+      printf("%d", cnt);
+    }else{
+      break;
+    }
+    cnt++;
     emu->tick();
+    printf("DDBUG pc: %d\n", emu->c->Patmos__DOT__cores_0__DOT__fetch__DOT__pcReg);
+    emu->emu_extmem();
     //printf("%d \n", emu->get_tick_count());
   }
   printf("This is a hacked harness \n");
