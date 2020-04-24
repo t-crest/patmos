a886c358e865adef5a533965c40ab8eb7fa4f299
Thonner <anthon.riber@yahoo.com>
2020-04-24 14:06:58 +0200

    bootable cache/ext. mem read write test

--- c/longwrite.c
+++ c/longwrite.c
@@ -7,21 +7,22 @@
 
 #include "include/bootable.h"
 #include "include/patio.h"
+#include <machine/patmos.h>
 #include <machine/spm.h>
 
 #define LOOP_DELAY 100
 
 int main() {
-
+	global_mode();
 	volatile _SPM int *led_ptr  = (volatile _SPM int *) PATMOS_IO_LED;
 	int i, j;
 	
-	char a[262144];
-	for(int i = 0; i < 262144; i++){
-		a[i] = '0' + (i % 74);
+	char a[8000];
+	for(int i = 0; i < 8000; i++){
+		a[i] = '0' + (i % 10);
 	}
 
-	for(int i = 0; i < 262144; i++){
+	for(int i = 0; i < 8000; i++){
 		WRITECHAR(a[i]);
 	}
 }
