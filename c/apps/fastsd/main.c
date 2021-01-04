f6c22446071b47d9fea503ef0ffd09061bddab65
Elthra <schwendinger.martin@gmail.com>
2021-01-04 03:34:15 +0100

    edit fastsd

--- c/apps/fastsd/main.c
+++ c/apps/fastsd/main.c
@@ -5,9 +5,10 @@
 #include "sdcio.h"
 
 int main() {
-    sdcio_write(0, 0xFFFFFFFF);
-    uint32_t data = sdcio_read(0);
+    uint32_t wr_data = 0xFFFFFFFF;
+    sdcio_write(0, wr_data);
+    uint32_t rd_data = sdcio_read(0);
 
-    printf("Wrote to  %08lx data %08lx\n", (uint32_t) SDCIO_BASE+0, (uint32_t) 0xAA55AA55);
-    printf("Read from %08lx data %08lx\n", (uint32_t) SDCIO_BASE+0, (uint32_t) data);
+    printf("Wrote to  %08lx data %08lx\n", (uint32_t) SDCIO_BASE+0, (uint32_t) wr_data);
+    printf("Read from %08lx data %08lx\n", (uint32_t) SDCIO_BASE+0, (uint32_t) rd_data);
 }
