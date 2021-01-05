195ad94547affe71b9bbbd143f49386b4cf0389a
Philipp Birkl <philipp.daniel.birkl@icloud.com>
2021-01-05 12:21:48 +0100

    Added new interface

--- c/apps/fastsd/sdcio.c
+++ c/apps/fastsd/sdcio.c
@@ -1,9 +1,25 @@
 #include "sdcio.h"
 
-void sdcio_write(const uint32_t address, const uint32_t value) {
-    *(SDCIO_BASE+address) = value;
+#include <machine/patmos.h>
+
+static volatile _IODEV uint32_t *const SDCIO_BASE = (volatile _IODEV uint32_t *)0xF00D0000;
+
+void sdc_reg_write(const sdc_reg_t reg, const uint32_t value)
+{
+    *(SDCIO_BASE + reg) = value;
 }
 
-uint32_t sdcio_read(const uint32_t address){
-    return *(SDCIO_BASE+address);
+uint32_t sdc_reg_read(const sdc_reg_t reg)
+{
+    return *(SDCIO_BASE + reg);
+}
+
+void sdc_buffer_write(const sdc_reg_t address, const uint32_t value)
+{
+    *(SDCIO_BASE + address) = value;
+}
+
+uint32_t sdc_buffer_read(const sdc_reg_t address)
+{
+    return *(SDCIO_BASE + address);
 }
--- c/apps/fastsd/sdcio.h
+++ c/apps/fastsd/sdcio.h
@@ -7,12 +7,34 @@
 #ifndef SDCIO_H
 #define SDCIO_H
 
-#include <machine/patmos.h>
 #include <stdint.h>
 
-volatile _IODEV uint32_t * const SDCIO_BASE = (volatile _IODEV uint32_t *)0xf00d0000;
+typedef enum {
+    R_ARGUMENT      = 0x00,
+    R_COMMAND       = 0x04,
+    R_RESP1         = 0x08,
+    R_RESP2         = 0x0C,
+    R_RESP3         = 0x10,
+    R_RESP4         = 0x14,
+    R_DAT_TIMEOUT   = 0x18,
+    R_CONTROL       = 0x1C,
+    R_CMD_TIMEOUT   = 0x20,
+    R_CLK_DEVIDER   = 0x24,
+    R_RESET         = 0x28,
+    R_VOLTAGE       = 0x2C,
+    R_CAPABILITIES  = 0x30,
+    R_CMD_EV_STATUS = 0x34,
+    R_CMD_EV_ENABLE = 0x38,
+    R_DAT_EV_STATUS = 0x3C,
+    R_DAT_EV_ENABLE = 0x40,
+    R_BLOCK_SIZE    = 0x44,
+    R_BLOCK_COUNT   = 0x48,
+    R_BUFF_ADDR     = 0x60
+} sdc_reg_t;
 
-void sdcio_write(const uint32_t address, const uint32_t value);
-uint32_t sdcio_read(const uint32_t address);
+void sdc_reg_write(const sdc_reg_t reg, const uint32_t value);
+uint32_t sdc_reg_read(const sdc_reg_t reg);
 
+void sdc_buffer_write(const sdc_reg_t reg, const uint32_t value);
+uint32_t sdc_buffer_read(const sdc_reg_t reg);
 #endif
