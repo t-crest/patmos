5b2b203194cab1e42f5bc0c0430d5d8800b6edc9
Philipp Birkl <philipp.birkl@student.tuwien.ac.at>
2021-01-06 12:17:35 +0100

    Added simple dir lister

--- c/apps/fastsd/main.c
+++ c/apps/fastsd/main.c
@@ -1,31 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include "ff.h"
 
-#include "sdc_drv.h"
-#include "sdc_mmc.h"
+FRESULT scan_files (
+    char* path        /* Start node to be scanned (***also used as work area***) */
+)
+{
+    FRESULT res;
+    DIR dir;
+    UINT i;
+    static FILINFO fno;
+
+
+    res = f_opendir(&dir, path);                       /* Open the directory */
+    if (res == FR_OK) {
+        for (;;) {
+            res = f_readdir(&dir, &fno);                   /* Read a directory item */
+            if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
+            if (fno.fattrib & AM_DIR) {                    /* It is a directory */
+                i = strlen(path);
+                sprintf(&path[i], "/%s", fno.fname);
+                res = scan_files(path);                    /* Enter the directory */
+                if (res != FR_OK) break;
+                path[i] = 0;
+            } else {                                       /* It is a file. */
+                printf("%s/%s\n", path, fno.fname);
+            }
+        }
+        f_closedir(&dir);
+    }
+
+    return res;
+}
 
 int main()
 {
-    printf("SD CARD TEST !!!\n");
-    struct mmc *drv = sdcdrv_init();
-    if (!drv)
-    {
-        printf("sdcdrv_init failed\n\r");
-        return -1;
+    FATFS fs;
+    FRESULT res;
+    char buff[256];
+
+    res = f_mount(&fs, "", 1);
+    if (res == FR_OK) {
+        strcpy(buff, "/");
+        res = scan_files(buff);
     }
-    printf("sdcdrv_init success\n");
 
-    drv->has_init = 0;
-    int err = mmc_init(drv);
-    if (err != 0 || drv->has_init == 0)
-    {
-        printf("mmc_init failed\n");
-        return -1;
-    }
-    printf("mmc_init success\n\r");
+    return res;
 
-    print_mmcinfo(drv);
-
-    return 0;
 }
--- c/apps/fastsd/sdc_debug.h
+++ c/apps/fastsd/sdc_debug.h
@@ -1,6 +1,8 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#include <stdio.h>
+
 #ifdef DEBUG_SDC
 #define DEBUG_PRINT(fmt, args...) printf("%s:%d: " fmt "\n", __FILE__, __LINE__, ##args)
 #else
