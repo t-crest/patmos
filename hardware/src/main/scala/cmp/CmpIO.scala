e709353c2f61b0ef117530be2749bf5622e748eb
Martin Schoeberl <martin@jopdesign.com>
2022-12-15 14:05:20 -0800

    Fix CmpIO

--- hardware/src/main/scala/cmp/CmpIO.scala
+++ hardware/src/main/scala/cmp/CmpIO.scala
@@ -1,3 +1,9 @@
-package cmp class CmpIO {
+package cmp
 
+import Chisel.{Bundle, Vec}
+import ocp.OcpCoreSlavePort
+import patmos.Constants.{ADDR_WIDTH, DATA_WIDTH}
+
+class CmpIO(val corecnt: Int) extends Bundle {
+  val cores = Vec(corecnt, new OcpCoreSlavePort(ADDR_WIDTH, DATA_WIDTH))
 }
