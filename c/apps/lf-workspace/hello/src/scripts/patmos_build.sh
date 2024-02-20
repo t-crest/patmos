bed4fc0ca565571c286f8aeb03d309dc18a3c99b
Ehsan Khodadad <ehkh@dtu.dk>
2024-02-20 14:05:35 +0100

    upgraded for LF version 0.6.1

--- c/apps/lf-workspace/hello/src/scripts/patmos_build.sh
+++ c/apps/lf-workspace/hello/src/scripts/patmos_build.sh
@@ -9,6 +9,7 @@ echo root is $PROJECT_ROOT and name is $PROJECT_NAME
 cp "$PROJECT_ROOT/src/platform/lf_patmos_support.h"     "$PROJECT_ROOT/src-gen/$PROJECT_NAME/include/core/platform/"
 cp "$PROJECT_ROOT/src/platform/platform.h"              "$PROJECT_ROOT/src-gen/$PROJECT_NAME/include/core/"
 cp "$PROJECT_ROOT/src/platform/lf_patmos_support.c"     "$PROJECT_ROOT/src-gen/$PROJECT_NAME/core/platform/"
+cp "$PROJECT_ROOT/src/platform/lf_atomic_patmos.c"      "$PROJECT_ROOT/src-gen/$PROJECT_NAME/core/platform/"
 cp "$PROJECT_ROOT/src/platform/lf_patmos_support.h"     "$PROJECT_ROOT/include/core/platform/"
 cp "$PROJECT_ROOT/src/platform/platform.h"              "$PROJECT_ROOT/include/core"
 cp "$PROJECT_ROOT/src/Makefile"                         "$PROJECT_ROOT/src-gen/$PROJECT_NAME/" 
